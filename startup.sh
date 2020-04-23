348d3ce5eb5cd80c5c766ef2cae45a332e5ec4cc
Deepanshu Tripathi <deepanshu@persistence.one>
2020-04-24 01:32:57 +0530

    Pruning non-SDK related files.

--- startup.sh
+++
@@ -1,7 +0,0 @@
-hubNode start>~/.hubNode/log &
-sleep 10 ; hubClient rest-server --chain-id test>>~/.hubClient/log &
-echo "
-Node and Client started up. For logs:
-tail -f ~/.hubNode/log
-tail -f ~/.hubClient/log
-"
