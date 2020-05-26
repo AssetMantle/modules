1907e6a2befd88994353a50174d316f1efa2b582
Deepanshu Tripathi <deepanshu@persistence.one>
2020-05-26 17:29:41 +0530

    Moving store key prefix to constants, to be enforced.

--- modules/assetFactory/constants/module.go
+++ modules/assetFactory/constants/module.go
@@ -3,4 +3,7 @@ package constants
 const ModuleName = "assetFactory"
 const DefaultParamspace = "assetFactory"
 const StoreKey = "assetFactory"
+
+var StoreKeyPrefix = []byte{0x11}
+
 const AttributeValueCategory = "assetFactory"
