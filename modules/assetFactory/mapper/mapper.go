6e75cd2978c95bd6816ecbeb0dad0139fc37c3c1
Deepanshu Tripathi <deepanshu@persistence.one>
2020-05-28 06:12:15 +0530

    Updating assetFactory mapper interface functions.

--- modules/assetFactory/mapper/mapper.go
+++ modules/assetFactory/mapper/mapper.go
@@ -7,23 +7,23 @@ import (
 	"github.com/persistenceOne/persistenceSDK/types"
 )
 
-func storeKey(baseAssetID baseAssetID) []byte {
-	return append(constants.StoreKeyPrefix, baseAssetID.Bytes()...)
+func storeKey(assetID assetID) []byte {
+	return append(constants.StoreKeyPrefix, assetID.Bytes()...)
 }
 
 type Mapper interface {
 	create(sdkTypes.Context, baseAsset)
-	read(sdkTypes.Context, baseAssetID) baseAsset
+	read(sdkTypes.Context, assetID) baseAsset
 	update(sdkTypes.Context, baseAsset)
-	delete(sdkTypes.Context, baseAssetID)
-	iterate(sdkTypes.Context, baseAssetID, func(baseAsset) bool)
+	delete(sdkTypes.Context, assetID)
+	iterate(sdkTypes.Context, assetID, func(baseAsset) bool)
 
 	assetBaseImplementationFromInterface(asset types.Asset) baseAsset
 
-	AssetID(chainID types.ID, maintainersID types.ID, classificationID types.ID, hashID types.ID) baseAssetID
+	GenerateAssetID(chainID types.ID, maintainersID types.ID, classificationID types.ID, hashID types.ID) assetID
 
 	New(sdkTypes.Context) types.Assets
-	Assets(sdkTypes.Context, baseAssetID) types.Assets
+	Assets(sdkTypes.Context, assetID) types.Assets
 }
 
 type baseMapper struct {
@@ -46,11 +46,11 @@ func (baseMapper baseMapper) create(context sdkTypes.Context, baseAsset baseAsse
 		panic(err)
 	}
 	kvStore := context.KVStore(baseMapper.storeKey)
-	kvStore.Set(storeKey(baseAsset.baseAssetID), bytes)
+	kvStore.Set(storeKey(baseAsset.assetID), bytes)
 }
-func (baseMapper baseMapper) read(context sdkTypes.Context, baseAssetID baseAssetID) baseAsset {
+func (baseMapper baseMapper) read(context sdkTypes.Context, assetID assetID) baseAsset {
 	kvStore := context.KVStore(baseMapper.storeKey)
-	bytes := kvStore.Get(storeKey(baseAssetID))
+	bytes := kvStore.Get(storeKey(assetID))
 	if bytes == nil {
 		return baseAsset{}
 	}
@@ -66,21 +66,21 @@ func (baseMapper baseMapper) update(context sdkTypes.Context, baseAsset baseAsse
 	if err != nil {
 		panic(err)
 	}
-	baseAssetID := baseAsset.baseAssetID
+	assetID := baseAsset.assetID
 	kvStore := context.KVStore(baseMapper.storeKey)
-	kvStore.Set(storeKey(baseAssetID), bytes)
+	kvStore.Set(storeKey(assetID), bytes)
 }
-func (baseMapper baseMapper) delete(context sdkTypes.Context, baseAssetID baseAssetID) {
+func (baseMapper baseMapper) delete(context sdkTypes.Context, assetID assetID) {
 	bytes, err := baseMapper.codec.MarshalBinaryBare(&baseAsset{})
 	if err != nil {
 		panic(err)
 	}
 	kvStore := context.KVStore(baseMapper.storeKey)
-	kvStore.Set(storeKey(baseAssetID), bytes)
+	kvStore.Set(storeKey(assetID), bytes)
 }
-func (baseMapper baseMapper) iterate(context sdkTypes.Context, baseAssetID baseAssetID, accumulator func(baseAsset) bool) {
+func (baseMapper baseMapper) iterate(context sdkTypes.Context, assetID assetID, accumulator func(baseAsset) bool) {
 	store := context.KVStore(baseMapper.storeKey)
-	kvStorePrefixIterator := sdkTypes.KVStorePrefixIterator(store, storeKey(baseAssetID))
+	kvStorePrefixIterator := sdkTypes.KVStorePrefixIterator(store, storeKey(assetID))
 
 	defer kvStorePrefixIterator.Close()
 	for ; kvStorePrefixIterator.Valid(); kvStorePrefixIterator.Next() {
@@ -95,36 +95,36 @@ func (baseMapper baseMapper) iterate(context sdkTypes.Context, baseAssetID baseA
 	}
 }
 
-func (baseMapper baseMapper) New(context sdkTypes.Context) types.Assets {
-	return &baseAssets{baseMapper: baseMapper, context: context}
-}
-
-func (baseMapper baseMapper) Assets(context sdkTypes.Context, baseAssetID baseAssetID) types.Assets {
-	var baseAssetList []baseAsset
-
-	appendBaseAssetList := func(baseAsset baseAsset) bool {
-		baseAssetList = append(baseAssetList, baseAsset)
-		return false
-	}
-	baseMapper.iterate(context, baseAssetID, appendBaseAssetList)
-
-	return &baseAssets{baseAssetID, baseAssetList, baseMapper, context}
-}
 func (baseMapper baseMapper) assetBaseImplementationFromInterface(asset types.Asset) baseAsset {
 	return baseAsset{
-		baseAssetID{
+		assetID{
 			asset.ChainID(),
 			asset.MaintainersID(),
 			asset.ClassificationID(),
 			asset.HashID(),
 		},
-		asset.OwnersID(),
 		asset.Properties(),
 		asset.GetLock(),
 		asset.GetBurn(),
 	}
 }
 
-func (baseMapper baseMapper) AssetID(chainID types.ID, maintainersID types.ID, classificationID types.ID, hashID types.ID) baseAssetID {
-	return baseAssetID{chainID, maintainersID, classificationID, hashID}
+func (baseMapper baseMapper) GenerateAssetID(chainID types.ID, maintainersID types.ID, classificationID types.ID, hashID types.ID) assetID {
+	return assetID{chainID, maintainersID, classificationID, hashID}
+}
+
+func (baseMapper baseMapper) New(context sdkTypes.Context) types.Assets {
+	return &baseAssets{baseMapper: baseMapper, context: context}
+}
+
+func (baseMapper baseMapper) Assets(context sdkTypes.Context, assetID assetID) types.Assets {
+	var baseAssetList []baseAsset
+
+	appendBaseAssetList := func(baseAsset baseAsset) bool {
+		baseAssetList = append(baseAssetList, baseAsset)
+		return false
+	}
+	baseMapper.iterate(context, assetID, appendBaseAssetList)
+
+	return &baseAssets{assetID, baseAssetList, baseMapper, context}
 }
