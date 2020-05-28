9a3588e086dd4b952fcf64601c5cff3c87a8cc82
Deepanshu Tripathi <deepanshu@persistence.one>
2020-05-28 05:50:06 +0530

    Defining string encoding for asset id.

--- modules/assetFactory/mapper/codec.go
+++ modules/assetFactory/mapper/codec.go
@@ -6,6 +6,6 @@ import (
 
 func RegisterCodec(codec *codec.Codec) {
 	codec.RegisterConcrete(&baseAsset{}, "assetFactory/baseAsset", nil)
-	codec.RegisterConcrete(&baseAssetID{}, "assetFactory/baseAssetID", nil)
+	codec.RegisterConcrete(&assetID{}, "assetFactory/assetID", nil)
 	codec.RegisterConcrete(&baseAssets{}, "assetFactory/baseAssets", nil)
 }
