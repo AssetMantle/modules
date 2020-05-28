369d3fc4163d05fb1324b5bcba51fc3ad1220acd
Deepanshu Tripathi <deepanshu@persistence.one>
2020-05-28 05:51:17 +0530

    Defining base ID.

--- types/id.go
+++ types/id.go
@@ -1,9 +1,22 @@
 package types
 
+import (
+	"bytes"
+	"encoding/base64"
+)
+
 type ID interface {
 	String() string
-
 	Bytes() []byte
-
 	Compare(ID) int
 }
+
+type BaseID struct {
+	BaseBytes []byte
+}
+
+var _ ID = (*BaseID)(nil)
+
+func (baseID BaseID) String() string    { return base64.URLEncoding.EncodeToString(baseID.BaseBytes) }
+func (baseID BaseID) Bytes() []byte     { return baseID.BaseBytes }
+func (baseID BaseID) Compare(id ID) int { return bytes.Compare(baseID.BaseBytes, id.Bytes()) }
