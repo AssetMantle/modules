850faac2c661da8c2cdad5d58df0a03adf163c75
Deepanshu Tripathi <deepanshu@persistence.one>
2020-06-11 12:08:05 +0530

    Removing assets helper type from codec.

--- modules/assetFactory/mapper/codec.go
+++ modules/assetFactory/mapper/codec.go
@@ -7,5 +7,4 @@ import (
 func RegisterCodec(codec *codec.Codec) {
 	codec.RegisterConcrete(&asset{}, "assetFactory/asset", nil)
 	codec.RegisterConcrete(&assetID{}, "assetFactory/assetID", nil)
-	codec.RegisterConcrete(&assets{}, "assetFactory/assets", nil)
 }
