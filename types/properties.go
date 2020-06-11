5c4985748b5968cd06dd2251ac12a6dea9f30667
Deepanshu Tripathi <deepanshu@persistence.one>
2020-06-11 12:06:33 +0530

    Updating types interface definitions.

--- types/properties.go
+++ types/properties.go
@@ -3,36 +3,52 @@ package types
 type Properties interface {
 	Get(ID) Property
 
+	PropertyList() []Property
 	Add(Property) error
 	Remove(Property) error
 	Mutate(Property) error
 }
-
+type BasePropertyList []BaseProperty
 type BaseProperties struct {
-	PropertyList []Property
+	BasePropertyList BasePropertyList
 }
 
 var _ Properties = (*BaseProperties)(nil)
 
 func (baseProperties BaseProperties) Get(id ID) Property {
-	for _, property := range baseProperties.PropertyList {
+	for _, property := range baseProperties.BasePropertyList {
 		if property.ID().Compare(id) == 0 {
 			return property
 		}
 	}
 	return nil
 }
+func (baseProperties *BaseProperties) PropertyList() []Property {
+	var propertyList []Property
+	for _, baseProperty := range baseProperties.BasePropertyList {
+		propertyList = append(propertyList, &baseProperty)
+	}
+	return propertyList
+}
 func (baseProperties *BaseProperties) Add(property Property) error {
-	propertyList := baseProperties.PropertyList
+	propertyList := baseProperties.BasePropertyList
 	for i, oldProperty := range propertyList {
 		if oldProperty.ID().Compare(property.ID()) < 0 {
-			propertyList = append(append(propertyList[:i], property), propertyList[i+1:]...)
+			propertyList = append(append(propertyList[:i], BaseProperty{
+				BaseID: BaseID{
+					IDString: property.ID().String(),
+				},
+				BaseFact: BaseFact{
+					BaseBytes:      property.Fact().Bytes(),
+					BaseSignatures: BaseSignaturesFromInterface(property.Fact().Signatures()),
+				},
+			}), propertyList[i+1:]...)
 		}
 	}
 	return nil
 }
 func (baseProperties *BaseProperties) Remove(property Property) error {
-	propertyList := baseProperties.PropertyList
+	propertyList := baseProperties.BasePropertyList
 	for i, oldProperty := range propertyList {
 		if oldProperty.ID().Compare(property.ID()) == 0 {
 			propertyList = append(propertyList[:i], propertyList[i+1:]...)
@@ -41,11 +57,32 @@ func (baseProperties *BaseProperties) Remove(property Property) error {
 	return nil
 }
 func (baseProperties *BaseProperties) Mutate(property Property) error {
-	propertyList := baseProperties.PropertyList
-	for i, oldProperty := range propertyList {
+	basePropertyList := baseProperties.BasePropertyList
+	for i, oldProperty := range basePropertyList {
 		if oldProperty.ID().Compare(property.ID()) == 0 {
-			propertyList[i] = property
+			basePropertyList[i] = BaseProperty{
+				BaseID: BaseID{
+					IDString: property.ID().String(),
+				},
+				BaseFact: BaseFact{
+					BaseBytes:      property.Fact().Bytes(),
+					BaseSignatures: BaseSignaturesFromInterface(property.Fact().Signatures()),
+				},
+			}
 		}
 	}
 	return nil
+}
+func BasePropertiesFromInterface(properties Properties) BaseProperties {
+	var basePropertyList []BaseProperty
+	for _, property := range properties.PropertyList() {
+		basePropertyList = append(basePropertyList, BaseProperty{
+			BaseID: BaseID{IDString: property.ID().String()},
+			BaseFact: BaseFact{
+				BaseBytes:      property.Fact().Bytes(),
+				BaseSignatures: BaseSignaturesFromInterface(property.Fact().Signatures()),
+			},
+		})
+	}
+	return BaseProperties{BasePropertyList: basePropertyList}
 }
--- types/signatures.go
+++ types/signatures.go
@@ -3,36 +3,49 @@ package types
 type Signatures interface {
 	Get(ID) Signature
 
+	SignatureList() []Signature
 	Add(Signature) error
 	Remove(Signature) error
 	Mutate(Signature) error
 }
-
+type BaseSignatureList []BaseSignature
 type BaseSignatures struct {
-	SignatureList []Signature
+	BaseSignatureList BaseSignatureList
 }
 
 var _ Signatures = (*BaseSignatures)(nil)
 
 func (baseSignatures BaseSignatures) Get(id ID) Signature {
-	for _, signature := range baseSignatures.SignatureList {
+	for _, signature := range baseSignatures.BaseSignatureList {
 		if signature.ID().Compare(id) == 0 {
 			return signature
 		}
 	}
 	return nil
 }
+func (baseSignatures *BaseSignatures) SignatureList() []Signature {
+
+	var signatureList []Signature
+	for _, baseSignature := range baseSignatures.BaseSignatureList {
+		signatureList = append(signatureList, &baseSignature)
+	}
+	return signatureList
+}
 func (baseSignatures *BaseSignatures) Add(signature Signature) error {
-	signatureList := baseSignatures.SignatureList
+	signatureList := baseSignatures.BaseSignatureList
 	for i, oldSignature := range signatureList {
 		if oldSignature.ID().Compare(signature.ID()) < 0 {
-			signatureList = append(append(signatureList[:i], signature), signatureList[i+1:]...)
+			signatureList = append(append(signatureList[:i], BaseSignature{
+				BaseID:             BaseID{signature.ID().String()},
+				BaseBytes:          signature.Bytes(),
+				ValidityBaseHeight: BaseHeight{signature.ValidityHeight().Count()},
+			}), signatureList[i+1:]...)
 		}
 	}
 	return nil
 }
 func (baseSignatures *BaseSignatures) Remove(signature Signature) error {
-	signatureList := baseSignatures.SignatureList
+	signatureList := baseSignatures.BaseSignatureList
 	for i, oldSignature := range signatureList {
 		if oldSignature.ID().Compare(signature.ID()) == 0 {
 			signatureList = append(signatureList[:i], signatureList[i+1:]...)
@@ -41,11 +54,22 @@ func (baseSignatures *BaseSignatures) Remove(signature Signature) error {
 	return nil
 }
 func (baseSignatures *BaseSignatures) Mutate(signature Signature) error {
-	signatureList := baseSignatures.SignatureList
+	signatureList := baseSignatures.BaseSignatureList
 	for i, oldSignature := range signatureList {
 		if oldSignature.ID().Compare(signature.ID()) == 0 {
-			signatureList[i] = signature
+			signatureList[i] = BaseSignature{
+				BaseID:             BaseID{signature.ID().String()},
+				BaseBytes:          signature.Bytes(),
+				ValidityBaseHeight: BaseHeight{signature.ValidityHeight().Count()},
+			}
 		}
 	}
 	return nil
+}
+func BaseSignaturesFromInterface(signatures Signatures) BaseSignatures {
+	var baseSignatureList []BaseSignature
+	for _, signature := range signatures.SignatureList() {
+		baseSignatureList = append(baseSignatureList, BaseSignature{BaseID: BaseID{signature.String()}, BaseBytes: signature.Bytes(), ValidityBaseHeight: BaseHeight{signature.ValidityHeight().Count()}})
+	}
+	return BaseSignatures{BaseSignatureList: baseSignatureList}
 }
