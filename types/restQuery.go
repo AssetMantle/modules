66a05c14ce3de97def0640c7e1341e58da1a7b66
Deepanshu Tripathi <deepanshu@persistence.one>
2020-06-21 03:09:19 +0530

    Adding restQuery and restRequest types.

---
+++ types/restQuery.go
@@ -0,0 +1,50 @@
+package types
+
+import (
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strings"
+)
+
+type RESTQuery interface {
+	CreateQuery(context.CLIContext, func(map[string]string) []byte) http.HandlerFunc
+}
+type Query interface{}
+
+type restQuery struct {
+	route string
+	path  string
+	query Query
+}
+
+var _ RESTQuery = (*restQuery)(nil)
+
+func (restQuery restQuery) CreateQuery(cliContext context.CLIContext, makeQueryBytes func(map[string]string) []byte) http.HandlerFunc {
+	return func(responseWriter http.ResponseWriter, httpRequest *http.Request) {
+		responseWriter.Header().Set("Content-Type", "application/json")
+		cliContext, ok := rest.ParseQueryHeightOrReturnBadRequest(responseWriter, cliContext, httpRequest)
+		if !ok {
+			return
+		}
+
+		vars := mux.Vars(httpRequest)
+		bytes := makeQueryBytes(vars)
+		response, height, err := cliContext.QueryWithData(strings.Join([]string{"", "custom", restQuery.route, restQuery.path}, "/"), bytes)
+		if err != nil {
+			rest.WriteErrorResponse(responseWriter, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		cliContext = cliContext.WithHeight(height)
+		rest.PostProcessResponse(responseWriter, cliContext, response)
+	}
+}
+func NewRESTQuery(route string, path string, query interface{}) RESTQuery {
+	return &restQuery{
+		route: route,
+		path:  path,
+		query: query,
+	}
+}
---
+++ types/restRequest.go
@@ -0,0 +1,46 @@
+package types
+
+import (
+	"github.com/cosmos/cosmos-sdk/client/context"
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/cosmos/cosmos-sdk/x/auth/client"
+	"net/http"
+)
+
+type RESTRequest interface {
+	CreateRequest(context.CLIContext, func(Request) (rest.BaseReq, sdkTypes.Msg)) http.HandlerFunc
+}
+type Request interface{}
+
+type restRequest struct {
+	request Request
+}
+
+var _ RESTRequest = (*restRequest)(nil)
+
+func (restRequest restRequest) CreateRequest(cliContext context.CLIContext, makeBaseReqAndMsg func(Request) (rest.BaseReq, sdkTypes.Msg)) http.HandlerFunc {
+	return func(responseWriter http.ResponseWriter, httpRequest *http.Request) {
+
+		if !rest.ReadRESTReq(responseWriter, httpRequest, cliContext.Codec, &restRequest.request) {
+			rest.WriteErrorResponse(responseWriter, http.StatusBadRequest, "")
+			return
+		}
+
+		baseReq, msg := makeBaseReqAndMsg(restRequest.request)
+
+		baseReq = baseReq.Sanitize()
+		if !baseReq.ValidateBasic(responseWriter) {
+			rest.WriteErrorResponse(responseWriter, http.StatusBadRequest, "")
+			return
+		}
+
+		Error := msg.ValidateBasic()
+		if Error != nil {
+			rest.WriteErrorResponse(responseWriter, http.StatusBadRequest, Error.Error())
+			return
+		}
+		client.WriteGenerateStdTxResponse(responseWriter, cliContext, baseReq, []sdkTypes.Msg{msg})
+	}
+}
+func NewRESTRequest(request interface{}) RESTRequest { return &restRequest{request: request} }
