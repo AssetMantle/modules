2cb50ba1ccf60dee43da310715730052b95d8c20
Deepanshu Tripathi <deepanshu@persistence.one>
2020-06-21 21:52:53 +0530

    Adding TransactionKeeper interface.

--- modules/assetFactory/keeper.go
+++ modules/assetFactory/keeper.go
@@ -7,16 +7,17 @@ import (
 	"github.com/persistenceOne/persistenceSDK/modules/assetFactory/mapper"
 	"github.com/persistenceOne/persistenceSDK/modules/assetFactory/queries/asset"
 	"github.com/persistenceOne/persistenceSDK/modules/assetFactory/transactions/mint"
+	"github.com/persistenceOne/persistenceSDK/types"
 )
 
 type Keeper interface {
-	getMintKeeper() mint.Keeper
+	getMintKeeper() types.TransactionKeeper
 
 	getAssetQuerier() asset.Querier
 }
 
 type keeper struct {
-	mintKeeper mint.Keeper
+	mintKeeper types.TransactionKeeper
 
 	assetQuerier asset.Querier
 }
@@ -24,13 +25,13 @@ type keeper struct {
 func NewKeeper(codec *codec.Codec, storeKey sdkTypes.StoreKey, paramSpace params.Subspace) Keeper {
 	Mapper := mapper.NewMapper(codec, storeKey)
 	return keeper{
-		mintKeeper:   mint.NewKeeper(Mapper),
+		mintKeeper:   mint.NewTransactionKeeper(Mapper),
 		assetQuerier: asset.NewQuerier(Mapper),
 	}
 }
 
 var _ Keeper = (*keeper)(nil)
 
-func (keeper keeper) getMintKeeper() mint.Keeper { return keeper.mintKeeper }
+func (keeper keeper) getMintKeeper() types.TransactionKeeper { return keeper.mintKeeper }
 
 func (keeper keeper) getAssetQuerier() asset.Querier { return keeper.assetQuerier }
