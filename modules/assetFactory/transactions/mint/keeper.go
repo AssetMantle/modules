ebf452da838f1aa5c826143812a7d5cb34dcc8ae
Deepanshu Tripathi <deepanshu@persistence.one>
2020-06-21 21:52:43 +0530

    Adding TransactionKeeper interface.

--- modules/assetFactory/transactions/mint/keeper.go
+++ modules/assetFactory/transactions/mint/keeper.go
@@ -4,28 +4,26 @@ import (
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/persistenceOne/persistenceSDK/modules/assetFactory/constants"
 	"github.com/persistenceOne/persistenceSDK/modules/assetFactory/mapper"
+	"github.com/persistenceOne/persistenceSDK/types"
 )
 
-type Keeper interface {
-	transact(sdkTypes.Context, Message) error
-}
-
-type keeper struct {
+type transactionKeeper struct {
 	mapper mapper.Mapper
 }
 
-func NewKeeper(mapper mapper.Mapper) Keeper {
-	return keeper{mapper: mapper}
+func NewTransactionKeeper(mapper mapper.Mapper) types.TransactionKeeper {
+	return transactionKeeper{mapper: mapper}
 }
 
-var _ Keeper = (*keeper)(nil)
+var _ types.TransactionKeeper = (*transactionKeeper)(nil)
 
-func (keeper keeper) transact(context sdkTypes.Context, message Message) error {
+func (transactionKeeper transactionKeeper) Transact(context sdkTypes.Context, msg sdkTypes.Msg) error {
+	message := msg.(Message)
 	immutablePropertyList := message.Properties.GetList()
-	hashID := keeper.mapper.MakeHashID(immutablePropertyList)
+	hashID := transactionKeeper.mapper.MakeHashID(immutablePropertyList)
 	assetID := mapper.NewAssetID(message.ChainID, message.MaintainersID, message.ClassificationID, hashID)
 	asset := mapper.NewAsset(assetID, message.Properties, message.Lock, message.Burn)
-	assets := keeper.mapper.Assets(context, assetID)
+	assets := transactionKeeper.mapper.Assets(context, assetID)
 	if assets.Get(assetID) != nil {
 		return constants.EntityAlreadyExistsCode
 	}
