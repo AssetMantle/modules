eb570594056408318dce175f383b71a8cffdb961
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-01 01:46:50 +0530

    Adding getList and read methods to interNFT interface.

--- modules/assets/mapper/assets.go
+++ modules/assets/mapper/assets.go
@@ -11,47 +11,74 @@ type assets struct {
 	ID   types.ID
 	List []types.InterNFT
 
-	Mapper  mapper
+	Mapper  Mapper
 	Context sdkTypes.Context
 }
 
-func (assets assets) GetID() types.ID { return assets.ID }
-func (assets assets) Get(id types.ID) types.InterNFT {
-	for _, asset := range assets.List {
-		if asset.GetID().Compare(id) == 0 {
-			return asset
+func (Assets assets) GetID() types.ID { return Assets.ID }
+func (Assets assets) Get(id types.ID) types.InterNFT {
+	for _, oldAsset := range Assets.List {
+		if oldAsset.GetID().Compare(id) == 0 {
+			return oldAsset
 		}
 	}
 	return nil
 }
+func (Assets assets) GetList() []types.InterNFT {
+	return Assets.List
+}
 
-func (assets assets) Add(asset types.InterNFT) types.InterNFTs {
-	assets.Mapper.create(assets.Context, asset)
-	for i, oldAsset := range assets.List {
+func (Assets assets) Read(id types.ID) types.InterNFTs {
+	var assetList []types.InterNFT
+	assetsID := assetIDFromInterface(id)
+	if assetsID.HashID != nil {
+		assetList = append(assetList, Assets.Mapper.read(Assets.Context, assetsID))
+	} else {
+		appendAssetList := func(asset types.InterNFT) bool {
+			assetList = append(assetList, asset)
+			return false
+		}
+		Assets.Mapper.iterate(Assets.Context, assetsID, appendAssetList)
+	}
+	return &assets{id, assetList, Assets.Mapper, Assets.Context}
+}
+func (Assets assets) Add(asset types.InterNFT) types.InterNFTs {
+	Assets.ID = nil
+	Assets.Mapper.create(Assets.Context, asset)
+	for i, oldAsset := range Assets.List {
 		if oldAsset.GetID().Compare(asset.GetID()) < 0 {
-			assets.List = append(append(assets.List[:i], asset), assets.List[i+1:]...)
+			Assets.List = append(append(Assets.List[:i], asset), Assets.List[i+1:]...)
 			break
 		}
 	}
-	return assets
+	return Assets
 }
-func (assets assets) Remove(asset types.InterNFT) types.InterNFTs {
-	assets.Mapper.delete(assets.Context, asset.GetID())
-	for i, oldAsset := range assets.List {
+func (Assets assets) Remove(asset types.InterNFT) types.InterNFTs {
+	Assets.Mapper.delete(Assets.Context, asset.GetID())
+	for i, oldAsset := range Assets.List {
 		if oldAsset.GetID().Compare(asset.GetID()) == 0 {
-			assets.List = append(assets.List[:i], assets.List[i+1:]...)
+			Assets.List = append(Assets.List[:i], Assets.List[i+1:]...)
 			break
 		}
 	}
-	return assets
+	return Assets
 }
-func (assets assets) Mutate(asset types.InterNFT) types.InterNFTs {
-	assets.Mapper.update(assets.Context, asset)
-	for i, oldAsset := range assets.List {
+func (Assets assets) Mutate(asset types.InterNFT) types.InterNFTs {
+	Assets.Mapper.update(Assets.Context, asset)
+	for i, oldAsset := range Assets.List {
 		if oldAsset.GetID().Compare(asset.GetID()) == 0 {
-			assets.List[i] = asset
+			Assets.List[i] = asset
 			break
 		}
 	}
-	return assets
+	return Assets
+}
+
+func NewAssets(mapper Mapper, context sdkTypes.Context) types.InterNFTs {
+	return &assets{
+		ID:      nil,
+		List:    nil,
+		Mapper:  mapper,
+		Context: context,
+	}
 }
--- types/interNFTs.go
+++ types/interNFTs.go
@@ -2,8 +2,11 @@ package types
 
 type InterNFTs interface {
 	GetID() ID
-	Get(ID) InterNFT
 
+	Get(ID) InterNFT
+	GetList() []InterNFT
+
+	Read(ID) InterNFTs
 	Add(InterNFT) InterNFTs
 	Remove(InterNFT) InterNFTs
 	Mutate(InterNFT) InterNFTs
