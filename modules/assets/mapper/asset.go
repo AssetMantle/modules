b0adc3d3ba0c967412aab593790a9481cdf50ac3
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-02 13:08:39 +0530

    Reformatting code.

--- modules/assets/mapper/asset.go
+++ modules/assets/mapper/asset.go
@@ -13,27 +13,41 @@ type asset struct {
 	Burn       types.Height
 }
 
-func (asset asset) GetID() types.ID { return asset.ID }
+func (asset asset) GetID() types.ID {
+	return asset.ID
+}
 
-func (asset asset) GetChainID() types.ID { return assetIDFromInterface(asset.ID).ChainID }
+func (asset asset) GetChainID() types.ID {
+	return assetIDFromInterface(asset.ID).ChainID
+}
 
 func (asset asset) GetClassificationID() types.ID {
 	return assetIDFromInterface(asset.ID).ClassificationID
 }
 
-func (asset asset) GetMaintainersID() types.ID { return assetIDFromInterface(asset.ID).MaintainersID }
+func (asset asset) GetMaintainersID() types.ID {
+	return assetIDFromInterface(asset.ID).MaintainersID
+}
 
-func (asset asset) GetHashID() types.ID { return assetIDFromInterface(asset.ID).HashID }
+func (asset asset) GetHashID() types.ID {
+	return assetIDFromInterface(asset.ID).HashID
+}
 
-func (asset asset) GetProperties() types.Properties { return asset.Properties }
+func (asset asset) GetProperties() types.Properties {
+	return asset.Properties
+}
 
-func (asset asset) GetLock() types.Height { return asset.Lock }
+func (asset asset) GetLock() types.Height {
+	return asset.Lock
+}
 
 func (asset asset) CanSend(currentHeight types.Height) bool {
 	return currentHeight.IsGraterThat(asset.Lock)
 }
 
-func (asset asset) GetBurn() types.Height { return asset.Burn }
+func (asset asset) GetBurn() types.Height {
+	return asset.Burn
+}
 
 func (asset asset) CanBurn(currentHeight types.Height) bool {
 	return currentHeight.IsGraterThat(asset.Burn)
