d43ee3d6c6068db435ee273e68fe52636f99c528
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-08 02:59:56 +0530

    Removing unused UnknownMessage and UnknownQuery errors.

--- modules/assets/constants/error.go
+++ modules/assets/constants/error.go
@@ -2,9 +2,7 @@ package constants
 
 import "github.com/cosmos/cosmos-sdk/types/errors"
 
-var UnknownMessage = errors.Register(ModuleName, 001, "UnknownMessage")
 var IncorrectMessage = errors.Register(ModuleName, 002, "IncorrectMessage")
-var UnknownQuery = errors.Register(ModuleName, 101, "UnknownQuery")
 var EntityNotFound = errors.Register(ModuleName, 103, "EntityNotFound")
 var EntityAlreadyExists = errors.Register(ModuleName, 104, "EntityAlreadyExists")
 var BurnNotAllowed = errors.Register(ModuleName, 105, "BurnNotAllowed")
