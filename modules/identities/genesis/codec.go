222439160384c8cb05d6f03ed2a5c50de7091d4e
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-13 18:23:40 +0530

    Defining identities module genesis state.

---
+++ modules/identities/genesis/codec.go
@@ -0,0 +1,18 @@
+package genesis
+
+import (
+	"fmt"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/persistenceOne/persistenceSDK/modules/identities/constants"
+)
+
+func (genesisState) RegisterCodec(codec *codec.Codec) {
+	codec.RegisterConcrete(genesisState{}, fmt.Sprintf("/%v/%v", constants.ModuleName, "genesisState"), nil)
+}
+
+var packageCodec = codec.New()
+
+func init() {
+	GenesisState.RegisterCodec(packageCodec)
+	packageCodec.Seal()
+}
