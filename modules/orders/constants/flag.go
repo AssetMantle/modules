b9ed406a39a1f84522476ac537cb2ccb018e29a1
puneet2019 <puneet@cosmiclabs.co>
2020-07-13 19:06:49 +0530

    added mint transaction for order with coins.

--- modules/orders/constants/flag.go
+++ modules/orders/constants/flag.go
@@ -2,13 +2,7 @@ package constants
 
 import "github.com/persistenceOne/persistenceSDK/types"
 
-var AssetID = types.NewCLIFlag("assetID", "", "AssetID")
-var ChainID = types.NewCLIFlag("chainID", "", "GetChainID")
-var MaintainersID = types.NewCLIFlag("maintainersID", "", "GetMaintainersID")
-var ClassificationID = types.NewCLIFlag("classificationID", "", "GetClassificationID")
-var Properties = types.NewCLIFlag("properties", "", "GetProperties")
-var Lock = types.NewCLIFlag("lock", int64(-1), "Lock")
-var Burn = types.NewCLIFlag("burn", int64(-1), "Burn")
+var OrderID = types.NewCLIFlag("orderID", "", "AssetID")
 
 var BuyCoinDenom = types.NewCLIFlag("buyDemon", "commit", "buycoindemon")
 var BuyCoinAmount = types.NewCLIFlag("buyAmount", int64(1), "buycoinadmoun")
