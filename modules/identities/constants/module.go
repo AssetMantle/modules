db8b00e20e9bea7609aafa53ea2b044a96a74e9d
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-14 17:47:12 +0530

    Retiring module module constants.

--- modules/identities/constants/module.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-const ModuleName = "identities"
-const DefaultParamspace = "identities"
-const StoreKey = "identities"
-
-const IDSeparator = "-"
-
-var StoreKeyPrefix = []byte{0x12}
