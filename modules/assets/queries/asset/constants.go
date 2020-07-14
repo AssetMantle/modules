12c1657a26779e5e586eb9a4107f49a2de63f81f
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-14 18:02:44 +0530

    Defining query constants.

---
+++ modules/assets/queries/asset/constants.go
@@ -0,0 +1,8 @@
+package asset
+
+import "github.com/persistenceOne/persistenceSDK/modules/assets/mapper"
+
+const QueryName = "assets"
+const QueryRoute = mapper.QueryRoute + "/" + QueryName
+const QueryShort = ""
+const QueryLong = ""
