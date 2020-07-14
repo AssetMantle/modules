2cbc7d3067537b99669989d0f18bbeb24058466d
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-14 17:49:15 +0530

    Retiring module project constants.

---
+++ constants/project.go
@@ -0,0 +1,10 @@
+package constants
+
+const ProjectName = "xprt"
+const ProjectRoute = "/" + ProjectName
+
+const IDSeparator = "-"
+const PropertiesSeparator = ","
+const TraitIDAndPropertySeparator = ":"
+
+const MaxTraitCount = 22
