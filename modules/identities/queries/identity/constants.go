4c497a56b2ff84837e79ae6644f865fe5004467b
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-15 17:02:14 +0530

    Correcting identities imports.

--- modules/identities/queries/identity/constants.go
+++ modules/identities/queries/identity/constants.go
@@ -1,6 +1,6 @@
 package identity
 
-import "github.com/persistenceOne/persistenceSDK/modules/assets/mapper"
+import "github.com/persistenceOne/persistenceSDK/modules/identities/mapper"
 
 const QueryName = "identities"
 const QueryRoute = mapper.QueryRoute + "/" + QueryName
