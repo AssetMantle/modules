f2407e1c8a674c220cd41774de436ce0a9ff5758
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-16 17:30:44 +0530

    Defining unprovision transaction constants.

---
+++ modules/identities/transactions/unprovision/constants.go
@@ -0,0 +1,8 @@
+package unprovision
+
+import "github.com/persistenceOne/persistenceSDK/modules/identities/mapper"
+
+const TransactionName = "unprovision"
+const TransactionRoute = mapper.TransactionRoute + "/" + TransactionName
+const TransactionShort = ""
+const TransactionLong = ""
