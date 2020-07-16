ef3728736818fcd7883182e7664bca74844d8a38
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-16 13:23:15 +0530

    Defining add identity tranasction constants.

---
+++ modules/identities/transactions/add/constants.go
@@ -0,0 +1,8 @@
+package add
+
+import "github.com/persistenceOne/persistenceSDK/modules/identities/mapper"
+
+const TransactionName = "add"
+const TransactionRoute = mapper.TransactionRoute + "/" + TransactionName
+const TransactionShort = ""
+const TransactionLong = ""
