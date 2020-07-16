db2a73b6dbb2c58aa1cd8444b3c6b8a3d3d9d2d5
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-16 18:08:10 +0530

    Defining unprovision transaction keeper.

---
+++ modules/identities/transactions/unprovision/keeper.go
@@ -0,0 +1,30 @@
+package unprovision
+
+import (
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/persistenceOne/persistenceSDK/constants"
+	"github.com/persistenceOne/persistenceSDK/modules/identities/mapper"
+	"github.com/persistenceOne/persistenceSDK/types"
+)
+
+type transactionKeeper struct {
+	mapper types.Mapper
+}
+
+var _ types.TransactionKeeper = (*transactionKeeper)(nil)
+
+func (transactionKeeper transactionKeeper) Transact(context sdkTypes.Context, msg sdkTypes.Msg) error {
+	message := messageFromInterface(msg)
+	identityID := message.IdentityID
+	identities := mapper.NewIdentities(transactionKeeper.mapper, context).Fetch(identityID)
+	identity := identities.Get(identityID)
+	if identity == nil {
+		return constants.EntityNotFound
+	}
+	identities.Mutate(identity.UnprovisionAddress(message.To))
+	return nil
+}
+
+func initializeTransactionKeeper(mapper types.Mapper, externalKeepers []interface{}) types.TransactionKeeper {
+	return transactionKeeper{mapper: mapper}
+}
