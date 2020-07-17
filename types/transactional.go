b18aa6e95c2d2d9e59e376195c752545b95ec934
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-17 17:11:43 +0530

    Defining transactional interface.

---
+++ types/transactional.go
@@ -0,0 +1,13 @@
+package types
+
+import (
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+)
+
+type Transactional interface {
+	Splittable
+	Send(sdkTypes.Dec) Transactional
+	Receive(sdkTypes.Dec) Transactional
+
+	CanSend(sdkTypes.Dec) bool
+}
