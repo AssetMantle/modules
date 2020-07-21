062b2382d24eea8509926c2ef5612fea7622f8bc
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-21 17:43:04 +0530

    Defining split query request.

---
+++ modules/splits/queries/split/request.go
@@ -0,0 +1,39 @@
+package split
+
+import (
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/persistenceOne/persistenceSDK/constants"
+	"github.com/persistenceOne/persistenceSDK/types/schema"
+	"github.com/persistenceOne/persistenceSDK/types/utility"
+)
+
+type queryRequest struct {
+	SplitID schema.ID
+}
+
+var _ utility.QueryRequest = (*queryRequest)(nil)
+
+func (QueryRequest queryRequest) FromCLI(cliCommand utility.CLICommand, _ context.CLIContext) utility.QueryRequest {
+	return newQueryRequest(schema.NewID(cliCommand.ReadString(constants.SplitID)))
+}
+
+func (QueryRequest queryRequest) FromMap(vars map[string]string) utility.QueryRequest {
+	return newQueryRequest(schema.NewID(vars[constants.SplitID.GetName()]))
+}
+
+func queryRequestPrototype() utility.QueryRequest {
+	return queryRequest{}
+}
+
+func queryRequestFromInterface(QueryRequest utility.QueryRequest) queryRequest {
+	switch value := QueryRequest.(type) {
+	case queryRequest:
+		return value
+	default:
+		return queryRequest{}
+	}
+}
+
+func newQueryRequest(splitID schema.ID) utility.QueryRequest {
+	return queryRequest{SplitID: splitID}
+}
