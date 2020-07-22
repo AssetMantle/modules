11355c67262585dda033a5d6fffc9e1c706f4c03
Deepanshu Tripathi <deepanshu@persistence.one>
2020-07-22 13:13:59 +0530

    Defining splits send transaction.

---
+++ modules/splits/transactions/send/transaction.go
@@ -0,0 +1,19 @@
+package send
+
+import (
+	"github.com/persistenceOne/persistenceSDK/constants"
+	"github.com/persistenceOne/persistenceSDK/modules/assets/mapper"
+	"github.com/persistenceOne/persistenceSDK/types/utility"
+)
+
+var Transaction = utility.NewTransaction(
+	mapper.ModuleName,
+	TransactionName,
+	TransactionRoute,
+	TransactionShort,
+	TransactionLong,
+	registerCodec,
+	initializeTransactionKeeper,
+	requestPrototype,
+	[]utility.CLIFlag{constants.AssetID, constants.Properties, constants.Lock, constants.Burn},
+)
