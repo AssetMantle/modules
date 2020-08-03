dcc6f60746480cc52adb24a75d8559354223a2c0
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-03 21:50:31 +0530

    Adding burn splits auxiliary to burn asset transaction.

--- modules/assets/transactions/burn/keeper.go
+++ modules/assets/transactions/burn/keeper.go
@@ -4,12 +4,14 @@ import (
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/persistenceOne/persistenceSDK/constants"
 	"github.com/persistenceOne/persistenceSDK/modules/assets/mapper"
+	"github.com/persistenceOne/persistenceSDK/modules/splits/auxiliaries/burn"
 	"github.com/persistenceOne/persistenceSDK/schema/helpers"
 	"github.com/persistenceOne/persistenceSDK/schema/types/base"
 )
 
 type transactionKeeper struct {
-	mapper helpers.Mapper
+	mapper        helpers.Mapper
+	burnAuxiliary helpers.Auxiliary
 }
 
 var _ helpers.TransactionKeeper = (*transactionKeeper)(nil)
@@ -24,10 +26,23 @@ func (transactionKeeper transactionKeeper) Transact(context sdkTypes.Context, ms
 	if !asset.CanBurn(base.NewHeight(context.BlockHeight())) {
 		return constants.DeletionNotAllowed
 	}
+	if Error := transactionKeeper.burnAuxiliary.GetKeeper().Help(context, burn.NewAuxiliaryRequest(message.FromID, message.AssetID, sdkTypes.OneDec())); Error != nil {
+		return Error
+	}
 	assets.Remove(asset)
 	return nil
 }
 
-func initializeTransactionKeeper(mapper helpers.Mapper, _ []interface{}) helpers.TransactionKeeper {
-	return transactionKeeper{mapper: mapper}
+func initializeTransactionKeeper(mapper helpers.Mapper, auxiliaries []interface{}) helpers.TransactionKeeper {
+	transactionKeeper := transactionKeeper{mapper: mapper}
+	for _, auxiliary := range auxiliaries {
+		switch value := auxiliary.(type) {
+		case helpers.Auxiliary:
+			switch value.GetName() {
+			case burn.Auxiliary.GetName():
+				transactionKeeper.burnAuxiliary = value
+			}
+		}
+	}
+	return transactionKeeper
 }
---
+++ modules/splits/auxiliaries/burn/request.go
@@ -0,0 +1,33 @@
+package burn
+
+import (
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/persistenceOne/persistenceSDK/schema/helpers"
+	"github.com/persistenceOne/persistenceSDK/schema/types"
+	"github.com/persistenceOne/persistenceSDK/schema/types/base"
+)
+
+type auxiliaryRequest struct {
+	OwnerID   types.ID     `json:"ownerID" valid:"required~required field ownerID missing"`
+	OwnableID types.ID     `json:"ownableID" valid:"required~required field ownableID missing"`
+	Split     sdkTypes.Dec `json:"split" valid:"required~required field assetID missing matches(^[0-9]$)~invalid field split"`
+}
+
+var _ helpers.AuxiliaryRequest = (*auxiliaryRequest)(nil)
+
+func auxiliaryRequestFromInterface(AuxiliaryRequest helpers.AuxiliaryRequest) auxiliaryRequest {
+	switch value := AuxiliaryRequest.(type) {
+	case auxiliaryRequest:
+		return value
+	default:
+		return auxiliaryRequest{}
+	}
+}
+
+func NewAuxiliaryRequest(ownerID types.ID, ownableID types.ID, split sdkTypes.Dec) helpers.AuxiliaryRequest {
+	return auxiliaryRequest{
+		OwnerID:   base.NewID(ownerID.String()),
+		OwnableID: base.NewID(ownableID.String()),
+		Split:     split,
+	}
+}
