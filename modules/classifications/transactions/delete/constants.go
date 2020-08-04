0668df75a9337192daa46d1fbda1942c4f7f2a3c
avkr003 <abhinav@persistence.one>
2020-08-04 13:39:48 +0530

    Adding delete transaction for classification module.

---
+++ modules/classifications/transactions/delete/constants.go
@@ -0,0 +1,8 @@
+package create
+
+import "github.com/persistenceOne/persistenceSDK/modules/classifications/mapper"
+
+const TransactionName = "delete"
+const TransactionRoute = mapper.TransactionRoute + "/" + TransactionName
+const TransactionShort = ""
+const TransactionLong = ""
