da170cd2ee85432560e84873a793e9f3ecfbca1b
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-06 14:48:56 +0530

    Cleaning up code.

--- constants/errors.go
+++ constants/errors.go
@@ -1,3 +1,8 @@
+/*
+ *  Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package constants
 
 import "github.com/cosmos/cosmos-sdk/types/errors"
@@ -6,5 +11,4 @@ var DeletionNotAllowed = errors.Register(ProjectRoute, 101, "DeletionNotAllowed"
 var EntityAlreadyExists = errors.Register(ProjectRoute, 102, "EntityAlreadyExists")
 var EntityNotFound = errors.Register(ProjectRoute, 103, "EntityNotFound")
 var IncorrectMessage = errors.Register(ProjectRoute, 104, "IncorrectMessage")
-var MutationNotAllowed = errors.Register(ProjectRoute, 105, "MutationNotAllowed")
 var NotAuthorized = errors.Register(ProjectRoute, 106, "NotAuthorized")
