5a8738b7c537dfe9e5779a56ed7c00cf99270ed1
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-06 18:30:38 +0530

    Updating maintainer.go interface definition.

--- schema/mappables/maintainer.go
+++ schema/mappables/maintainer.go
@@ -6,25 +6,18 @@
 package mappables
 
 import (
-	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/persistenceOne/persistenceSDK/schema/traits"
 	"github.com/persistenceOne/persistenceSDK/schema/types"
 )
 
 type Maintainer interface {
-	Sting() string
-
-	GetAddress() sdkTypes.AccAddress
-	GetID() types.ID
-
-	CanMutateMaintainersProperty(types.ID) bool
+	GetIdentityID() types.ID
+	GetMaintainedID() types.ID
 
 	CanAddMaintainer() bool
 	CanRemoveMaintainer() bool
 	CanMutateMaintainer() bool
 
-	CanMutateLock() bool
-	CanMutateBurn() bool
-	CanMutateTrait(types.ID) bool
+	MaintainsTrait(types.ID) bool
 	traits.Mappable
 }
