b6a3e285b1b379f408d88e2372182dcefac6e0c4
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-06 16:32:55 +0530

    Cleaning up code.

--- constants/errors.go
+++ constants/errors.go
@@ -1,7 +1,7 @@
 /*
- *  Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
- * SPDX-License-Identifier: Apache-2.0
- */
+  Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
 
 package constants
 
