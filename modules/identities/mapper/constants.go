d5561b088b38afa9c77c70f13ff3d6df283a4c48
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-06 16:41:37 +0530

    Updating source headers.

--- modules/identities/mapper/constants.go
+++ modules/identities/mapper/constants.go
@@ -1,3 +1,8 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
 package mapper
 
 import "github.com/persistenceOne/persistenceSDK/constants"
