824530ca55d77d333957afe5ebbcbabce9310aee
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-10 13:19:38 +0530

    Defining metas module object.

---
+++ modules/metas/module.go
@@ -0,0 +1,28 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package metas
+
+import (
+	"github.com/persistenceOne/persistenceSDK/modules/metas/auxiliaries/initialize"
+	"github.com/persistenceOne/persistenceSDK/modules/metas/genesis"
+	"github.com/persistenceOne/persistenceSDK/modules/metas/mapper"
+	"github.com/persistenceOne/persistenceSDK/modules/metas/queries/meta"
+	"github.com/persistenceOne/persistenceSDK/modules/metas/transactions/reveal"
+	"github.com/persistenceOne/persistenceSDK/schema/helpers"
+	"github.com/persistenceOne/persistenceSDK/schema/helpers/base"
+)
+
+var Module = base.NewModule(
+	mapper.ModuleName,
+	mapper.DefaultParamspace,
+	mapper.QueryRoute,
+	mapper.TransactionRoute,
+	genesis.GenesisState,
+	mapper.Mapper,
+	[]helpers.Auxiliary{initialize.Auxiliary},
+	[]helpers.Query{meta.Query},
+	[]helpers.Transaction{reveal.Transaction},
+)
