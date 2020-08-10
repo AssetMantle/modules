4289bd76fcd0e9cc8f8be92e3888c4e03f8fdb8a
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-10 13:17:58 +0530

    Adding meta reveal transaction method.

---
+++ modules/metas/transactions/reveal/transaction.go
@@ -0,0 +1,25 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package reveal
+
+import (
+	"github.com/persistenceOne/persistenceSDK/constants"
+	"github.com/persistenceOne/persistenceSDK/modules/metas/mapper"
+	"github.com/persistenceOne/persistenceSDK/schema/helpers"
+	"github.com/persistenceOne/persistenceSDK/schema/helpers/base"
+)
+
+var Transaction = base.NewTransaction(
+	mapper.ModuleName,
+	TransactionName,
+	TransactionRoute,
+	TransactionShort,
+	TransactionLong,
+	registerCodec,
+	revealTransactionKeeper,
+	requestPrototype,
+	[]helpers.CLIFlag{constants.FromID, constants.Data},
+)
