596c9e207e7df336b38ce460ffd2dbb1c7e91789
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-10 13:14:44 +0530

    Adding register codec method.

---
+++ modules/metas/mapper/codec.go
@@ -0,0 +1,25 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package mapper
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/persistenceOne/persistenceSDK/schema"
+)
+
+func registerCodec(codec *codec.Codec) {
+	codec.RegisterConcrete(metas{}, ModuleRoute+"/"+"metas", nil)
+	codec.RegisterConcrete(text{}, ModuleRoute+"/"+"text", nil)
+	codec.RegisterConcrete(metaID{}, ModuleRoute+"/"+"metaID", nil)
+}
+
+var packageCodec = codec.New()
+
+func init() {
+	registerCodec(packageCodec)
+	schema.RegisterCodec(packageCodec)
+	packageCodec.Seal()
+}
