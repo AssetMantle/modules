7504912b6f05ff06f0b957bd5bb581bb9bab4063
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-13 09:57:16 +0530

    Defining transaction response helper interface.

---
+++ schema/helpers/transactionResponse.go
@@ -0,0 +1,10 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package helpers
+
+type TransactionResponse interface {
+	Response
+}
