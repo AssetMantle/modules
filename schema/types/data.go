591ab2990f490d2af84790dd999ea919daef39e3
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-17 04:48:30 +0530

    Defining data interface.

---
+++ schema/types/data.go
@@ -0,0 +1,18 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package types
+
+import sdkTypes "github.com/cosmos/cosmos-sdk/types"
+
+type Data interface {
+	GenerateHash() string
+
+	AsString() (string, error)
+	AsDec() (sdkTypes.Dec, error)
+	AsHeight() (Height, error)
+
+	Get() interface{}
+}
