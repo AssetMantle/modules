0214b3cdb87e726a53f8fcfb38ac03abfeb37aab
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-17 17:45:08 +0530

    Modifying burnable and lockable interfaces to return facts instead of height.

--- schema/traits/lockable.go
+++ schema/traits/lockable.go
@@ -8,6 +8,5 @@ package traits
 import "github.com/persistenceOne/persistenceSDK/schema/types"
 
 type Lockable interface {
-	CanSend(types.Height) bool
-	GetLock() types.Height
+	GetLock() types.Fact
 }
---
+++ schema/types/base/heightData.go
@@ -0,0 +1,46 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package base
+
+import (
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/persistenceOne/persistenceSDK/constants"
+	"github.com/persistenceOne/persistenceSDK/schema/types"
+	"github.com/persistenceOne/persistenceSDK/utilities/meta"
+	"strconv"
+)
+
+type heightData struct {
+	Value types.Height `json:"value"`
+}
+
+var _ types.Data = (*heightData)(nil)
+
+func (heightData heightData) GenerateHash() string {
+	return meta.Hash(strconv.FormatInt(heightData.Value.Get(), 10))
+}
+
+func (heightData heightData) AsString() (string, error) {
+	return "", constants.EntityNotFound
+}
+
+func (heightData heightData) AsDec() (sdkTypes.Dec, error) {
+	return sdkTypes.Dec{}, constants.EntityNotFound
+}
+
+func (heightData heightData) AsHeight() (types.Height, error) {
+	return heightData.Value, nil
+}
+
+func (heightData heightData) Get() interface{} {
+	return heightData.Value
+}
+
+func NewHeightData(value types.Height) types.Data {
+	return heightData{
+		Value: value,
+	}
+}
