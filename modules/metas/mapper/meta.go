f4ca3e3d76232a239f8f5c02a95b7d89a0889458
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-18 15:43:34 +0530

    Renaming metas meta implementation struct name from text to meta.

---
+++ modules/metas/mapper/meta.go
@@ -0,0 +1,42 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package mapper
+
+import (
+	"github.com/persistenceOne/persistenceSDK/schema/mappables"
+	"github.com/persistenceOne/persistenceSDK/schema/traits"
+	"github.com/persistenceOne/persistenceSDK/schema/types"
+	"github.com/persistenceOne/persistenceSDK/schema/types/base"
+)
+
+type meta struct {
+	Data types.Data `json:"data" valid:"required field data missing"`
+}
+
+var _ mappables.Meta = (*meta)(nil)
+
+func (meta meta) GetData() types.Data { return meta.Data }
+
+func (meta meta) GetID() types.ID { return base.NewID(meta.Data.GenerateHash()) }
+
+func (meta meta) Encode() []byte {
+	return packageCodec.MustMarshalBinaryBare(meta)
+}
+
+func (meta meta) Decode(bytes []byte) traits.Mappable {
+	packageCodec.MustUnmarshalBinaryBare(bytes, &meta)
+	return meta
+}
+
+func metaPrototype() traits.Mappable {
+	return meta{}
+}
+
+func NewMeta(data types.Data) mappables.Meta {
+	return meta{
+		Data: data,
+	}
+}
