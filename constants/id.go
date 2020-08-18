b00d1b669a797b80c9bb2f109d9902ff7c1fb7b8
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-18 15:37:54 +0530

    Defining BurnID and LockID as constants.

---
+++ constants/id.go
@@ -0,0 +1,11 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package constants
+
+import "github.com/persistenceOne/persistenceSDK/schema/types/base"
+
+var BurnID = base.NewID("burn")
+var LockID = base.NewID("lock")
