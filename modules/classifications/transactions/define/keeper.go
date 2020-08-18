24eba6658af0172a16271cd5b247a76cc2b023ee
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-18 15:54:08 +0530

    Updating define classification transaction to scrub metadata from properties.

--- modules/classifications/transactions/define/keeper.go
+++ modules/classifications/transactions/define/keeper.go
@@ -10,36 +10,41 @@ import (
 	"github.com/persistenceOne/persistenceSDK/constants"
 	"github.com/persistenceOne/persistenceSDK/modules/classifications/mapper"
 	"github.com/persistenceOne/persistenceSDK/modules/identities/auxiliaries/verify"
-	"github.com/persistenceOne/persistenceSDK/modules/metas/auxiliaries/initialize"
+	"github.com/persistenceOne/persistenceSDK/modules/metas/auxiliaries/scrub"
 	"github.com/persistenceOne/persistenceSDK/schema/helpers"
 	"github.com/persistenceOne/persistenceSDK/schema/types/base"
 )
 
 type transactionKeeper struct {
-	mapper                    helpers.Mapper
-	identitiesVerifyAuxiliary helpers.Auxiliary
-	metasInitializeAuxiliary  helpers.Auxiliary
+	mapper          helpers.Mapper
+	verifyAuxiliary helpers.Auxiliary
+	scrubAuxiliary  helpers.Auxiliary
 }
 
 var _ helpers.TransactionKeeper = (*transactionKeeper)(nil)
 
 func (transactionKeeper transactionKeeper) Transact(context sdkTypes.Context, msg sdkTypes.Msg) helpers.TransactionResponse {
 	message := messageFromInterface(msg)
-	if auxiliaryResponse := transactionKeeper.identitiesVerifyAuxiliary.GetKeeper().Help(context, verify.NewAuxiliaryRequest(message.From, message.FromID)); !auxiliaryResponse.IsSuccessful() {
+	if auxiliaryResponse := transactionKeeper.verifyAuxiliary.GetKeeper().Help(context, verify.NewAuxiliaryRequest(message.From, message.FromID)); !auxiliaryResponse.IsSuccessful() {
 		return newTransactionResponse(auxiliaryResponse.GetError())
 	}
 
-	immutableTraits := base.NewImmutables(base.NewProperties(append(message.ImmutableMetaTraits.RemoveData().GetList(), message.ImmutableTraits.GetList()...)))
-	mutableTraits := base.NewMutables(base.NewProperties(append(message.MutableMetaTraits.RemoveData().GetList(), message.MutableTraits.GetList()...)))
+	scrubImmutableMetaTraitsAuxiliaryResponse, Error := scrub.ValidateResponse(transactionKeeper.scrubAuxiliary.GetKeeper().Help(context, scrub.NewAuxiliaryRequest(message.ImmutableMetaTraits)))
+	if Error != nil {
+		return newTransactionResponse(Error)
+	}
+	immutableTraits := base.NewImmutables(base.NewProperties(append(scrubImmutableMetaTraitsAuxiliaryResponse.Properties.GetList(), message.ImmutableTraits.GetList()...)))
+
+	scrubMutableMetaTraitsAuxiliaryResponse, Error := scrub.ValidateResponse(transactionKeeper.scrubAuxiliary.GetKeeper().Help(context, scrub.NewAuxiliaryRequest(message.MutableMetaTraits)))
+	if Error != nil {
+		return newTransactionResponse(Error)
+	}
+	mutableTraits := base.NewMutables(base.NewProperties(append(scrubMutableMetaTraitsAuxiliaryResponse.Properties.GetList(), message.MutableMetaTraits.GetList()...)))
 
 	classificationID := mapper.NewClassificationID(base.NewID(context.ChainID()), immutableTraits, mutableTraits)
 	classifications := mapper.NewClassifications(transactionKeeper.mapper, context).Fetch(classificationID)
 	if classifications.Get(classificationID) != nil {
 		return newTransactionResponse(constants.EntityAlreadyExists)
-	}
-
-	if auxiliaryResponse := transactionKeeper.metasInitializeAuxiliary.GetKeeper().Help(context, initialize.NewAuxiliaryRequest(message.ImmutableMetaTraits, message.MutableMetaTraits)); !auxiliaryResponse.IsSuccessful() {
-		return newTransactionResponse(auxiliaryResponse.GetError())
 	}
 
 	classifications = classifications.Add(mapper.NewClassification(classificationID, immutableTraits, mutableTraits))
@@ -52,10 +57,10 @@ func initializeTransactionKeeper(mapper helpers.Mapper, auxiliaries []interface{
 		switch value := auxiliary.(type) {
 		case helpers.Auxiliary:
 			switch value.GetName() {
+			case scrub.Auxiliary.GetName():
+				transactionKeeper.scrubAuxiliary = value
 			case verify.Auxiliary.GetName():
-				transactionKeeper.identitiesVerifyAuxiliary = value
-			case initialize.Auxiliary.GetName():
-				transactionKeeper.metasInitializeAuxiliary = value
+				transactionKeeper.verifyAuxiliary = value
 			}
 		}
 	}
