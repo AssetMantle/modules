0a54903922259a7fecd3a1f0694a5051fa573c24
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-27 03:57:15 +0530

    Adding nub/basic issue identity transaction.

---
+++ modules/identities/transactions/nub/message.go
@@ -0,0 +1,53 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package nub
+
+import (
+	"github.com/asaskevich/govalidator"
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/errors"
+	xprtErrors "github.com/persistenceOne/persistenceSDK/constants/errors"
+	"github.com/persistenceOne/persistenceSDK/schema/types"
+)
+
+type message struct {
+	From  sdkTypes.AccAddress `json:"from" valid:"required~required field from missing matches(^commit[a-z0-9]{39}$)~invalid field from"`
+	NubID types.ID            `json:"nubID" valid:"required~required field nubID missing"`
+}
+
+var _ sdkTypes.Msg = message{}
+
+func (message message) Route() string { return Transaction.GetModuleName() }
+func (message message) Type() string  { return Transaction.GetName() }
+func (message message) ValidateBasic() error {
+	var _, Error = govalidator.ValidateStruct(message)
+	if Error != nil {
+		return errors.Wrap(xprtErrors.IncorrectMessage, Error.Error())
+	}
+	return nil
+}
+func (message message) GetSignBytes() []byte {
+	return sdkTypes.MustSortJSON(packageCodec.MustMarshalJSON(message))
+}
+func (message message) GetSigners() []sdkTypes.AccAddress {
+	return []sdkTypes.AccAddress{message.From}
+}
+
+func messageFromInterface(msg sdkTypes.Msg) message {
+	switch value := msg.(type) {
+	case message:
+		return value
+	default:
+		return message{}
+	}
+}
+
+func newMessage(from sdkTypes.AccAddress, nubID types.ID) sdkTypes.Msg {
+	return message{
+		From:  from,
+		NubID: nubID,
+	}
+}
