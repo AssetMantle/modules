3bd9b451bccab604e8636e2ad20f34e709f0bbae
Deepanshu Tripathi <deepanshu@persistence.one>
2020-08-26 13:17:35 +0530

    Refactoring maintainer struct's maintainedTraits type to mutables.

--- modules/maintainers/mapper/maintainer.go
+++ modules/maintainers/mapper/maintainer.go
@@ -12,11 +12,11 @@ import (
 )
 
 type maintainer struct {
-	ID               types.ID         `json:"id" valid:"required field id missing"`
-	MaintainedTraits types.Properties `json:"maintainedTraits" valid:"required field maintainedTraits missing"`
-	AddMaintainer    bool             `json:"addMaintainer" valid:"required field addMaintainer missing"`
-	RemoveMaintainer bool             `json:"removeMaintainer" valid:"required field removeMaintainer missing"`
-	MutateMaintainer bool             `json:"mutateMaintainer" valid:"required field mutateMaintainer missing"`
+	ID               types.ID       `json:"id" valid:"required field id missing"`
+	MaintainedTraits types.Mutables `json:"maintainedTraits" valid:"required field maintainedTraits missing"`
+	AddMaintainer    bool           `json:"addMaintainer" valid:"required field addMaintainer missing"`
+	RemoveMaintainer bool           `json:"removeMaintainer" valid:"required field removeMaintainer missing"`
+	MutateMaintainer bool           `json:"mutateMaintainer" valid:"required field mutateMaintainer missing"`
 }
 
 var _ mappables.Maintainer = (*maintainer)(nil)
@@ -32,7 +32,7 @@ func (maintainer maintainer) CanAddMaintainer() bool    { return maintainer.AddM
 func (maintainer maintainer) CanRemoveMaintainer() bool { return maintainer.RemoveMaintainer }
 func (maintainer maintainer) CanMutateMaintainer() bool { return maintainer.MutateMaintainer }
 func (maintainer maintainer) MaintainsTrait(id types.ID) bool {
-	for _, trait := range maintainer.MaintainedTraits.GetList() {
+	for _, trait := range maintainer.MaintainedTraits.Get().GetList() {
 		if trait.GetID().Compare(id) == 0 {
 			return true
 		}
@@ -49,7 +49,7 @@ func (maintainer maintainer) Decode(bytes []byte) traits.Mappable {
 func maintainerPrototype() traits.Mappable {
 	return maintainer{}
 }
-func NewMaintainer(ID types.ID, maintainedTraits types.Properties, addMaintainer bool, removeMaintainer bool, mutateMaintainer bool) mappables.Maintainer {
+func NewMaintainer(ID types.ID, maintainedTraits types.Mutables, addMaintainer bool, removeMaintainer bool, mutateMaintainer bool) mappables.Maintainer {
 	return maintainer{
 		ID:               ID,
 		MaintainedTraits: maintainedTraits,
