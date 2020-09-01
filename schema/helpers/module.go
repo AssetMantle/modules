7f6b62044fd412192c990b23378f183c528f8968
Deepanshu Tripathi <deepanshu@persistence.one>
2020-09-01 12:04:35 +0530

    Adding AppModuleSimulation to helpers Module interface.

--- schema/helpers/module.go
+++ schema/helpers/module.go
@@ -14,6 +14,7 @@ import (
 type Module interface {
 	sdkTypesModule.AppModuleBasic
 	sdkTypesModule.AppModule
+	sdkTypesModule.AppModuleSimulation
 
 	GetKVStoreKey() *sdkTypes.KVStoreKey
 	GetDefaultParamspace() string
