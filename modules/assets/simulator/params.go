6568eca85b5a9366a19c4535eba7f8fdb844dec9
Deepanshu Tripathi <deepanshu@persistence.one>
2020-09-02 21:21:09 +0530

    Defining assets simulator param change methods.

---
+++ modules/assets/simulator/params.go
@@ -0,0 +1,15 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package simulator
+
+import (
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+	"math/rand"
+)
+
+func ParamChangeList(_ *rand.Rand) []simulation.ParamChange {
+	return []simulation.ParamChange{}
+}
