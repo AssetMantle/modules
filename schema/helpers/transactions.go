d9bcb6c1f72b5c06ee7e76a9640218836be4b6be
Deepanshu Tripathi <deepanshu@persistence.one>
2020-09-08 04:10:12 +0530

    Defining transactions interface.

---
+++ schema/helpers/transactions.go
@@ -0,0 +1,12 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package helpers
+
+type Transactions interface {
+	Get(string) Transaction
+
+	GetList() []Transaction
+}
