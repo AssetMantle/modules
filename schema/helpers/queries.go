1d5af6998d423a56a59e01a821dcfd1e48d694bd
Deepanshu Tripathi <deepanshu@persistence.one>
2020-09-08 04:10:18 +0530

    Defining queries interface.

---
+++ schema/helpers/queries.go
@@ -0,0 +1,11 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package helpers
+
+type Queries interface {
+	Get(string) Query
+	GetList() []Query
+}
