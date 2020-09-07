010592dedaee01894ad285f82d02f21572d01a2f
Deepanshu Tripathi <deepanshu@persistence.one>
2020-09-08 04:10:29 +0530

    Defining auxiliaries interface.

---
+++ schema/helpers/auxiliaries.go
@@ -0,0 +1,11 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package helpers
+
+type Auxiliaries interface {
+	Get(string) Auxiliary
+	GetList() []Auxiliary
+}
