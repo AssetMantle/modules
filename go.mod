9d3096ed6a490e0f85efa8462300569d6fb2cec6
puneet2019 <puneet@persistence.one>
2020-09-28 16:17:16 +0530

    adding mocks for auxiliaries and adding test cases.

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.6.1
 	github.com/tendermint/tendermint v0.33.6
 	github.com/tendermint/tm-db v0.5.1
 	honnef.co/go/tools v0.0.1-2019.2.3
---
+++ modules/identities/internal/transactions/nub/keeper_test.go
@@ -0,0 +1,110 @@
+package nub
+
+import (
+	"github.com/cosmos/cosmos-sdk/store"
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/persistenceOne/persistenceSDK/constants/errors"
+	"github.com/persistenceOne/persistenceSDK/modules/classifications/auxiliaries/define"
+	"github.com/persistenceOne/persistenceSDK/modules/identities/internal/mapper"
+	"github.com/persistenceOne/persistenceSDK/modules/metas/auxiliaries/scrub"
+	"github.com/persistenceOne/persistenceSDK/schema/helpers"
+	"github.com/persistenceOne/persistenceSDK/schema/types/base"
+	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
+	"reflect"
+	"testing"
+)
+
+type TestKeepers struct {
+	IdentitiesKeeper helpers.TransactionKeeper
+}
+
+func CreateTestInput(t *testing.T) (sdkTypes.Context, TestKeepers) {
+
+	keyIdentity := mapper.Mapper.GetKVStoreKey()
+
+	db := dbm.NewMemDB()
+	ms := store.NewCommitMultiStore(db)
+	ms.MountStoreWithDB(keyIdentity, sdkTypes.StoreTypeIAVL, db)
+	err := ms.LoadLatestVersion()
+	require.Nil(t, err)
+
+	ctx := sdkTypes.NewContext(ms, abci.Header{
+		ChainID: "test",
+	}, false, log.NewNopLogger())
+
+	scrub.AuxiliaryMock.InitializeKeeper(mapper.Mapper)
+	define.AuxiliaryMock.InitializeKeeper(mapper.Mapper)
+	keepers := TestKeepers{
+		IdentitiesKeeper: initializeTransactionKeeper(mapper.Mapper,
+			[]interface{}{scrub.AuxiliaryMock,
+				define.AuxiliaryMock}),
+	}
+
+	return ctx, keepers
+}
+
+func Test_transactionKeeper_Transact(t *testing.T) {
+
+	ctx, keepers := CreateTestInput(t)
+
+	defaultIdentityID := mapper.NewIdentityID(base.NewID("test.cGn3HMW8M3t5gMDv-wXa9sseHnA="), base.NewID("d0Jhri_bOd3EEPXpyPUpNpGiQ1U="))
+	mapper.NewIdentities(mapper.Mapper, ctx).Add(mapper.NewIdentity(defaultIdentityID, []sdkTypes.AccAddress{sdkTypes.AccAddress("addr")},
+		[]sdkTypes.AccAddress{}, base.NewImmutables(base.NewProperties()), base.NewMutables(base.NewProperties())))
+
+	type fields struct {
+		keeper helpers.TransactionKeeper
+	}
+
+	type args struct {
+		context sdkTypes.Context
+		msg     sdkTypes.Msg
+	}
+
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   helpers.TransactionResponse
+	}{
+		{
+			name:   "Expected Case",
+			fields: fields{keepers.IdentitiesKeeper},
+			args: args{
+				context: ctx,
+				msg: message{
+					From:  sdkTypes.AccAddress("addr"),
+					NubID: base.NewID("nubID1"),
+				},
+			},
+			want: transactionResponse{
+				Success: true,
+				Error:   nil,
+			},
+		},
+		{
+			name:   "Duplicate nub identity addition.",
+			fields: fields{keepers.IdentitiesKeeper},
+			args: args{
+				context: ctx,
+				msg: message{
+					From:  sdkTypes.AccAddress("addr"),
+					NubID: base.NewID("nubID"),
+				},
+			},
+			want: transactionResponse{
+				Success: false,
+				Error:   errors.EntityAlreadyExists,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.fields.keeper.Transact(tt.args.context, tt.args.msg); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Transact() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
