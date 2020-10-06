9cc87984703982a6ba08accce182edec4ff8c1f4
puneet2019 <puneet@persistence.one>
2020-10-06 19:13:46 +0530

    renaming splits send keeper_test.go.

---
+++ modules/splits/internal/transactions/send/keeper_test.go
@@ -0,0 +1,104 @@
+package send
+
+import (
+	"github.com/cosmos/cosmos-sdk/store"
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/persistenceOne/persistenceSDK/constants/errors"
+	"github.com/persistenceOne/persistenceSDK/modules/identities/auxiliaries/verify"
+	"github.com/persistenceOne/persistenceSDK/modules/splits/internal/mapper"
+	"github.com/persistenceOne/persistenceSDK/modules/splits/internal/parameters"
+	"github.com/persistenceOne/persistenceSDK/schema/helpers"
+	"github.com/persistenceOne/persistenceSDK/schema/types/base"
+	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
+	"reflect"
+	"testing"
+)
+
+type TestKeepers struct {
+	SplitsKeeper helpers.TransactionKeeper
+}
+
+func CreateTestInput(t *testing.T) (sdkTypes.Context, TestKeepers) {
+
+	keyIdentity := mapper.Mapper.GetKVStoreKey()
+
+	db := dbm.NewMemDB()
+	ms := store.NewCommitMultiStore(db)
+	ms.MountStoreWithDB(keyIdentity, sdkTypes.StoreTypeIAVL, db)
+	err := ms.LoadLatestVersion()
+	require.Nil(t, err)
+
+	ctx := sdkTypes.NewContext(ms, abci.Header{
+		ChainID: "test",
+	}, false, log.NewNopLogger())
+
+	verify.AuxiliaryMock.InitializeKeeper(mapper.Mapper, parameters.Prototype)
+	keepers := TestKeepers{
+		SplitsKeeper: initializeTransactionKeeper(mapper.Mapper, parameters.Prototype, []interface{}{verify.AuxiliaryMock}),
+	}
+
+	return ctx, keepers
+}
+
+func Test_transactionKeeper_Transact(t *testing.T) {
+
+	ctx, keepers := CreateTestInput(t)
+	defaultAddr := sdkTypes.AccAddress("addr")
+	verifyMockErrorAddress := sdkTypes.AccAddress("verifyError")
+
+	fromID := base.NewID("fromID")
+	toID := base.NewID("toID")
+	ownableID := base.NewID("stake")
+
+	mapper.NewSplits(mapper.Mapper, ctx).Add(mapper.NewSplit(mapper.NewSplitID(fromID, ownableID), sdkTypes.NewDec(100)))
+
+	t.Run("PositiveCase", func(t *testing.T) {
+		want := newTransactionResponse(nil)
+		if got := keepers.SplitsKeeper.Transact(ctx, newMessage(defaultAddr, fromID, toID, ownableID, sdkTypes.NewDec(1))); !reflect.DeepEqual(got, want) {
+			t.Errorf("Transact() = %v, want %v", got, want)
+		}
+	})
+
+	t.Run("Positive Case-Send All splits", func(t *testing.T) {
+		want := newTransactionResponse(nil)
+		if got := keepers.SplitsKeeper.Transact(ctx, newMessage(defaultAddr, toID, fromID, ownableID, sdkTypes.NewDec(1))); !reflect.DeepEqual(got, want) {
+			t.Errorf("Transact() = %v, want %v", got, want)
+		}
+	})
+
+	t.Run("NegativeCase-Verify Identity Failure", func(t *testing.T) {
+		t.Parallel()
+		want := newTransactionResponse(errors.MockError)
+		if got := keepers.SplitsKeeper.Transact(ctx, newMessage(verifyMockErrorAddress, fromID, toID, ownableID, sdkTypes.NewDec(1))); !reflect.DeepEqual(got, want) {
+			t.Errorf("Transact() = %v, want %v", got, want)
+		}
+	})
+
+	t.Run("NegativeCase-Negative Split exchange", func(t *testing.T) {
+		t.Parallel()
+		want := newTransactionResponse(errors.NotAuthorized)
+		if got := keepers.SplitsKeeper.Transact(ctx, newMessage(defaultAddr, fromID, toID, ownableID, sdkTypes.NewDec(-1))); !reflect.DeepEqual(got, want) {
+			t.Errorf("Transact() = %v, want %v", got, want)
+		}
+	})
+
+	t.Run("NegativeCase-Split not found", func(t *testing.T) {
+		t.Parallel()
+		want := newTransactionResponse(errors.EntityNotFound)
+		if got := keepers.SplitsKeeper.Transact(ctx, newMessage(defaultAddr, base.NewID("fakeFromID"), toID, ownableID, sdkTypes.NewDec(1))); !reflect.DeepEqual(got, want) {
+			t.Errorf("Transact() = %v, want %v", got, want)
+		}
+	})
+
+	t.Run("NegativeCase-Send More than available splits", func(t *testing.T) {
+		t.Parallel()
+		want := newTransactionResponse(errors.NotAuthorized)
+		if got := keepers.SplitsKeeper.Transact(ctx, newMessage(defaultAddr, fromID, toID, ownableID, sdkTypes.NewDec(101))); !reflect.DeepEqual(got, want) {
+			t.Errorf("Transact() = %v, want %v", got, want)
+		}
+	})
+
+}
