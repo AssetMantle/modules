dbc24c22e74cae810d9bf89832cf1cf77bd3f882
Deepanshu Tripathi <deepanshu@persistence.one>
2020-10-06 23:18:40 +0530

    Removing pointer manipulation for auxiliary InitializeKeeper method to avoid side effects.

--- schema/helpers/auxiliary.go
+++ schema/helpers/auxiliary.go
@@ -8,5 +8,5 @@ package helpers
 type Auxiliary interface {
 	GetName() string
 	GetKeeper() AuxiliaryKeeper
-	InitializeKeeper(Mapper, Parameters, ...interface{})
+	InitializeKeeper(Mapper, Parameters, ...interface{}) Auxiliary
 }
--- schema/helpers/base/auxiliary.go
+++ schema/helpers/base/auxiliary.go
@@ -19,11 +19,12 @@ var _ helpers.Auxiliary = (*auxiliary)(nil)
 
 func (auxiliary auxiliary) GetName() string                    { return auxiliary.name }
 func (auxiliary auxiliary) GetKeeper() helpers.AuxiliaryKeeper { return auxiliary.auxiliaryKeeper }
-func (auxiliary *auxiliary) InitializeKeeper(mapper helpers.Mapper, parameters helpers.Parameters, auxiliaryKeepers ...interface{}) {
+func (auxiliary auxiliary) InitializeKeeper(mapper helpers.Mapper, parameters helpers.Parameters, auxiliaryKeepers ...interface{}) helpers.Auxiliary {
 	auxiliary.auxiliaryKeeper = auxiliary.initializeKeeper(mapper, parameters, auxiliaryKeepers)
+	return auxiliary
 }
 func NewAuxiliary(moduleName string, name string, route string, initializeKeeper func(helpers.Mapper, helpers.Parameters, []interface{}) helpers.AuxiliaryKeeper) helpers.Auxiliary {
-	return &auxiliary{
+	return auxiliary{
 		moduleName:       moduleName,
 		name:             name,
 		route:            route,
