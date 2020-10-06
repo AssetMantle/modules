8b22c84d4d09346116784badce07a4b26d619120
Deepanshu Tripathi <deepanshu@persistence.one>
2020-10-06 23:03:00 +0530

    Defining key helper interface.

---
+++ schema/helpers/key.go
@@ -0,0 +1,13 @@
+/*
+ Copyright [2019] - [2020], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package helpers
+
+import "github.com/cosmos/cosmos-sdk/codec"
+
+type Key interface {
+	GenerateStoreKeyBytes() []byte
+	RegisterCodec(*codec.Codec)
+}
