335af908a8622e7c7b6d1be74efafd20daf13028
Deepanshu Tripathi <deepanshu@persistence.one>
2020-10-08 23:40:59 +0530

    Adding new collection method to mapper and retiring parameters, key generator and register codec members.

--- schema/helpers/base/mapper.go
+++ schema/helpers/base/mapper.go
@@ -11,51 +11,50 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/persistenceOne/persistenceSDK/schema/helpers"
-	"github.com/persistenceOne/persistenceSDK/schema/traits"
-	"github.com/persistenceOne/persistenceSDK/schema/types"
-	"github.com/persistenceOne/persistenceSDK/schema/types/base"
 	"github.com/tendermint/tendermint/libs/kv"
 )
 
 type mapper struct {
 	kvStoreKey        *sdkTypes.KVStoreKey
-	parameters        helpers.Parameters
-	keyGenerator      func(types.ID) []byte
-	mappablePrototype func() traits.Mappable
-	registerCodec     func(*codec.Codec)
+	keyPrototype      func() helpers.Key
+	mappablePrototype func() helpers.Mappable
 }
 
 var _ helpers.Mapper = (*mapper)(nil)
 
+func (mapper mapper) NewCollection(context sdkTypes.Context) helpers.Collection {
+	return collection{}.Initialize(context, mapper)
+}
+
 func (mapper mapper) GetKVStoreKey() *sdkTypes.KVStoreKey {
 	return mapper.kvStoreKey
 }
-func (mapper mapper) Create(context sdkTypes.Context, mappable traits.Mappable) {
+func (mapper mapper) Create(context sdkTypes.Context, mappable helpers.Mappable) {
 	Bytes := mappable.Encode()
 	kvStore := context.KVStore(mapper.kvStoreKey)
-	kvStore.Set(mapper.keyGenerator(mappable.GetID()), Bytes)
+	kvStore.Set(mappable.GetKey().GenerateStoreKeyBytes(), Bytes)
 }
-func (mapper mapper) Read(context sdkTypes.Context, id types.ID) traits.Mappable {
+func (mapper mapper) Read(context sdkTypes.Context, key helpers.Key) helpers.Mappable {
 	kvStore := context.KVStore(mapper.kvStoreKey)
-	Bytes := kvStore.Get(mapper.keyGenerator(id))
+	Bytes := kvStore.Get(key.GenerateStoreKeyBytes())
 	if Bytes == nil {
 		return nil
 	}
 	return mapper.mappablePrototype().Decode(Bytes)
 }
-func (mapper mapper) Update(context sdkTypes.Context, mappable traits.Mappable) {
+func (mapper mapper) Update(context sdkTypes.Context, mappable helpers.Mappable) {
 	Bytes := mappable.Encode()
-	id := mappable.GetID()
+	key := mappable.GetKey()
 	kvStore := context.KVStore(mapper.kvStoreKey)
-	kvStore.Set(mapper.keyGenerator(id), Bytes)
+	kvStore.Set(key.GenerateStoreKeyBytes(), Bytes)
 }
-func (mapper mapper) Delete(context sdkTypes.Context, id types.ID) {
+func (mapper mapper) Delete(context sdkTypes.Context, key helpers.Key) {
 	kvStore := context.KVStore(mapper.kvStoreKey)
-	kvStore.Delete(mapper.keyGenerator(id))
+	kvStore.Delete(key.GenerateStoreKeyBytes())
 }
-func (mapper mapper) Iterate(context sdkTypes.Context, id types.ID, accumulator func(traits.Mappable) bool) {
+func (mapper mapper) Iterate(context sdkTypes.Context, key helpers.Key, accumulator func(helpers.Mappable) bool) {
 	store := context.KVStore(mapper.kvStoreKey)
-	kvStorePrefixIterator := sdkTypes.KVStorePrefixIterator(store, mapper.keyGenerator(id))
+	kvStorePrefixIterator := sdkTypes.KVStorePrefixIterator(store, key.GenerateStoreKeyBytes())
 
 	defer kvStorePrefixIterator.Close()
 	for ; kvStorePrefixIterator.Valid(); kvStorePrefixIterator.Next() {
@@ -66,22 +65,23 @@ func (mapper mapper) Iterate(context sdkTypes.Context, id types.ID, accumulator 
 	}
 }
 func (mapper mapper) StoreDecoder(_ *codec.Codec, kvA kv.Pair, kvB kv.Pair) string {
-	if bytes.Equal(kvA.Key[:1], mapper.keyGenerator(base.NewID(""))) {
+	if bytes.Equal(kvA.Key[:1], mapper.keyPrototype().GenerateStoreKeyBytes()) {
 		return fmt.Sprintf("%v\n%v", mapper.mappablePrototype().Decode(kvA.Value), mapper.mappablePrototype().Decode(kvB.Value))
 	} else {
 		panic(fmt.Sprintf("invalid key prefix %X", kvA.Key[:1]))
 	}
 }
 func (mapper mapper) RegisterCodec(codec *codec.Codec) {
-	mapper.registerCodec(codec)
+	mapper.keyPrototype().RegisterCodec(codec)
+	mapper.mappablePrototype().RegisterCodec(codec)
 }
-
-func NewMapper(module string, parameters helpers.Parameters, keyGenerator func(types.ID) []byte, mappablePrototype func() traits.Mappable, registerCodec func(*codec.Codec)) helpers.Mapper {
+func (mapper mapper) Initialize(kvStoreKey *sdkTypes.KVStoreKey) helpers.Mapper {
+	mapper.kvStoreKey = kvStoreKey
+	return mapper
+}
+func NewMapper(keyPrototype func() helpers.Key, mappablePrototype func() helpers.Mappable) helpers.Mapper {
 	return mapper{
-		kvStoreKey:        sdkTypes.NewKVStoreKey(module),
-		parameters:        parameters,
-		keyGenerator:      keyGenerator,
+		keyPrototype:      keyPrototype,
 		mappablePrototype: mappablePrototype,
-		registerCodec:     registerCodec,
 	}
 }
