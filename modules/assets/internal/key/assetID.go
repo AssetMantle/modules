4604ef205f6a30e75c4f6b7224f2ef7fd26c58b9
Deepanshu Tripathi <deepanshu@persistence.one>
2020-10-09 13:54:08 +0530

    Moving assetID utility method to a separate utility file in key package.

--- modules/assets/internal/key/assetID.go
+++ modules/assets/internal/key/assetID.go
@@ -12,17 +12,12 @@ import (
 	"github.com/persistenceOne/persistenceSDK/modules/assets/module"
 	"github.com/persistenceOne/persistenceSDK/schema/helpers"
 	"github.com/persistenceOne/persistenceSDK/schema/types"
-	"github.com/persistenceOne/persistenceSDK/schema/types/base"
 	"strings"
 )
 
 type assetID struct {
 	ClassificationID types.ID `json:"classificationID" valid:"required~required field classificationID missing"`
 	HashID           types.ID `json:"hashID" valid:"required~required field hashID missing"`
-}
-
-func (AssetID assetID) GenerateStoreKeyBytes() []byte {
-	return append(storeKeyPrefix, AssetID.Bytes()...)
 }
 
 var _ types.ID = (*assetID)(nil)
@@ -42,7 +37,7 @@ func (AssetID assetID) String() string {
 	return strings.Join(values, constants.FirstOrderCompositeIDSeparator)
 }
 
-func (AssetID assetID) Equal(id types.ID) bool {
+func (AssetID assetID) Equals(id types.ID) bool {
 	switch id.(type) {
 	case assetID:
 		return bytes.Compare(AssetID.Bytes(), id.Bytes()) == 0
@@ -50,38 +45,30 @@ func (AssetID assetID) Equal(id types.ID) bool {
 		return false
 	}
 }
-
+func (AssetID assetID) Matches(key helpers.Key) bool {
+	switch value := key.(type) {
+	case assetID:
+		return bytes.Compare(AssetID.Bytes(), value.Bytes()) == 0
+	default:
+		return false
+	}
+}
+func (AssetID assetID) GenerateStoreKeyBytes() []byte {
+	return append(storeKeyPrefix, AssetID.Bytes()...)
+}
 func (assetID) RegisterCodec(codec *codec.Codec) {
 	codec.RegisterConcrete(assetID{}, module.Route+"/"+"assetID", nil)
 }
 
-func readAssetID(assetIDString string) types.ID {
-	idList := strings.Split(assetIDString, constants.FirstOrderCompositeIDSeparator)
-	if len(idList) == 2 {
-		return assetID{
-			ClassificationID: base.NewID(idList[0]),
-			HashID:           base.NewID(idList[1]),
-		}
+func (AssetID assetID) IsPartial() bool {
+	if len(AssetID.HashID.Bytes()) > 0 {
+		return false
 	}
-	return assetID{ClassificationID: base.NewID(""), HashID: base.NewID("")}
+	return true
 }
 
-func assetIDFromInterface(id types.ID) assetID {
-	switch value := id.(type) {
-	case assetID:
-		return value
-	default:
-		return assetIDFromInterface(readAssetID(id.String()))
-	}
-}
-func AssetIDAsKey(id types.ID) helpers.Key {
+func New(id types.ID) helpers.Key {
 	return assetIDFromInterface(id)
-}
-func ReadClassificationID(assetID types.ID) types.ID {
-	return assetIDFromInterface(assetID).ClassificationID
-}
-func ReadHashID(assetID types.ID) types.ID {
-	return assetIDFromInterface(assetID).HashID
 }
 
 func NewAssetID(classificationID types.ID, immutables types.Immutables) types.ID {
