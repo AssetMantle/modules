b56a056c4c92b34932389dbdf8a97fb12749b499
puneet2019 <puneet@persistence.one>
2020-10-19 15:40:14 +0530

    updating maintainers module queries.

--- modules/maintainers/internal/queries/maintainer/response.go
+++ modules/maintainers/internal/queries/maintainer/response.go
@@ -6,14 +6,14 @@
 package maintainer
 
 import (
+	"github.com/persistenceOne/persistenceSDK/modules/maintainers/internal/module"
 	"github.com/persistenceOne/persistenceSDK/schema/helpers"
-	"github.com/persistenceOne/persistenceSDK/schema/mappers"
 )
 
 type queryResponse struct {
-	Success     bool                `json:"success"`
-	Error       error               `json:"error"`
-	Maintainers mappers.Maintainers `json:"maintainers" valid:"required~required field maintainers missing"`
+	Success     bool               `json:"success"`
+	Error       error              `json:"error"`
+	Maintainers helpers.Collection `json:"maintainers" valid:"required~required field maintainers missing"`
 }
 
 var _ helpers.QueryResponse = (*queryResponse)(nil)
@@ -24,11 +24,21 @@ func (queryResponse queryResponse) IsSuccessful() bool {
 func (queryResponse queryResponse) GetError() error {
 	return queryResponse.Error
 }
-func queryResponsePrototype() helpers.QueryResponse {
+func (queryResponse queryResponse) Encode() ([]byte, error) {
+	return module.Codec.MarshalJSON(queryResponse)
+}
+
+func (queryResponse queryResponse) Decode(bytes []byte) (helpers.QueryResponse, error) {
+	if Error := module.Codec.UnmarshalJSON(bytes, &queryResponse); Error != nil {
+		return nil, Error
+	}
+	return queryResponse, nil
+}
+func responsePrototype() helpers.QueryResponse {
 	return queryResponse{}
 }
 
-func newQueryResponse(maintainers mappers.Maintainers, error error) helpers.QueryResponse {
+func newQueryResponse(maintainers helpers.Collection, error error) helpers.QueryResponse {
 	success := true
 	if error != nil {
 		success = false
