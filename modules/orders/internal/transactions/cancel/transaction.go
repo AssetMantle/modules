6959538dd6e138a2af077244fccbb92a019f1d45
puneet2019 <puneet@persistence.one>
2020-10-19 15:42:18 +0530

    updating orders module cancel transaction.

--- modules/orders/internal/transactions/cancel/transaction.go
+++ modules/orders/internal/transactions/cancel/transaction.go
@@ -7,19 +7,17 @@ package cancel
 
 import (
 	"github.com/persistenceOne/persistenceSDK/constants/flags"
-	"github.com/persistenceOne/persistenceSDK/modules/orders/internal/mapper"
-	"github.com/persistenceOne/persistenceSDK/schema/helpers"
 	"github.com/persistenceOne/persistenceSDK/schema/helpers/base"
 )
 
 var Transaction = base.NewTransaction(
-	mapper.ModuleName,
-	TransactionName,
-	TransactionRoute,
-	TransactionShort,
-	TransactionLong,
-	registerCodec,
-	initializeTransactionKeeper,
+	"cancel",
+	"",
+	"",
+
 	requestPrototype,
-	[]helpers.CLIFlag{flags.OrderID, flags.FromID},
+	messagePrototype,
+	keeperPrototype,
+	flags.OrderID,
+	flags.FromID,
 )
