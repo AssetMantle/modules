f3feb9db0ef78578de03f0b2cfca08007c63633a
puneet2019 <puneet@persistence.one>
2020-10-19 15:42:24 +0530

    updating orders module make transaction.

--- modules/orders/internal/transactions/make/message.go
+++ modules/orders/internal/transactions/make/message.go
@@ -7,9 +7,12 @@ package make
 
 import (
 	"github.com/asaskevich/govalidator"
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/errors"
 	xprtErrors "github.com/persistenceOne/persistenceSDK/constants/errors"
+	"github.com/persistenceOne/persistenceSDK/modules/orders/internal/module"
+	"github.com/persistenceOne/persistenceSDK/schema/helpers"
 	"github.com/persistenceOne/persistenceSDK/schema/types"
 )
 
@@ -29,7 +32,7 @@ type message struct {
 
 var _ sdkTypes.Msg = message{}
 
-func (message message) Route() string { return Transaction.GetModuleName() }
+func (message message) Route() string { return module.Name }
 func (message message) Type() string  { return Transaction.GetName() }
 func (message message) ValidateBasic() error {
 	var _, Error = govalidator.ValidateStruct(message)
@@ -39,12 +42,14 @@ func (message message) ValidateBasic() error {
 	return nil
 }
 func (message message) GetSignBytes() []byte {
-	return sdkTypes.MustSortJSON(packageCodec.MustMarshalJSON(message))
+	return sdkTypes.MustSortJSON(module.Codec.MustMarshalJSON(message))
 }
 func (message message) GetSigners() []sdkTypes.AccAddress {
 	return []sdkTypes.AccAddress{message.From}
 }
-
+func (message) RegisterCodec(codec *codec.Codec) {
+	codec.RegisterConcrete(message{}, Transaction.GetName()+"/"+"message", nil)
+}
 func messageFromInterface(msg sdkTypes.Msg) message {
 	switch value := msg.(type) {
 	case message:
@@ -52,6 +57,9 @@ func messageFromInterface(msg sdkTypes.Msg) message {
 	default:
 		return message{}
 	}
+}
+func messagePrototype() helpers.Message {
+	return message{}
 }
 
 func newMessage(from sdkTypes.AccAddress, fromID types.ID, classificationID types.ID, makerOwnableID types.ID, takerOwnableID types.ID, expiresIn types.Height, makerOwnableSplit sdkTypes.Dec, immutableMetaProperties types.MetaProperties, immutableProperties types.Properties, mutableMetaProperties types.MetaProperties, mutableProperties types.Properties) sdkTypes.Msg {
