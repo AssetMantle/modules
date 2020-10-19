0c2d24a7fc536a465f679431b7ffb54e492622bf
puneet2019 <puneet@persistence.one>
2020-10-19 15:40:38 +0530

    updating splits module queries.

--- modules/orders/internal/transactions/take/transaction.go
+++ modules/orders/internal/transactions/take/transaction.go
@@ -7,19 +7,18 @@ package take
 
 import (
 	"github.com/persistenceOne/persistenceSDK/constants/flags"
-	"github.com/persistenceOne/persistenceSDK/modules/orders/internal/mapper"
-	"github.com/persistenceOne/persistenceSDK/schema/helpers"
 	"github.com/persistenceOne/persistenceSDK/schema/helpers/base"
 )
 
 var Transaction = base.NewTransaction(
-	mapper.ModuleName,
-	TransactionName,
-	TransactionRoute,
-	TransactionShort,
-	TransactionLong,
-	registerCodec,
-	initializeTransactionKeeper,
+	"take",
+	"",
+	"",
+
 	requestPrototype,
-	[]helpers.CLIFlag{flags.FromID, flags.TakerOwnableSplit, flags.OrderID},
+	messagePrototype,
+	keeperPrototype,
+	flags.FromID,
+	flags.TakerOwnableSplit,
+	flags.OrderID,
 )
