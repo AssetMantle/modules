a69042b85b741dd5625016a65d1b3c8276384311
puneet2019 <puneet@persistence.one>
2020-12-02 17:50:25 +0530

    adding test cases for mapper - mapper_test.go.

--- schema/helpers/base/mapper_test.go
+++ schema/helpers/base/mapper_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/persistenceOne/persistenceSDK/schema/helpers"
 	"github.com/stretchr/testify/require"
 	abciTypes "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/kv"
 	"github.com/tendermint/tendermint/libs/log"
 	tendermintDB "github.com/tendermint/tm-db"
 
@@ -16,69 +17,72 @@ import (
 
 // key struct, implements helpers.Key
 type testKey struct {
-	name string
+	ID string
 }
 
 var _ helpers.Key = (*testKey)(nil)
 
 func (t testKey) GenerateStoreKeyBytes() []byte {
-	return []byte(t.name)
+	return append([]byte{0x11}, []byte(t.ID)...)
 }
 
 func (t testKey) RegisterCodec(codec *codec.Codec) {
-	codec.RegisterConcrete(testKey{}, "test/schema/helpers/base/testKey", nil)
+	codec.RegisterConcrete(testKey{}, "test/testKey", nil)
 }
 
 func (t testKey) IsPartial() bool {
-	if t.name == "" {
+	if t.ID == "" {
 		return false
 	}
 	return true
 }
 
 func (t testKey) Matches(key helpers.Key) bool {
-	if bytes.Equal([]byte(t.name), key.GenerateStoreKeyBytes()) {
+	if bytes.Equal([]byte(t.ID), key.GenerateStoreKeyBytes()) {
 		return true
 	}
 	return false
 }
 
-func NewKey(name string) helpers.Key {
-	return testKey{name: name}
+func NewKey(id string) helpers.Key {
+	return testKey{ID: id}
 }
 
 func keyPrototype() helpers.Key {
-	return NewKey("")
+	return testKey{}
 }
 
 // mappable struct, implements helpers.Mappable
 type testMappable struct {
-	id string
+	ID    string
+	Value string
 }
 
 var _ helpers.Mappable = (*testMappable)(nil)
 
 func (t testMappable) GetKey() helpers.Key {
-	return NewKey(t.id)
+	return NewKey(t.ID)
 }
 
 func (t testMappable) RegisterCodec(c *codec.Codec) {
-	c.RegisterConcrete(testMappable{}, "test/schema/helpers/base/testMappable", nil)
+	c.RegisterConcrete(testMappable{}, "test/testMappable", nil)
 	return
 }
 
-func NewMappable(id string) helpers.Mappable {
-	return testMappable{id: id}
+func NewMappable(id string, value string) helpers.Mappable {
+	return testMappable{ID: id, Value: value}
 }
 
 func mappablePrototype() helpers.Mappable {
-	return NewMappable("")
+	return testMappable{}
 }
 
 func TestMapper(t *testing.T) {
-	mapper := NewMapper(keyPrototype, mappablePrototype)
-	storeKey := sdkTypes.NewKVStoreKey("testHelper")
-	mapper = mapper.Initialize(storeKey)
+
+	storeKey := sdkTypes.NewKVStoreKey("test")
+
+	mapper := NewMapper(keyPrototype, mappablePrototype).Initialize(storeKey).(mapper)
+
 	memDB := tendermintDB.NewMemDB()
 	commitMultiStore := store.NewCommitMultiStore(memDB)
 	commitMultiStore.MountStoreWithDB(storeKey, sdkTypes.StoreTypeIAVL, memDB)
@@ -89,6 +93,17 @@ func TestMapper(t *testing.T) {
 		ChainID: "test",
 	}, false, log.NewNopLogger())
 
-	require.Equal(t, nil, mapper.NewCollection(context).Get(keyPrototype()))
+	mapper.GetKVStoreKey()
+
+	mapper.NewCollection(context)
+	mapper.Create(context, NewMappable("test1", "value1"))
+	mapper.Create(context, NewMappable("test2", "value2"))
+	mapper.Read(context, NewKey("test1"))
+	mapper.Update(context, NewMappable("test1", "value1"))
+	mapper.Delete(context, NewKey("test2"))
+	mapper.Iterate(context, NewKey("test1"), func(mappable helpers.Mappable) bool { return false })
+	mapper.StoreDecoder(codec.New(), kv.Pair{
+		Key: append([]byte{0x11}, []byte("test1")...), Value: mapper.codec.MustMarshalBinaryBare(NewMappable("test1", "value1"))}, kv.Pair{
+		Key: append([]byte{0x11}, []byte("test1")...), Value: mapper.codec.MustMarshalBinaryBare(NewMappable("test1", "value1"))})
 
 }
