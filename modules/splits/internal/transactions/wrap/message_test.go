5cbb54651c3c26a38e592701ca4e2b8ab29d9b45
Deepanshu Tripathi <deepanshu@persistence.one>
2020-12-18 17:08:27 +0530

    Updating splits module message test cases for codec reg utility change.

--- modules/splits/internal/transactions/wrap/message_test.go
+++ modules/splits/internal/transactions/wrap/message_test.go
@@ -2,9 +2,9 @@ package wrap
 
 import (
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
-	"github.com/persistenceOne/persistenceSDK/modules/splits/internal/common"
 	"github.com/persistenceOne/persistenceSDK/modules/splits/internal/module"
 	"github.com/persistenceOne/persistenceSDK/schema/types/base"
+	"github.com/persistenceOne/persistenceSDK/utilities/transaction"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -23,7 +23,7 @@ func Test_Wrap_Message(t *testing.T) {
 	require.Equal(t, Transaction.GetName(), testMessage.Type())
 	require.Equal(t, nil, testMessage.ValidateBasic())
 	require.NotNil(t, message{}.ValidateBasic())
-	require.Equal(t, sdkTypes.MustSortJSON(common.Codec.MustMarshalJSON(testMessage)), testMessage.GetSignBytes())
+	require.Equal(t, sdkTypes.MustSortJSON(transaction.RegisterCodec(messagePrototype).MustMarshalJSON(testMessage)), testMessage.GetSignBytes())
 	require.Equal(t, []sdkTypes.AccAddress{fromAccAddress}, testMessage.GetSigners())
 	require.Equal(t, testMessage, messageFromInterface(testMessage))
 	require.Equal(t, message{}, messageFromInterface(nil))
