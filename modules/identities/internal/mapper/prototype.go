5d8cc5b39a669845cc34f1c31390a6767a6a80e1
deepanshutr <deepanshutr@gmail.com>
2021-01-08 18:21:25 +0530

    Updating file headers and adding package doc file.

--- modules/identities/internal/mapper/prototype.go
+++ modules/identities/internal/mapper/prototype.go
@@ -1,3 +1,8 @@
+/*
+ Copyright [2019] - [2021], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
 package mapper
 
 import (
---
+++ modules/metas/doc.go
@@ -0,0 +1,7 @@
+/*
+ Copyright [2019] - [2021], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+// metas package defines the assets module.
+package metas
