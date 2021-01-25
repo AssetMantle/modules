df1531339665153c480f4b897bd382874824eb4e
deepanshutr <deepanshutr@gmail.com>
2021-01-25 17:13:15 +0530

    Defining common utility methods for splits module internal.

---
+++ modules/assets/internal/transactions/renumerate/keeper.go
@@ -0,0 +1,85 @@
+/*
+ Copyright [2019] - [2021], PERSISTENCE TECHNOLOGIES PTE. LTD. and the persistenceSDK contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package renumerate
+
+import (
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/persistenceOne/persistenceSDK/constants/errors"
+	"github.com/persistenceOne/persistenceSDK/constants/properties"
+	"github.com/persistenceOne/persistenceSDK/modules/assets/internal/key"
+	"github.com/persistenceOne/persistenceSDK/modules/identities/auxiliaries/verify"
+	"github.com/persistenceOne/persistenceSDK/modules/metas/auxiliaries/supplement"
+	"github.com/persistenceOne/persistenceSDK/modules/splits/auxiliaries/renumerate"
+	"github.com/persistenceOne/persistenceSDK/schema/helpers"
+	"github.com/persistenceOne/persistenceSDK/schema/mappables"
+	"github.com/persistenceOne/persistenceSDK/schema/types/base"
+)
+
+type transactionKeeper struct {
+	mapper              helpers.Mapper
+	renumerateAuxiliary helpers.Auxiliary
+	supplementAuxiliary helpers.Auxiliary
+	verifyAuxiliary     helpers.Auxiliary
+}
+
+var _ helpers.TransactionKeeper = (*transactionKeeper)(nil)
+
+func (transactionKeeper transactionKeeper) Transact(context sdkTypes.Context, msg sdkTypes.Msg) helpers.TransactionResponse {
+	message := messageFromInterface(msg)
+	if auxiliaryResponse := transactionKeeper.verifyAuxiliary.GetKeeper().Help(context, verify.NewAuxiliaryRequest(message.From, message.FromID)); !auxiliaryResponse.IsSuccessful() {
+		return newTransactionResponse(auxiliaryResponse.GetError())
+	}
+
+	assets := transactionKeeper.mapper.NewCollection(context).Fetch(key.New(message.AssetID))
+
+	asset := assets.Get(key.New(message.AssetID))
+	if asset == nil {
+		return newTransactionResponse(errors.EntityNotFound)
+	}
+
+	metaProperties, Error := supplement.GetMetaPropertiesFromResponse(transactionKeeper.supplementAuxiliary.GetKeeper().Help(context, supplement.NewAuxiliaryRequest(asset.(mappables.InterNFT).GetBurn())))
+	if Error != nil {
+		return newTransactionResponse(Error)
+	}
+
+	if valueMetaProperty := metaProperties.GetMetaProperty(base.NewID(properties.Value)); valueMetaProperty != nil {
+		if value, Error := valueMetaProperty.GetMetaFact().GetData().AsDec(); Error != nil {
+			return newTransactionResponse(Error)
+		} else {
+			if auxiliaryResponse := transactionKeeper.renumerateAuxiliary.GetKeeper().Help(context, renumerate.NewAuxiliaryRequest(message.FromID, message.AssetID, value)); !auxiliaryResponse.IsSuccessful() {
+				return newTransactionResponse(auxiliaryResponse.GetError())
+			}
+		}
+	}
+
+	return newTransactionResponse(nil)
+}
+
+func (transactionKeeper transactionKeeper) Initialize(mapper helpers.Mapper, _ helpers.Parameters, auxiliaries []interface{}) helpers.Keeper {
+	transactionKeeper.mapper = mapper
+
+	for _, auxiliary := range auxiliaries {
+		switch value := auxiliary.(type) {
+		case helpers.Auxiliary:
+			switch value.GetName() {
+			case renumerate.Auxiliary.GetName():
+				transactionKeeper.renumerateAuxiliary = value
+			case supplement.Auxiliary.GetName():
+				transactionKeeper.supplementAuxiliary = value
+			case verify.Auxiliary.GetName():
+				transactionKeeper.verifyAuxiliary = value
+			}
+		default:
+			panic(errors.UninitializedUsage)
+		}
+	}
+
+	return transactionKeeper
+}
+
+func keeperPrototype() helpers.TransactionKeeper {
+	return transactionKeeper{}
+}
