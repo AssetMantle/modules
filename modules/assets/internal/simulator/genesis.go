155131d81fe419443f5b0d271ea107a2f8905d7a
puneet2019 <puneet@persistence.one>
2021-01-25 08:37:18 +0530

    adding random generation of types for simulation.

--- modules/assets/internal/simulator/genesis.go
+++ modules/assets/internal/simulator/genesis.go
@@ -7,11 +7,14 @@ package simulator
 
 import (
 	"fmt"
+	"github.com/persistenceOne/persistenceSDK/schema/helpers"
+	"math"
 	"math/rand"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
 	"github.com/persistenceOne/persistenceSDK/modules/assets/internal/key"
 	"github.com/persistenceOne/persistenceSDK/modules/assets/internal/mappable"
 	assetsModule "github.com/persistenceOne/persistenceSDK/modules/assets/internal/module"
@@ -32,9 +35,56 @@ func (simulator) RandomizedGenesisState(simulationState *module.SimulationState)
 		func(rand *rand.Rand) { data = base.NewDecData(sdkTypes.NewDecWithPrec(int64(rand.Intn(99)), 2)) },
 	)
 
-	// TODO add assetList
-	genesisState := baseHelpers.NewGenesis(key.Prototype, mappable.Prototype, nil, nil).Initialize(nil, []types.Parameter{dummy.Parameter.Mutate(data)})
+	var mappableList []helpers.Mappable
+	for range simulationState.Accounts {
+		immutables := GenerateRandomImmutables(simulationState.Rand)
+		mappableList = append(mappableList, mappable.NewAsset(key.NewAssetID(GenerateRandomID(simulationState.Rand), immutables), immutables, GenerateRandomMutables(simulationState.Rand)))
+	}
+	genesisState := baseHelpers.NewGenesis(key.Prototype, mappable.Prototype, nil, nil).Initialize(mappableList, []types.Parameter{dummy.Parameter.Mutate(data)})
 
 	fmt.Printf("Selected randomly generated minting parameters:\n%s\n", codec.MustMarshalJSONIndent(simulationState.Cdc, genesisState))
 	simulationState.GenState[assetsModule.Name] = simulationState.Cdc.MustMarshalJSON(genesisState)
+}
+
+func GenerateRandomImmutables(r *rand.Rand) types.Immutables {
+	return base.NewImmutables(GenerateRandomProperties(r))
+}
+
+func GenerateRandomMutables(r *rand.Rand) types.Mutables {
+	return base.NewMutables(GenerateRandomProperties(r))
+}
+
+func GenerateRandomID(r *rand.Rand) types.ID {
+	return base.NewID(simulation.RandStringOfLength(r, r.Int()))
+}
+
+func GenerateRandomFact(r *rand.Rand) types.Fact {
+	randomPositiveInt := int(math.Abs(float64(r.Int())))
+	var data types.Data
+	switch randomPositiveInt % 4 {
+	case 0:
+		data = base.NewIDData(GenerateRandomID(r))
+	case 1:
+		data = base.NewStringData(simulation.RandStringOfLength(r, r.Int()))
+	case 2:
+		data = base.NewDecData(simulation.RandomDecAmount(r, sdkTypes.NewDec(9999999999)))
+	case 3:
+		data = base.NewHeightData(base.NewHeight(r.Int63()))
+	default:
+		return nil
+	}
+	return base.NewFact(data)
+}
+
+func GenerateRandomProperty(r *rand.Rand) types.Property {
+	return base.NewProperty(GenerateRandomID(r), GenerateRandomFact(r))
+}
+
+func GenerateRandomProperties(r *rand.Rand) types.Properties {
+	randomPositiveInt := int(math.Abs(float64(r.Int()))) % 11
+	var propertyList []types.Property
+	for i := 0; i <= randomPositiveInt; i++ {
+		propertyList = append(propertyList, GenerateRandomProperty(r))
+	}
+	return base.NewProperties(propertyList...)
 }
