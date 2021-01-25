49769cf6ce58dff56bfefecb60241a3334c69c5f
deepanshutr <deepanshutr@gmail.com>
2021-01-25 17:14:51 +0530

    Renaming split input flag to value flag and property.

--- modules/splits/auxiliaries/transfer/request_test.go
+++ modules/splits/auxiliaries/transfer/request_test.go
@@ -6,10 +6,11 @@
 package transfer
 
 import (
+	"testing"
+
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/persistenceOne/persistenceSDK/schema/types/base"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func Test_Transfer_Request(t *testing.T) {
@@ -19,7 +20,7 @@ func Test_Transfer_Request(t *testing.T) {
 	splits := sdkTypes.NewDec(10)
 	testAuxiliaryRequest := NewAuxiliaryRequest(fromID, toID, ownableID, splits)
 
-	require.Equal(t, auxiliaryRequest{FromID: fromID, ToID: toID, OwnableID: ownableID, Split: splits}, testAuxiliaryRequest)
+	require.Equal(t, auxiliaryRequest{FromID: fromID, ToID: toID, OwnableID: ownableID, Value: splits}, testAuxiliaryRequest)
 	require.Equal(t, nil, testAuxiliaryRequest.Validate())
 	require.Equal(t, testAuxiliaryRequest, auxiliaryRequestFromInterface(testAuxiliaryRequest))
 	require.Equal(t, auxiliaryRequest{}, auxiliaryRequestFromInterface(nil))
