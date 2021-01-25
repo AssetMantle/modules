e183906bde4bd398dff7da7cecebba78ff3ca0f9
deepanshutr <deepanshutr@gmail.com>
2021-01-25 17:09:43 +0530

    Renaming split input flag to value flag and property.

--- modules/splits/internal/mappable/split_test.go
+++ modules/splits/internal/mappable/split_test.go
@@ -6,11 +6,12 @@
 package mappable
 
 import (
+	"testing"
+
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/persistenceOne/persistenceSDK/modules/splits/internal/key"
 	"github.com/persistenceOne/persistenceSDK/schema/types/base"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func Test_Split_Methods(t *testing.T) {
@@ -19,14 +20,14 @@ func Test_Split_Methods(t *testing.T) {
 	ownableID := base.NewID("ownableID")
 
 	testSplitID := key.NewSplitID(ownerID, ownableID)
-	splitDec := sdkTypes.NewDec(12)
-	testSplit := NewSplit(testSplitID, splitDec).(split)
+	testValue := sdkTypes.NewDec(12)
+	testSplit := NewSplit(testSplitID, testValue).(split)
 
-	require.Equal(t, split{ID: testSplitID, Split: splitDec}, testSplit)
+	require.Equal(t, split{ID: testSplitID, Value: testValue}, testSplit)
 	require.Equal(t, testSplitID, testSplit.GetID())
 	require.Equal(t, ownerID, testSplit.GetOwnerID())
 	require.Equal(t, ownableID, testSplit.GetOwnableID())
-	require.Equal(t, splitDec, testSplit.GetValue())
+	require.Equal(t, testValue, testSplit.GetValue())
 	require.Equal(t, NewSplit(testSplitID, sdkTypes.NewDec(11)).(split), testSplit.Send(sdkTypes.NewDec(1)))
 	require.Equal(t, NewSplit(testSplitID, sdkTypes.NewDec(13)).(split), testSplit.Receive(sdkTypes.NewDec(1)))
 	require.Equal(t, true, testSplit.CanSend(sdkTypes.NewDec(5)))
