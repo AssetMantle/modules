0226dc0625b1d14a31a6e03fc07daa97a9edaeac
avkr003 <abhinav@persistence.one>
2021-02-18 18:17:55 +0530

    Correcting delete order on expiry

--- modules/orders/internal/block/block.go
+++ modules/orders/internal/block/block.go
@@ -11,6 +11,7 @@ import (
 	"github.com/persistenceOne/persistenceSDK/constants/properties"
 	"github.com/persistenceOne/persistenceSDK/modules/metas/auxiliaries/supplement"
 	"github.com/persistenceOne/persistenceSDK/modules/orders/internal/key"
+	"github.com/persistenceOne/persistenceSDK/modules/orders/internal/module"
 	"github.com/persistenceOne/persistenceSDK/modules/splits/auxiliaries/transfer"
 	"github.com/persistenceOne/persistenceSDK/schema/helpers"
 	"github.com/persistenceOne/persistenceSDK/schema/mappables"
@@ -34,25 +35,40 @@ func (block block) Begin(_ sdkTypes.Context, _ abciTypes.RequestBeginBlock) {
 func (block block) End(context sdkTypes.Context, endBlockRequest abciTypes.RequestEndBlock) {
 	orders := block.mapper.NewCollection(context)
 
-	accumulator := func(order helpers.Mappable) bool {
-		metaProperties, Error := supplement.GetMetaPropertiesFromResponse(block.supplementAuxiliary.GetKeeper().Help(context, supplement.NewAuxiliaryRequest(order.(mappables.Order).GetExpiry())))
-		if Error != nil {
-			panic(Error)
-		}
-
-		if expiryProperty := metaProperties.GetMetaProperty(base.NewID(properties.Expiry)); expiryProperty != nil {
-			expiry, Error := expiryProperty.GetMetaFact().GetData().AsHeight()
+	orders.Iterate(
+		key.New(base.NewID("")),
+		func(order helpers.Mappable) bool {
+			metaProperties, Error := supplement.GetMetaPropertiesFromResponse(block.supplementAuxiliary.GetKeeper().Help(context, supplement.NewAuxiliaryRequest(order.(mappables.Order).GetExpiry(), order.(mappables.Order).GetMakerOwnableSplit())))
 			if Error != nil {
 				panic(Error)
-			} else if !expiry.IsGreaterThan(base.NewHeight(endBlockRequest.Height)) {
-				orders.Remove(order)
 			}
-		}
 
-		return false
-	}
+			if expiryProperty := metaProperties.GetMetaProperty(base.NewID(properties.Expiry)); expiryProperty != nil {
+				expiry, Error := expiryProperty.GetMetaFact().GetData().AsHeight()
+				if Error != nil {
+					panic(Error)
+				} else if !expiry.IsGreaterThan(base.NewHeight(endBlockRequest.Height)) {
+					makerOwnableSplitProperty := metaProperties.GetMetaProperty(base.NewID(properties.MakerOwnableSplit))
+					if makerOwnableSplitProperty == nil {
+						panic(errors.MetaDataError)
+					}
 
-	orders.Iterate(key.New(base.NewID("")), accumulator)
+					makerOwnableSplit, Error := makerOwnableSplitProperty.GetMetaFact().GetData().AsDec()
+					if Error != nil {
+						panic(Error)
+					}
+
+					if auxiliaryResponse := block.transferAuxiliary.GetKeeper().Help(context, transfer.NewAuxiliaryRequest(base.NewID(module.Name), order.(mappables.Order).GetMakerID(), order.(mappables.Order).GetMakerOwnableID(), makerOwnableSplit)); !auxiliaryResponse.IsSuccessful() {
+						panic(auxiliaryResponse.GetError())
+					}
+
+					orders.Remove(order)
+				}
+			}
+
+			return false
+		},
+	)
 }
 
 func (block block) Initialize(mapper helpers.Mapper, parameters helpers.Parameters, auxiliaryKeepers ...interface{}) helpers.Block {
