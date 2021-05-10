2a83d59173795c7c21d7b0a3963c79b84f24336d
bohemianjock <appu.yess@gmail.com>
2021-05-10 16:20:36 +0530

    utilities

---
+++ utilities/transaction/codec_test.go
@@ -0,0 +1,13 @@
+package transaction
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+
+func TestRegisterCodec(t *testing.T) {
+	require.Panics(t, func() {
+		require.Equal(t, RegisterCodec(nil), nil)
+	})
+}
