d1190f2f17047c8154f102890c20f1b5889e0c7a
Deepanshu Tripathi <deepanshutr@gmail.com>
2021-06-28 17:44:04 +0530

    Removing queuing package dependency on flags package.

--- utilities/rest/queuing/routes.go
+++ utilities/rest/queuing/routes.go
@@ -3,11 +3,8 @@ package queuing
 import (
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/gorilla/mux"
-	"github.com/persistenceOne/persistenceSDK/constants/flags"
 )
 
 func RegisterRoutes(cliContext context.CLIContext, router *mux.Router) {
-	if flags.Queuing.GetValue().(bool) {
-		router.HandleFunc("/response/{TicketID}", queryDB(cliContext.Codec)).Methods("GET")
-	}
+	router.HandleFunc("/response/{TicketID}", queryDB(cliContext.Codec)).Methods("GET")
 }
