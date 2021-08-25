801513847fdd3f1abb21cef744e1f14125f89e1e
arhamchordia <chordia.1@iitj.ac.in>
2021-08-25 16:17:09 +0530

    Registering Schema types and traits

--- schema/codec.go
+++ schema/codec.go
@@ -27,19 +27,18 @@ func RegisterLegacyAminoCodec(codec *codec.LegacyAmino) {
 }
 
 func RegisterProtoCodec(registry codecTypes.InterfaceRegistry) {
-	registry.RegisterImplementations((*types.Parameter)(nil), &baseTypes.Parameter{})
+	registry.RegisterImplementations((*types.ID)(nil), &baseTypes.ID{})
+	registry.RegisterImplementations((*types.Height)(nil), &baseTypes.Height{})
+	registry.RegisterImplementations((*types.Signature)(nil), &baseTypes.Signature{})
+	registry.RegisterImplementations((*types.Signatures)(nil), &baseTypes.Signatures{})
 	registry.RegisterImplementations((*types.Data)(nil), &baseTypes.AccAddressData{}, &baseTypes.HeightData{}, &baseTypes.IDData{}, &baseTypes.StringData{}, &baseTypes.DecData{})
 	registry.RegisterImplementations((*types.Fact)(nil), &baseTypes.Fact{})
-	registry.RegisterImplementations((*types.Height)(nil), &baseTypes.Height{})
-	registry.RegisterImplementations((*types.ID)(nil), &baseTypes.ID{})
 	registry.RegisterImplementations((*types.ListData)(nil), &baseTypes.ListData{})
 	registry.RegisterImplementations((*types.MetaFact)(nil), &baseTypes.MetaFact{})
 	registry.RegisterImplementations((*types.MetaProperties)(nil), &baseTypes.MetaProperties{})
 	registry.RegisterImplementations((*types.Parameter)(nil), &baseTypes.Parameter{})
-	registry.RegisterImplementations((*types.Properties)(nil), &baseTypes.Properties{})
 	registry.RegisterImplementations((*types.Property)(nil), &baseTypes.Property{})
-	registry.RegisterImplementations((*types.Signature)(nil), &baseTypes.Signature{})
-	registry.RegisterImplementations((*types.Signatures)(nil), &baseTypes.Signatures{})
+	registry.RegisterImplementations((*types.Properties)(nil), &baseTypes.Properties{})
 	registry.RegisterImplementations((*traits.HasMutables)(nil), &baseTraits.HasMutables{})
 	registry.RegisterImplementations((*traits.HasImmutables)(nil), &baseTraits.HasImmutables{})
 }
