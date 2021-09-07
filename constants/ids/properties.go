edf2c7b689fbb3daa8ee5b082bc98384fccc1d1f
Deepanshu Tripathi <deepanshutr@gmail.com>
2021-09-07 22:33:03 +0530

    Defining ids constants for properties.

---
+++ constants/ids/properties.go
@@ -0,0 +1,19 @@
+package ids
+
+import "github.com/persistenceOne/persistenceSDK/schema/types/base"
+
+//Note: Arranged alphabetically
+var (
+	Authentication       = base.NewID("authentication")
+	Burn                 = base.NewID("burn")
+	Creation             = base.NewID("creation")
+	ExchangeRate         = base.NewID("exchangeRate")
+	Expiry               = base.NewID("expiry")
+	Lock                 = base.NewID("lock")
+	MaintainedProperties = base.NewID("maintainedProperties")
+	MakerOwnableSplit    = base.NewID("makerOwnableSplit")
+	NubID                = base.NewID("nubID")
+	Permissions          = base.NewID("permissions")
+	TakerID              = base.NewID("takerID")
+	Value                = base.NewID("value")
+)
