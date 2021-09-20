711fd1194c2ae9f84b901db9d59f77217bc1a384
Ajaz Ahmed Ansari <ajansari9585@gmail.com>
2021-09-20 17:52:22 +0530

    method modification query keeper

--- modules/metas/internal/queries/meta/keeper.go
+++ modules/metas/internal/queries/meta/keeper.go
@@ -32,7 +32,7 @@ func (queryKeeper queryKeeper) LegacyEnquire(context sdkTypes.Context, queryRequ
 }
 
 func (queryKeeper queryKeeper) Get(ctx context.Context, queryRequest *QueryRequest) (*QueryResponse, error) {
-	response := newQueryResponse(queryKeeper.mapper.NewCollection(sdkTypes.UnwrapSDKContext(ctx)).Fetch(key.FromID(base.NewID(queryRequest.MetaID.IdString))), nil)
+	response := newQueryResponse(queryKeeper.mapper.NewCollection(sdkTypes.UnwrapSDKContext(ctx)).Fetch(key.FromID(base.NewID(queryRequest.MetaID.String()))), nil)
 	return &response, response.GetError()
 }
 func (queryKeeper queryKeeper) GetQueryClient(ctx client.Context) QueryClient {
