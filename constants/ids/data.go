f76c47019e231a9481f25fc3da9ef8452380a87f
Deepanshu Tripathi <deepanshutr@gmail.com>
2021-09-22 14:53:27 +0530

    Defining data id constants.

---
+++ constants/ids/data.go
@@ -0,0 +1,12 @@
+package ids
+
+import "github.com/persistenceOne/persistenceSDK/schema/types/base"
+
+var (
+	BooleanData = base.NewID("B")
+	DecData     = base.NewID("D")
+	HeightData  = base.NewID("H")
+	IDData      = base.NewID("I")
+	ListData    = base.NewID("L")
+	StringData  = base.NewID("S")
+)
