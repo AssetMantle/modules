6d0e89bb1569bcb7abf7e70f0a00bf059044ff75
Evgeny Danienko <6655321@bk.ru>
2021-12-14 15:02:54 +0300

    fix staticcheck

--- utilities/rest/queuing/kafkaConsumer_test.go
+++ utilities/rest/queuing/kafkaConsumer_test.go
@@ -1,17 +1,20 @@
 package queuing
 
 import (
+	"testing"
+
 	"github.com/Shopify/sarama"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/vesting"
-	"github.com/persistenceOne/persistenceSDK/schema"
 	"github.com/stretchr/testify/require"
-	"testing"
+
+	"github.com/persistenceOne/persistenceSDK/schema"
 )
 
 func TestKafkaTopicConsumer(t *testing.T) {
 	testConsumers := []string{"testConsumers"}
+
 	var Codec = codec.New()
 	schema.RegisterCodec(Codec)
 	sdkTypes.RegisterCodec(Codec)
@@ -22,16 +25,19 @@ func TestKafkaTopicConsumer(t *testing.T) {
 	require.Panics(t, func() {
 		testKafkaState := NewKafkaState(testConsumers)
 		partitionConsumer := testKafkaState.Consumers["Topic"]
+
 		var kafkaStore kafkaMsg
 		if len(partitionConsumer.Messages()) == 0 {
 			kafkaStore = kafkaMsg{Msg: nil}
 		}
-		kafkaMsg := <-partitionConsumer.Messages()
-		err := Codec.UnmarshalJSON(kafkaMsg.Value, &kafkaStore)
 
+		kafkaMsg := <-partitionConsumer.Messages()
+
+		err := Codec.UnmarshalJSON(kafkaMsg.Value, &kafkaStore)
 		if err != nil {
 			panic(err)
 		}
+
 		require.Equal(t, kafkaTopicConsumer("Topic", testKafkaState.Consumers, Codec), kafkaStore)
 	})
 }
@@ -39,10 +45,11 @@ func TestKafkaTopicConsumer(t *testing.T) {
 func TestNewConsumer(t *testing.T) {
 	consumers := []string{"testConsumers"}
 	config := sarama.NewConfig()
-	consumer, Error := sarama.NewConsumer(consumers, config)
 
-	if Error != nil {
-	}
+	consumer, err := sarama.NewConsumer(consumers, config)
+
+	require.Nilf(t, err, "should not happened. err %v", err)
+
 	require.Panics(t, func() {
 		require.Equal(t, newConsumer(consumers), consumer)
 	})
