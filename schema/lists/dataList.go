be48e7c7eb66e48edef5a31e5e4f911edb6f5236
deepanshutr <deepanshutr@gmail.com>
2022-04-04 21:53:30 +0530

    Creating DataList interface.

---
+++ schema/lists/dataList.go
@@ -0,0 +1,15 @@
+// Copyright [2021] - [2022], AssetMantle Pte. Ltd. and the code contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package lists
+
+import (
+	"github.com/persistenceOne/persistenceSDK/schema/types"
+)
+
+type DataList interface {
+	GetList() []types.Data
+	Size() int
+
+	Search(types.Data) int
+}
