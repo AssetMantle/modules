9534d7ae71023f0463cae11274e5c63f8a197960
deepanshutr <deepanshutr@gmail.com>
2022-07-07 20:30:32 +0530

    Implementing IsProvisioned, ProvisionAddress and UnprovisionAddress utilites.

--- modules/identities/internal/utilities/identity.go
+++ modules/identities/internal/utilities/identity.go
@@ -3,32 +3,38 @@ package utilities
 import (
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/AssetMantle/modules/modules/metas/auxiliaries/supplement"
+	"github.com/AssetMantle/modules/schema/data"
+	baseData "github.com/AssetMantle/modules/schema/data/base"
+	"github.com/AssetMantle/modules/schema/helpers"
 	"github.com/AssetMantle/modules/schema/mappables"
+	"github.com/AssetMantle/modules/schema/properties/base"
+	"github.com/AssetMantle/modules/schema/properties/constants"
 )
 
-// TODO Implement
-func IsProvisioned(identity mappables.Identity, accAddress sdkTypes.AccAddress) bool {
-	// _, found := identity.GetAuthentication().GetDataID().Search(baseData.NewAccAddressData(accAddress).GetID())
-	// return found
-	panic("implement")
+func IsProvisioned(context sdkTypes.Context, supplementAuxiliary helpers.Auxiliary, identity mappables.Identity, accAddress sdkTypes.AccAddress) (bool, error) {
+	if metaPropertyList, err := supplement.GetMetaPropertiesFromResponse(supplementAuxiliary.GetKeeper().Help(context, supplement.NewAuxiliaryRequest(identity.GetAuthentication()))); err != nil {
+		return false, err
+	} else {
+		_, found := metaPropertyList.GetMetaProperty(constants.AuthenticationProperty).GetData().(data.ListData).Get().Search(baseData.NewAccAddressData(accAddress))
+		return found, nil
+	}
 }
 
-// TODO Implement
-func ProvisionAddress(identity mappables.Identity, accAddress sdkTypes.AccAddress) mappables.Identity {
-	// identity.Document = identity.Document.Mutate(
-	// 	baseProperties.NewPropertyWithDataID(
-	// 		identity.GetAuthentication().GetID(),
-	// 		base.NewListDataID(identity.GetAuthentication().GetDataID().Add(baseData.NewAccAddressData(accAddress).GetID())))).(baseQualified.Document)
-	// return identity
-	panic("implement")
+func ProvisionAddress(context sdkTypes.Context, supplementAuxiliary helpers.Auxiliary, identity mappables.Identity, accAddress sdkTypes.AccAddress) (mappables.Identity, error) {
+	if metaPropertyList, err := supplement.GetMetaPropertiesFromResponse(supplementAuxiliary.GetKeeper().Help(context, supplement.NewAuxiliaryRequest(identity.GetAuthentication()))); err != nil {
+		return identity, err
+	} else {
+		identity.Mutate(base.NewProperty(constants.AuthenticationProperty, baseData.NewListData(metaPropertyList.GetMetaProperty(constants.AuthenticationProperty).GetData().(data.ListData).Get().Add(baseData.NewAccAddressData(accAddress)))))
+		return identity, nil
+	}
 }
 
-// TODO Implement
-func UnprovisionAddress(identity mappables.Identity, accAddress sdkTypes.AccAddress) mappables.Identity {
-	// identity.Document = identity.Document.Mutate(
-	// 	baseProperties.NewPropertyWithDataID(
-	// 		identity.GetAuthentication().GetID(),
-	// 		base.NewListDataID(identity.GetAuthentication().GetDataID().Remove(baseData.NewAccAddressData(accAddress).GetID())))).(baseQualified.Document)
-	// return identity
-	panic("implement")
+func UnprovisionAddress(context sdkTypes.Context, supplementAuxiliary helpers.Auxiliary, identity mappables.Identity, accAddress sdkTypes.AccAddress) (mappables.Identity, error) {
+	if metaPropertyList, err := supplement.GetMetaPropertiesFromResponse(supplementAuxiliary.GetKeeper().Help(context, supplement.NewAuxiliaryRequest(identity.GetAuthentication()))); err != nil {
+		return identity, err
+	} else {
+		identity.Mutate(base.NewProperty(constants.AuthenticationProperty, baseData.NewListData(metaPropertyList.GetMetaProperty(constants.AuthenticationProperty).GetData().(data.ListData).Get().Delete(baseData.NewAccAddressData(accAddress)))))
+		return identity, nil
+	}
 }
