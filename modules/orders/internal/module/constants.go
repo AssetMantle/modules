a9d7ed21a2ce40733f609660c96ee6113f39960c
deepanshutr <deepanshutr@gmail.com>
2022-08-30 13:58:04 +0530

    Refactoring: moving NubClassificationID from ID constants tp orders module.

--- modules/orders/internal/module/constants.go
+++ modules/orders/internal/module/constants.go
@@ -7,14 +7,15 @@ import (
 	baseData "github.com/AssetMantle/modules/schema/data/base"
 	"github.com/AssetMantle/modules/schema/helpers/constants"
 	baseIDs "github.com/AssetMantle/modules/schema/ids/base"
-	"github.com/AssetMantle/modules/schema/ids/constansts"
 	baseLists "github.com/AssetMantle/modules/schema/lists/base"
 	baseProperties "github.com/AssetMantle/modules/schema/properties/base"
 	constantProperties "github.com/AssetMantle/modules/schema/properties/constants"
 	baseQualified "github.com/AssetMantle/modules/schema/qualified/base"
+	qualifiedConstants "github.com/AssetMantle/modules/schema/qualified/constants"
 )
 
 const Name = "orders"
 
 var StoreKeyPrefix = constants.OrdersStoreKeyPrefix
-var ModuleIdentityID = baseIDs.NewIdentityID(constansts.NubClassificationID, baseQualified.NewImmutables(baseLists.NewPropertyList(baseProperties.NewMesaProperty(constantProperties.NubIDProperty.GetKey(), baseData.NewStringData(Name)))))
+var NubClassificationID = baseIDs.NewClassificationID(qualifiedConstants.NubImmutables, qualifiedConstants.NubMutables)
+var ModuleIdentityID = baseIDs.NewIdentityID(NubClassificationID, baseQualified.NewImmutables(baseLists.NewPropertyList(baseProperties.NewMesaProperty(constantProperties.NubIDProperty.GetKey(), baseData.NewStringData(Name)))))
