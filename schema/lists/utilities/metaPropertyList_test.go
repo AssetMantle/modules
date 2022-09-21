6bb6e434cdd0d61e318b8c61c59bf188723ea17f
Ankit Kumar <ankit.kr554@gmail.com>
2022-09-21 18:33:48 +0530

    test(schema): add mock for metaPropertyList

--- schema/lists/utilities/metaPropertyList_test.go
+++ schema/lists/utilities/metaPropertyList_test.go
@@ -4,7 +4,11 @@
 package utilities
 
 import (
+	base3 "github.com/AssetMantle/modules/schema/ids/base"
 	"github.com/AssetMantle/modules/schema/lists"
+	"github.com/AssetMantle/modules/schema/lists/base"
+	properties2 "github.com/AssetMantle/modules/schema/properties"
+	base2 "github.com/AssetMantle/modules/schema/properties/base"
 	"reflect"
 	"testing"
 )
@@ -20,6 +24,8 @@ func TestReadMetaProperties(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{"+ve with empty string", args{""}, base.NewMetaProperties([]properties2.MetaProperty{}...), false},
+		{"+ve", args{"ID:S|Data,ID1:S|Data1,ID2:S|Data2"}, base.NewMetaProperties([]properties2.MetaProperty{base2.NewMetaProperty(base3.NewID("ID"), base2.NewStringData("Data")), base2.NewMetaProperty(base3.NewID("ID1"), base2.NewStringData("Data1")), base2.NewMetaProperty(base3.NewID("ID2"), base2.NewStringData("Data2"))}...), false}, //TODO: Data & type is same but not matching
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
