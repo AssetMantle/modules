2f11c175472a8f691a691cba1c3158bf875e699a
Ankit Kumar <ankit.kr554@gmail.com>
2022-09-26 19:26:38 +0530

    test(identities): update unit test for mapper identity

--- modules/identities/internal/mapper/prototype_test.go
+++ modules/identities/internal/mapper/prototype_test.go
@@ -4,7 +4,10 @@
 package mapper
 
 import (
+	"github.com/AssetMantle/modules/modules/identities/internal/key"
+	"github.com/AssetMantle/modules/modules/identities/internal/mappable"
 	"github.com/AssetMantle/modules/schema/helpers"
+	baseHelpers "github.com/AssetMantle/modules/schema/helpers/base"
 	"reflect"
 	"testing"
 )
@@ -14,9 +17,8 @@ func TestPrototype(t *testing.T) {
 		name string
 		want helpers.Mapper
 	}{
-		// TODO: Add test cases.
-		// TODO: Pointer based unit tests required
-		//{"+ve", baseHelpers.NewMapper(key.Prototype, mappable.Prototype)},
+		//TODO: Same data and type but fails to equate
+		{"+ve", baseHelpers.NewMapper(key.Prototype, mappable.Prototype)},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
