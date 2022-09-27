f1a256ca73fcb858027e2036e48d547ad7c23dcd
Ankit Kumar <ankit.kr554@gmail.com>
2022-09-27 17:47:20 +0530

    test(maintainers): add unit test for verify keeper

--- modules/maintainers/auxiliaries/verify/keeper_test.go
+++ modules/maintainers/auxiliaries/verify/keeper_test.go
@@ -4,50 +4,89 @@
 package verify
 
 import (
-	"github.com/AssetMantle/modules/schema/errors/constants"
-	"reflect"
-	"testing"
-
+	"github.com/AssetMantle/modules/modules/maintainers/internal/key"
+	"github.com/AssetMantle/modules/modules/maintainers/internal/mappable"
+	"github.com/AssetMantle/modules/modules/maintainers/internal/parameters"
+	baseData "github.com/AssetMantle/modules/schema/data/base"
+	"github.com/AssetMantle/modules/schema/helpers"
+	baseHelpers "github.com/AssetMantle/modules/schema/helpers/base"
+	baseIDs "github.com/AssetMantle/modules/schema/ids/base"
+	baseLists "github.com/AssetMantle/modules/schema/lists/base"
+	baseProperties "github.com/AssetMantle/modules/schema/properties/base"
+	baseQualified "github.com/AssetMantle/modules/schema/qualified/base"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/store"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/vesting"
 	"github.com/cosmos/cosmos-sdk/x/params"
 	"github.com/stretchr/testify/require"
 	abciTypes "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/log"
 	tendermintDB "github.com/tendermint/tm-db"
-
-	"github.com/AssetMantle/modules/modules/maintainers/internal/key"
-	"github.com/AssetMantle/modules/modules/maintainers/internal/mappable"
-	"github.com/AssetMantle/modules/modules/maintainers/internal/parameters"
-	"github.com/AssetMantle/modules/schema"
-	"github.com/AssetMantle/modules/schema/helpers"
-	baseHelpers "github.com/AssetMantle/modules/schema/helpers/base"
-	baseIDs "github.com/AssetMantle/modules/schema/ids/base"
-	baseLists "github.com/AssetMantle/modules/schema/lists/base"
+	"reflect"
+	"testing"
 )
 
-type TestKeepers struct {
-	MaintainersKeeper helpers.AuxiliaryKeeper
+func Test_auxiliaryKeeper_Help(t *testing.T) {
+	storeKey := sdkTypes.NewKVStoreKey("test")
+	paramsStoreKey := sdkTypes.NewKVStoreKey("testParams")
+	paramsTransientStoreKeys := sdkTypes.NewTransientStoreKey("testParamsTransient")
+	Mapper := baseHelpers.NewMapper(key.Prototype, mappable.Prototype).Initialize(storeKey)
+
+	memDB := tendermintDB.NewMemDB()
+	commitMultiStore := store.NewCommitMultiStore(memDB)
+	commitMultiStore.MountStoreWithDB(storeKey, sdkTypes.StoreTypeIAVL, memDB)
+	commitMultiStore.MountStoreWithDB(paramsStoreKey, sdkTypes.StoreTypeIAVL, memDB)
+	commitMultiStore.MountStoreWithDB(paramsTransientStoreKeys, sdkTypes.StoreTypeTransient, memDB)
+
+	err := commitMultiStore.LoadLatestVersion()
+	require.Nil(t, err)
+
+	immutables := baseQualified.NewImmutables(baseLists.NewPropertyList(baseProperties.NewMesaProperty(baseIDs.NewStringID("ID2"), baseData.NewStringData("Data2"))))
+	mutables := baseQualified.NewMutables(baseLists.NewPropertyList(baseProperties.NewMesaProperty(baseIDs.NewStringID("ID1"), baseData.NewStringData("Data1"))))
+
+	classificationID := baseIDs.NewClassificationID(immutables, mutables)
+	identityID := baseIDs.NewIdentityID(classificationID, immutables)
+
+	context := sdkTypes.NewContext(commitMultiStore, abciTypes.Header{
+		ChainID: "test",
+	}, false, log.NewNopLogger())
+
+	type fields struct {
+		mapper helpers.Mapper
+	}
+	type args struct {
+		context sdkTypes.Context
+		request helpers.AuxiliaryRequest
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   helpers.AuxiliaryResponse
+	}{
+		// TODO: Test dependency on issue #95: https://github.com/AssetMantle/modules/issues/95
+		{"+ve", fields{Mapper}, args{context, NewAuxiliaryRequest(classificationID, identityID)}, newAuxiliaryResponse(nil)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			auxiliaryKeeper := auxiliaryKeeper{
+				mapper: tt.fields.mapper,
+			}
+			auxiliaryKeeper.mapper.NewCollection(context).Add(mappable.NewMaintainer(classificationID, immutables, mutables))
+			if got := auxiliaryKeeper.Help(tt.args.context, tt.args.request); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Help() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
-func CreateTestInput(t *testing.T) (sdkTypes.Context, TestKeepers) {
-	var Codec = codec.New()
-
-	schema.RegisterCodec(Codec)
-	sdkTypes.RegisterCodec(Codec)
-	codec.RegisterCrypto(Codec)
-	codec.RegisterEvidences(Codec)
-	vesting.RegisterCodec(Codec)
-	Codec.Seal()
-
+func Test_auxiliaryKeeper_Initialize(t *testing.T) {
 	storeKey := sdkTypes.NewKVStoreKey("test")
 	paramsStoreKey := sdkTypes.NewKVStoreKey("testParams")
 	paramsTransientStoreKeys := sdkTypes.NewTransientStoreKey("testParamsTransient")
 	Mapper := baseHelpers.NewMapper(key.Prototype, mappable.Prototype).Initialize(storeKey)
 	paramsKeeper := params.NewKeeper(
-		Codec,
+		codec.Cdc,
 		paramsStoreKey,
 		paramsTransientStoreKeys,
 	)
@@ -63,39 +102,49 @@ func CreateTestInput(t *testing.T) (sdkTypes.Context, TestKeepers) {
 	err := commitMultiStore.LoadLatestVersion()
 	require.Nil(t, err)
 
-	context := sdkTypes.NewContext(commitMultiStore, abciTypes.Header{
-		ChainID: "test",
-	}, false, log.NewNopLogger())
-
-	keepers := TestKeepers{
-		MaintainersKeeper: keeperPrototype().Initialize(Mapper, testParameters, []interface{}{}).(helpers.AuxiliaryKeeper),
+	type fields struct {
+		mapper helpers.Mapper
 	}
-
-	return context, keepers
+	type args struct {
+		mapper helpers.Mapper
+		in1    helpers.Parameters
+		in2    []interface{}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   helpers.Keeper
+	}{
+		// TODO: Test dependency on #96  https://github.com/AssetMantle/modules/issues/96
+		{"+ve with nil", fields{}, args{}, auxiliaryKeeper{}},
+		{"+ve", fields{Mapper}, args{Mapper, testParameters, []interface{}{}}, auxiliaryKeeper{Mapper}}, // TODO: type & data same but doesn't match
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			au := auxiliaryKeeper{
+				mapper: tt.fields.mapper,
+			}
+			if got := au.Initialize(tt.args.mapper, tt.args.in1, tt.args.in2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Initialize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
-func Test_Auxiliary_Keeper_Help(t *testing.T) {
-	context, keepers := CreateTestInput(t)
-
-	classificationID := baseIDs.NewStringID("classificationID")
-	identityID := baseIDs.NewStringID("identityID")
-
-	maintainerID := key.NewMaintainerID(classificationID, identityID)
-	keepers.MaintainersKeeper.(auxiliaryKeeper).mapper.NewCollection(context).Add(mappable.NewMaintainer(maintainerID, baseLists.NewPropertyList(), baseLists.NewPropertyList()))
-
-	t.Run("PositiveCase", func(t *testing.T) {
-		want := newAuxiliaryResponse(nil)
-		if got := keepers.MaintainersKeeper.Help(context, NewAuxiliaryRequest(classificationID, identityID)); !reflect.DeepEqual(got, want) {
-			t.Errorf("Transact() = %v, want %v", got, want)
-		}
-	})
-
-	t.Run("NegativeCase-Maintainer not present", func(t *testing.T) {
-		t.Parallel()
-		want := newAuxiliaryResponse(constants.EntityNotFound)
-		if got := keepers.MaintainersKeeper.Help(context, NewAuxiliaryRequest(baseIDs.NewStringID("classificationID1"), baseIDs.NewStringID("identityID1"))); !reflect.DeepEqual(got, want) {
-			t.Errorf("Transact() = %v, want %v", got, want)
-		}
-	})
-
+func Test_keeperPrototype(t *testing.T) {
+	tests := []struct {
+		name string
+		want helpers.AuxiliaryKeeper
+	}{
+		// TODO: Add test cases.
+		{"+ve", auxiliaryKeeper{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := keeperPrototype(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("keeperPrototype() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
