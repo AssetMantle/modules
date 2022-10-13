df4bba3271705aa113cf0244f246a8c9a55eb1e5
Ankit Kumar <ankit.kr554@gmail.com>
2022-10-13 19:41:12 +0530

    test(simulation): update unit test for simulation property

--- simulation/schema/types/base/property_test.go
+++ simulation/schema/types/base/property_test.go
@@ -13,11 +13,12 @@ func TestGenerateRandomProperty(t *testing.T) {
 	tests := []struct {
 		name      string
 		args      args
+		want      string
 		wantPanic bool
 	}{
 		// TODO: check for nil case
-		{"test for panic case", args{nil}, true},
-		{"test for type", args{rand.New(rand.NewSource(7))}, false},
+		{"test for panic case", args{nil}, "", true},
+		{"test for type", args{rand.New(rand.NewSource(7))}, "base.mesaProperty", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -27,8 +28,8 @@ func TestGenerateRandomProperty(t *testing.T) {
 					t.Errorf("GenerateRandomProperty() recover = %v, wantPanic = %v", r, tt.wantPanic)
 				}
 			}()
-			if got := GenerateRandomProperty(tt.args.r); reflect.TypeOf(got).String() != "base.property" {
-				t.Errorf("GenerateRandomProperty() = %v, want base.property", got)
+			if got := GenerateRandomProperty(tt.args.r); !reflect.DeepEqual(reflect.TypeOf(got).String(), tt.want) {
+				t.Errorf("GenerateRandomProperty() = %v, want %v", reflect.TypeOf(got).String(), tt.want)
 			}
 		})
 	}
