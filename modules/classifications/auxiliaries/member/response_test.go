317748bd4904137ead4a2af0742cf7834787ae11
Ankit Kumar <ankit.kr554@gmail.com>
2022-10-13 19:57:41 +0530

    test(classifications): update unit test for request member classification

--- modules/classifications/auxiliaries/member/response_test.go
+++ modules/classifications/auxiliaries/member/response_test.go
@@ -4,11 +4,10 @@
 package member
 
 import (
+	"github.com/AssetMantle/modules/schema/errors/constants"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/AssetMantle/modules/constants/errors"
 )
 
 func Test_Conform_Response(t *testing.T) {
@@ -17,8 +16,8 @@ func Test_Conform_Response(t *testing.T) {
 	require.Equal(t, true, testAuxiliaryResponse.IsSuccessful())
 	require.Equal(t, nil, testAuxiliaryResponse.GetError())
 
-	testAuxiliaryResponse2 := newAuxiliaryResponse(errors.IncorrectFormat)
-	require.Equal(t, auxiliaryResponse{Success: false, Error: errors.IncorrectFormat}, testAuxiliaryResponse2)
+	testAuxiliaryResponse2 := newAuxiliaryResponse(constants.IncorrectFormat)
+	require.Equal(t, auxiliaryResponse{Success: false, Error: constants.IncorrectFormat}, testAuxiliaryResponse2)
 	require.Equal(t, false, testAuxiliaryResponse2.IsSuccessful())
-	require.Equal(t, errors.IncorrectFormat, testAuxiliaryResponse2.GetError())
+	require.Equal(t, constants.IncorrectFormat, testAuxiliaryResponse2.GetError())
 }
