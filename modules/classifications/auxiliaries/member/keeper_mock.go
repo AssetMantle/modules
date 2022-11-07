ff8ccefeaa3a33b52af123d098f8ea2678e95a28
Ankit Kumar <ankit.kr554@gmail.com>
2022-11-07 17:21:34 +0530

    test(classification): refactoring

--- modules/classifications/auxiliaries/member/keeper_mock.go
+++ modules/classifications/auxiliaries/member/keeper_mock.go
@@ -4,6 +4,7 @@
 package member
 
 import (
+	"github.com/AssetMantle/modules/schema/documents"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/AssetMantle/modules/modules/classifications/internal/key"
@@ -11,7 +12,6 @@ import (
 	errorConstants "github.com/AssetMantle/modules/schema/errors/constants"
 	"github.com/AssetMantle/modules/schema/helpers"
 	baseIDs "github.com/AssetMantle/modules/schema/ids/base"
-	"github.com/AssetMantle/modules/schema/types"
 )
 
 type auxiliaryKeeperMock struct {
@@ -28,7 +28,7 @@ func (auxiliaryKeeper auxiliaryKeeperMock) Help(context sdkTypes.Context, reques
 		if Mappable == nil {
 			return newAuxiliaryResponse(errorConstants.EntityNotFound)
 		}
-		classification := Mappable.(types.Classification)
+		classification := Mappable.(documents.Classification)
 		if auxiliaryRequest.Mutables != nil {
 			if len(auxiliaryRequest.Mutables.GetMutablePropertyList().GetList()) > len(classification.GetMutables().GetMutablePropertyList().GetList()) {
 				return newAuxiliaryResponse(errorConstants.IncorrectFormat)
