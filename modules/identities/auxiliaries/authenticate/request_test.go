ae661b197051685d46f3e768ac6659ef519fed95
Ankit Kumar <ankit.kr554@gmail.com>
2022-11-10 19:43:52 +0530

    test(identity): add tests for authenticate auxilaries identity

--- modules/identities/auxiliaries/authenticate/request_test.go
+++ modules/identities/auxiliaries/authenticate/request_test.go
@@ -4,11 +4,28 @@
 package authenticate
 
 import (
+	baseData "github.com/AssetMantle/modules/schema/data/base"
 	"github.com/AssetMantle/modules/schema/helpers"
 	"github.com/AssetMantle/modules/schema/ids"
+	baseIDs "github.com/AssetMantle/modules/schema/ids/base"
+	"github.com/AssetMantle/modules/schema/lists/base"
+	base3 "github.com/AssetMantle/modules/schema/properties/base"
+	baseQualified "github.com/AssetMantle/modules/schema/qualified/base"
 	"github.com/cosmos/cosmos-sdk/types"
 	"reflect"
 	"testing"
+)
+
+var (
+	mutableProperties      = base.NewPropertyList(base3.NewMetaProperty(baseIDs.NewStringID("authentication"), baseData.NewListData(base.NewDataList())))
+	immutableProperties    = base.NewPropertyList(base3.NewMetaProperty(baseIDs.NewStringID("ID1"), baseData.NewListData(base.NewDataList())))
+	toMutateMetaProperties = base.NewPropertyList(base3.NewMetaProperty(baseIDs.NewStringID("ID1"), baseData.NewListData(base.NewDataList(base.NewStringData("Test")))))
+	immutables             = baseQualified.NewImmutables(immutableProperties)
+	mutables               = baseQualified.NewMutables(mutableProperties)
+	testClassificationID   = baseIDs.NewClassificationID(immutables, mutables)
+	testFromID             = baseIDs.NewIdentityID(testClassificationID, immutables)
+	fromAddress            = "cosmos1pkkayn066msg6kn33wnl5srhdt3tnu2vzasz9c"
+	fromAccAddress, _      = types.AccAddressFromBech32(fromAddress)
 )
 
 func TestNewAuxiliaryRequest(t *testing.T) {
@@ -21,7 +38,7 @@ func TestNewAuxiliaryRequest(t *testing.T) {
 		args args
 		want helpers.AuxiliaryRequest
 	}{
-		// TODO: Add test cases.
+		{"+ve", args{fromAccAddress, testFromID}, auxiliaryRequest{fromAccAddress, testFromID}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -41,7 +58,8 @@ func Test_auxiliaryRequestFromInterface(t *testing.T) {
 		args args
 		want auxiliaryRequest
 	}{
-		// TODO: Add test cases.
+		{"+ve with nil", args{}, auxiliaryRequest{}},
+		{"+ve", args{auxiliaryRequest{fromAccAddress, testFromID}}, auxiliaryRequest{fromAccAddress, testFromID}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -62,7 +80,8 @@ func Test_auxiliaryRequest_Validate(t *testing.T) {
 		fields  fields
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{"+ve with nil", fields{}, true},
+		{"+ve", fields{fromAccAddress, testFromID}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
