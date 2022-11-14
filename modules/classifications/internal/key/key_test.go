ec259e80c6dc6f8c8811aeb0f243d07259552a01
Ankit Kumar <ankit.kr554@gmail.com>
2022-11-14 16:35:47 +0530

    test(classification): add test for classification keys

---
+++ modules/classifications/internal/key/key_test.go
@@ -0,0 +1,188 @@
+// Copyright [2021] - [2022], AssetMantle Pte. Ltd. and the code contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package key
+
+import (
+	baseData "github.com/AssetMantle/modules/schema/data/base"
+	"github.com/AssetMantle/modules/schema/helpers"
+	"github.com/AssetMantle/modules/schema/ids"
+	baseIDs "github.com/AssetMantle/modules/schema/ids/base"
+	"github.com/AssetMantle/modules/schema/lists/base"
+	baseProperties "github.com/AssetMantle/modules/schema/properties/base"
+	baseQualified "github.com/AssetMantle/modules/schema/qualified/base"
+	baseHelpers "github.com/AssetMantle/modules/utilities/test/schema/helpers/base"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"reflect"
+	"testing"
+)
+
+func createTestInput() ids.ClassificationID {
+	immutables := baseQualified.NewImmutables(base.NewPropertyList(baseProperties.NewMesaProperty(baseIDs.NewStringID("ID1"), baseData.NewStringData("ImmutableData"))))
+	mutables := baseQualified.NewMutables(base.NewPropertyList(baseProperties.NewMesaProperty(baseIDs.NewStringID("ID2"), baseData.NewStringData("MutableData"))))
+	classificationID := baseIDs.NewClassificationID(immutables, mutables)
+	return classificationID
+}
+
+func TestNewKey(t *testing.T) {
+	type args struct {
+		classificationID ids.ClassificationID
+	}
+	tests := []struct {
+		name string
+		args args
+		want helpers.Key
+	}{
+		// TODO: Add test cases.
+		{"+ve", args{createTestInput()}, key{createTestInput()}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewKey(tt.args.classificationID); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPrototype(t *testing.T) {
+	tests := []struct {
+		name string
+		want helpers.Key
+	}{
+		{"+ve", key{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Prototype(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Prototype() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_keyFromInterface(t *testing.T) {
+	type args struct {
+		i interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    key
+		wantErr bool
+	}{
+		{"+ve", args{NewKey(createTestInput())}, key{createTestInput()}, false},
+		{"+ve MetaDataError", args{}, key{createTestInput()}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := keyFromInterface(tt.args.i)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("keyFromInterface() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("keyFromInterface() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_key_Equals(t *testing.T) {
+	type fields struct {
+		ClassificationID ids.ClassificationID
+	}
+	type args struct {
+		compareKey helpers.Key
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{"+ve", fields{createTestInput()}, args{key{createTestInput()}}, true},
+		{"+ve", fields{createTestInput()}, args{baseHelpers.KeyPrototype()}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			key := key{
+				ClassificationID: tt.fields.ClassificationID,
+			}
+			if got := key.Equals(tt.args.compareKey); got != tt.want {
+				t.Errorf("Equals() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_key_GenerateStoreKeyBytes(t *testing.T) {
+	type fields struct {
+		ClassificationID ids.ClassificationID
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []byte
+	}{
+		{"+ve", fields{createTestInput()}, []byte{9, 0, 24, 22, 48, 87, 145, 5, 101, 141, 249, 122, 119, 42, 90, 8, 102, 253, 238, 132, 47, 149, 50, 233, 9, 216, 124, 172, 149, 35, 55, 176, 219, 128}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			key := key{
+				ClassificationID: tt.fields.ClassificationID,
+			}
+			if got := key.GenerateStoreKeyBytes(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GenerateStoreKeyBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_key_IsPartial(t *testing.T) {
+	type fields struct {
+		ClassificationID ids.ClassificationID
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{"+ve", fields{createTestInput()}, false},
+		{"-ve", fields{baseIDs.PrototypeClassificationID()}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			key := key{
+				ClassificationID: tt.fields.ClassificationID,
+			}
+			if got := key.IsPartial(); got != tt.want {
+				t.Errorf("IsPartial() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_key_RegisterCodec(t *testing.T) {
+	type fields struct {
+		ClassificationID ids.ClassificationID
+	}
+	type args struct {
+		codec *codec.Codec
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{"+ve", fields{createTestInput()}, args{codec.New()}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ke := key{
+				ClassificationID: tt.fields.ClassificationID,
+			}
+			ke.RegisterCodec(tt.args.codec)
+		})
+	}
+}
