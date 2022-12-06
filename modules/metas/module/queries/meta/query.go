c2ae0e1d4ce4cd33c7feca359ce8d091c177fe5a
mukundrawat <mukund@assetmantle.one>
2022-12-06 20:38:17 +0530

    new changes for interfaces

--- modules/metas/module/queries/meta/query.go
+++ modules/metas/module/queries/meta/query.go
@@ -4,21 +4,42 @@
 package meta
 
 import (
+	"context"
 	"github.com/AssetMantle/modules/modules/metas/module/module"
+	"github.com/AssetMantle/modules/schema/helpers"
 	baseHelpers "github.com/AssetMantle/modules/schema/helpers/base"
 	"github.com/AssetMantle/modules/schema/helpers/constants"
+	"github.com/cosmos/cosmos-sdk/client"
+	sdkModuleTypes "github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 )
 
-var Query = baseHelpers.NewQuery(
-	"metas",
-	"",
-	"",
+type Configurator struct{}
 
-	module.Name,
+func (c Configurator) ConfigureGRPCServer(cfg sdkModuleTypes.Configurator) {
+	RegisterQueryServer(cfg.QueryServer(), queryKeeper{})
+}
 
-	requestPrototype,
-	responsePrototype,
-	keeperPrototype,
+func (c Configurator) ConfigureGRPCGatewayHandler(clientCtx client.Context, mux *runtime.ServeMux) {
+	RegisterQueryHandlerClient(context.Background(), mux, NewQueryClient(clientCtx))
+}
 
-	constants.DataID,
-)
+var _ helpers.GRPCConfigurator = &Configurator{}
+
+func Query() helpers.Query {
+	query := baseHelpers.NewQuery(
+		"metas",
+		"",
+		"",
+
+		module.Name,
+
+		requestPrototype,
+		responsePrototype,
+		keeperPrototype,
+		Configurator{},
+
+		constants.DataID,
+	)
+	return query
+}
