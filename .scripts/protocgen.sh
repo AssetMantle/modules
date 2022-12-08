c26207a7f0763be392abf7d3a9d019ca71520522
deepanshutr <deepanshutr@gmail.com>
2022-12-08 20:06:23 +0530

    fixing types

--- .scripts/protocgen.sh
+++ .scripts/protocgen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -xeo pipefail
 
 # get protoc executions
 go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
@@ -10,12 +10,12 @@ go get github.com/cosmos/cosmos-sdk@v0.45.9 2>/dev/null
 
 echo "Generating gogo proto code"
 
-proto_dirs=$(find ./modules -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -name '*.proto'); do
-    if grep go_package $file &>/dev/null; then
+    #if grep go_package $file &>/dev/null; then
       buf generate --template buf.gen.yaml $file
-    fi
+    #fi
   done
 done
 
