d696bf2b0a4cfab80b6218e079e036fc43b86b85
deepanshutr <deepanshutr@gmail.com>
2022-12-08 21:41:58 +0530

    fixing types

--- .scripts/protocgen.sh
+++ .scripts/protocgen.sh
@@ -9,15 +9,3 @@ go install github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 # get cosmos sdk from github
 go get github.com/cosmos/cosmos-sdk@v0.45.9 2>/dev/null
 
-
-echo "Generating gogo proto code"
-
-proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  for file in $(find "${dir}" -name '*.proto'); do
-    #if grep go_package $file &>/dev/null; then
-      buf generate --template buf.gen.yaml $file
-    #fi
-  done
-done
-
