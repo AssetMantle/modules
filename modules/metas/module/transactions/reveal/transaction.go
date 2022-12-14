d16538ecb419d387b1fb7e104965aac9bd8cc1e2
mukundrawat <mukund@assetmantle.one>
2022-12-14 19:33:50 +0530

    temporary changes for grpc gateway

--- modules/metas/module/transactions/reveal/transaction.go
+++ modules/metas/module/transactions/reveal/transaction.go
@@ -4,9 +4,26 @@
 package reveal
 
 import (
+	"context"
+	"github.com/AssetMantle/modules/schema/helpers"
 	baseHelpers "github.com/AssetMantle/modules/schema/helpers/base"
 	"github.com/AssetMantle/modules/schema/helpers/constants"
+	"github.com/cosmos/cosmos-sdk/client"
+	sdkModuleTypes "github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 )
+
+type Configurator struct{}
+
+var _ helpers.GRPCConfigurator = &Configurator{}
+
+func (c Configurator) ConfigureGRPCServer(cfg sdkModuleTypes.Configurator) {
+	RegisterTransactionServer(cfg.MsgServer(), transactionKeeper{})
+}
+
+func (c Configurator) ConfigureGRPCGatewayHandler(clientCtx client.Context, mux *runtime.ServeMux) {
+	RegisterTransactionHandlerClient(context.Background(), mux, NewTransactionClient(clientCtx))
+}
 
 var Transaction = baseHelpers.NewTransaction(
 	"reveal",
