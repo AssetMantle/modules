33fecbd3ab6397cb2e97b68898f69f0d6b9900f7
Ankit Kumar <ankit.kr554@gmail.com>
2022-12-15 15:42:54 +0530

    fix(schema): add sanitize method for documents

--- schema/documents/base/document.go
+++ schema/documents/base/document.go
@@ -6,9 +6,11 @@ package base
 import (
 	"github.com/AssetMantle/modules/schema/documents"
 	"github.com/AssetMantle/modules/schema/ids"
-	"github.com/AssetMantle/modules/schema/ids/base"
+	baseIDs "github.com/AssetMantle/modules/schema/ids/base"
+	baseLists "github.com/AssetMantle/modules/schema/lists/base"
 	"github.com/AssetMantle/modules/schema/properties"
 	"github.com/AssetMantle/modules/schema/qualified"
+	baseQualified "github.com/AssetMantle/modules/schema/qualified/base"
 )
 
 type document struct {
@@ -20,12 +22,20 @@ type document struct {
 var _ documents.Document = (*document)(nil)
 
 func (document document) GenerateHashID() ids.HashID {
-	return base.GenerateHashID(document.GetClassificationID().Bytes(), document.GetImmutables().GenerateHashID().Bytes())
+	return baseIDs.GenerateHashID(document.GetClassificationID().Bytes(), document.GetImmutables().GenerateHashID().Bytes())
 }
 func (document document) GetClassificationID() ids.ClassificationID {
+	if document.ClassificationID == nil {
+		return baseIDs.PrototypeClassificationID()
+	}
 	return document.ClassificationID
 }
 func (document document) GetProperty(propertyID ids.PropertyID) properties.Property {
+	if sanitizedDocument, err := document.Sanitize(); err == nil {
+		document.Immutables = sanitizedDocument.(documents.Document).GetImmutables()
+		document.Mutables = sanitizedDocument.(documents.Document).GetMutables()
+		document.ClassificationID = sanitizedDocument.(documents.Document).GetClassificationID()
+	}
 	if property := document.Immutables.GetImmutablePropertyList().GetProperty(propertyID); property != nil {
 		return property
 	} else if property := document.Mutables.GetMutablePropertyList().GetProperty(propertyID); property != nil {
@@ -35,16 +45,41 @@ func (document document) GetProperty(propertyID ids.PropertyID) properties.Prope
 	}
 }
 func (document document) GetImmutables() qualified.Immutables {
+	if document.Immutables == nil {
+		return baseQualified.NewImmutables(baseLists.NewPropertyList())
+	}
 	return document.Immutables
 }
 func (document document) GetMutables() qualified.Mutables {
+	if document.Mutables == nil {
+		return baseQualified.NewMutables(baseLists.NewPropertyList())
+	}
 	return document.Mutables
 }
 
 // TODO write test case
 func (document document) Mutate(propertyList ...properties.Property) documents.Document {
+	if sanitizedDocument, err := document.Sanitize(); err == nil {
+		document.Immutables = sanitizedDocument.(documents.Document).GetImmutables()
+		document.Mutables = sanitizedDocument.(documents.Document).GetMutables()
+		document.ClassificationID = sanitizedDocument.(documents.Document).GetClassificationID()
+	}
 	document.Mutables = document.Mutables.Mutate(propertyList...)
 	return document
+}
+
+func (document document) Sanitize() (documents.Document, error) {
+	if document.ClassificationID == nil {
+		document.ClassificationID = baseIDs.PrototypeClassificationID()
+	}
+	if document.Immutables == nil {
+		document.Immutables = baseQualified.NewImmutables(baseLists.NewPropertyList())
+	}
+	if document.Mutables == nil {
+		document.Mutables = baseQualified.NewMutables(baseLists.NewPropertyList())
+	}
+	//panic("implement me")
+	return document, nil
 }
 
 func NewDocument(classificationID ids.ClassificationID, immutables qualified.Immutables, mutables qualified.Mutables) documents.Document {
