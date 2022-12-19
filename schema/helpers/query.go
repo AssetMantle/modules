8bd51c2a13d1996aaa2ac648d46aec97b3ca53fb
deepanshutr <deepanshutr@gmail.com>
2022-12-20 01:23:30 +0530

    feat(query): adding RegisterService and RegisterGRPCGatewayRoute

--- schema/helpers/query.go
+++ schema/helpers/query.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/client"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/spf13/cobra"
 	abciTypes "github.com/tendermint/tendermint/abci/types"
 )
@@ -17,5 +19,7 @@ type Query interface {
 	Command() *cobra.Command
 	HandleMessage(sdkTypes.Context, abciTypes.RequestQuery) ([]byte, error)
 	RESTQueryHandler(client.Context) http.HandlerFunc
+	RegisterService(module.Configurator)
+	RegisterGRPCGatewayRoute(client.Context, *runtime.ServeMux)
 	Initialize(Mapper, Parameters, ...interface{}) Query
 }
