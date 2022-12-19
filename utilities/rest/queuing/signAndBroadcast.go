d75254f727ab7565a38d9e461a5c97380bd59f82
mukundrawat <mukund@assetmantle.one>
2022-12-19 21:10:45 +0530

    updating rest query handler

--- utilities/rest/queuing/signAndBroadcast.go
+++ utilities/rest/queuing/signAndBroadcast.go
@@ -4,6 +4,7 @@
 package queuing
 
 import (
+	"github.com/cosmos/cosmos-sdk/client/tx"
 	"strings"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -11,16 +12,12 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/keys"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
-	authClient "github.com/cosmos/cosmos-sdk/x/auth/client"
-	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 )
 
 func signAndBroadcastMultiple(kafkaMsgList []kafkaMsg, context client.Context) ([]byte, error) {
-	var stdTxs legacytx.StdTx
-
 	var txBytes []byte
 
 	msgList := make([]sdkTypes.Msg, len(kafkaMsgList))
@@ -36,7 +33,7 @@ func signAndBroadcastMultiple(kafkaMsgList []kafkaMsg, context client.Context) (
 			return nil, err
 		}
 
-		simAndExec, gas, err := flags.ParseGas(kafkaMsg.BaseRequest.Gas)
+		gasSetting, err := flags.ParseGasSetting(kafkaMsg.BaseRequest.Gas)
 		if err != nil {
 			return nil, err
 		}
@@ -46,7 +43,7 @@ func signAndBroadcastMultiple(kafkaMsgList []kafkaMsg, context client.Context) (
 			return nil, err
 		}
 
-		accountNumber, sequence, err := types.NewAccountRetriever(context).GetAccountNumberSequence(context.FromAddress)
+		accountNumber, sequence, err := types.AccountRetriever{}.GetAccountNumberSequence(context, context.FromAddress)
 		if err != nil {
 			return nil, err
 		}
@@ -62,56 +59,54 @@ func signAndBroadcastMultiple(kafkaMsgList []kafkaMsg, context client.Context) (
 		}
 
 		sequence += count
-		txBuilder := types.NewTxBuilder(
-			authClient.GetTxEncoder(context.LegacyAmino), accountNumber, sequence, gas, gasAdj,
-			kafkaMsg.BaseRequest.Simulate, kafkaMsg.BaseRequest.ChainID, kafkaMsg.BaseRequest.Memo, kafkaMsg.BaseRequest.Fees, kafkaMsg.BaseRequest.GasPrices,
-		)
 
-		txBuilder = txBuilder.WithKeybase(keyBase)
+		txFactory := tx.Factory{}.
+			WithAccountNumber(kafkaMsg.BaseRequest.AccountNumber).
+			WithSequence(kafkaMsg.BaseRequest.Sequence).
+			WithGas(gasSetting.Gas).
+			WithGasAdjustment(gasAdj).
+			WithMemo(kafkaMsg.BaseRequest.Memo).
+			WithChainID(kafkaMsg.BaseRequest.ChainID).
+			WithSimulateAndExecute(kafkaMsg.BaseRequest.Simulate).
+			WithTxConfig(context.TxConfig).
+			WithTimeoutHeight(kafkaMsg.BaseRequest.TimeoutHeight).
+			WithFees(kafkaMsg.BaseRequest.Fees.String()).
+			WithGasPrices(kafkaMsg.BaseRequest.GasPrices.String()).
+			WithKeybase(keyBase)
 
-		if kafkaMsg.BaseRequest.Simulate || simAndExec {
+		if kafkaMsg.BaseRequest.Simulate || gasSetting.Simulate {
 			if gasAdj < 0 {
 				return nil, errors.New("Error invalid gas adjustment")
 			}
 
-			txBuilder, err = authClient.EnrichWithGas(txBuilder, context, []sdkTypes.Msg{kafkaMsg.Msg})
+			_, adjusted, err := tx.CalculateGas(context, txFactory, kafkaMsg.Msg)
+
 			if err != nil {
 				return nil, err
 			}
 
+			txFactory = txFactory.WithGas(adjusted)
+
 			if kafkaMsg.BaseRequest.Simulate {
-				val, _ := simulationResponse(context.LegacyAmino, txBuilder.Gas())
+				val, _ := simulationResponse(context.LegacyAmino, txFactory.Gas())
 				return val, nil
 			}
 		}
 
-		stdMsg, err := txBuilder.BuildSignMsg(msgList)
+		txBuilder, err := txFactory.BuildUnsignedTx(msgList...)
 		if err != nil {
 			return nil, err
 		}
 
-		stdTx := legacytx.NewStdTx(stdMsg.Msgs, stdMsg.Fee, nil, stdMsg.Memo)
-
-		stdTx, err = txBuilder.SignStdTx(context.FromName, keys.DefaultKeyPass, stdTx, true)
+		err = tx.Sign(txFactory, context.FromName, txBuilder, true)
 		if err != nil {
 			return nil, err
 		}
 
-		if i == 0 {
-			stdTxs.Msgs = stdTx.Msgs
-			stdTxs.Fee = stdTx.Fee
-			stdTxs.Memo = stdTx.Memo
-		}
+		txBytes, err = context.TxConfig.TxEncoder()(txBuilder.GetTx())
 
-		if count == 0 {
-			stdTxs.Signatures = append(stdTxs.Signatures, stdTx.Signatures...)
-		}
-
-		if i == len(kafkaMsgList)-1 {
-			txBytes, err = txBuilder.TxEncoder()(stdTxs)
-			if err != nil {
-				return nil, err
-			}
+		if err != nil {
+			return nil, err
 		}
 	}
 
