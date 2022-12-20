c7ca52bed7bac05b8c2389b33bee754374724564
mukundrawat <mukund@assetmantle.one>
2022-12-21 03:02:46 +0530

    listables, properties,

--- schema/types/base/split.go
+++ schema/types/base/split.go
@@ -1,45 +1,46 @@
 package base
 
 import (
+	"github.com/AssetMantle/modules/schema/ids/base"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/AssetMantle/modules/schema/ids"
 	"github.com/AssetMantle/modules/schema/types"
 )
 
-type split struct {
-	OwnerID   ids.IdentityID
-	OwnableID ids.OwnableID
-	Value     sdkTypes.Dec
-}
+//type split struct {
+//	OwnerID   ids.IdentityID
+//	OwnableID ids.OwnableID
+//	Value     sdkTypes.Dec
+//}
 
-var _ types.Split = (*split)(nil)
+var _ types.Split = (*Split)(nil)
 
-func (split split) GetOwnerID() ids.IdentityID {
-	return split.OwnerID
+func (split *Split) GetOwnerID() ids.IdentityID {
+	return split.OwnerId
 }
-func (split split) GetOwnableID() ids.OwnableID {
-	return split.OwnableID
+func (split *Split) GetOwnableID() ids.OwnableID {
+	return split.OwnableId
 }
-func (split split) GetValue() sdkTypes.Dec {
+func (split *Split) GetValue() sdkTypes.Dec {
 	return split.Value
 }
-func (split split) Send(outValue sdkTypes.Dec) types.Split {
+func (split *Split) Send(outValue sdkTypes.Dec) types.Split {
 	split.Value = split.Value.Sub(outValue)
 	return split
 }
-func (split split) Receive(inValue sdkTypes.Dec) types.Split {
+func (split *Split) Receive(inValue sdkTypes.Dec) types.Split {
 	split.Value = split.Value.Add(inValue)
 	return split
 }
-func (split split) CanSend(outValue sdkTypes.Dec) bool {
+func (split *Split) CanSend(outValue sdkTypes.Dec) bool {
 	return split.Value.GTE(outValue)
 }
 
 func NewSplit(ownerID ids.IdentityID, ownableID ids.OwnableID, value sdkTypes.Dec) types.Split {
-	return split{
-		OwnerID:   ownerID,
-		OwnableID: ownableID,
+	return &Split{
+		OwnerId:   ownerID.(*base.IdentityID),
+		OwnableId: ownableID.(*base.OwnableID),
 		Value:     value,
 	}
 }
