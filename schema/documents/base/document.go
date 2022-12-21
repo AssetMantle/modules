7a8ad1ec1c40490c45db968026ecd252613d33da
mukundrawat <mukund@assetmantle.one>
2022-12-21 15:27:52 +0530

    listables, properties,

--- schema/documents/base/document.go
+++ schema/documents/base/document.go
@@ -14,7 +14,7 @@ import (
 
 //type document struct {
 //	ids.ClassificationID
-//	qualified.ImmutablesList
+//	qualified.ImmutableList
 //	qualified.Mutables
 //}
 
@@ -24,9 +24,9 @@ func (document *Document) GenerateHashID() ids.HashID {
 	return baseIDs.GenerateHashID(document.GetClassificationID().Bytes(), document.GetImmutables().GenerateHashID().Bytes())
 }
 func (document *Document) GetProperty(propertyID ids.PropertyID) properties.Property {
-	if property := document.ImmutablesList.GetProperty(propertyID); property != nil {
+	if property := document.ImmutableList.GetProperty(propertyID); property != nil {
 		return property
-	} else if property := document.MutablesList.GetProperty(propertyID); property != nil {
+	} else if property := document.MutableList.GetProperty(propertyID); property != nil {
 		return property
 	} else {
 		return nil
@@ -36,22 +36,22 @@ func (document *Document) GetClassificationID() ids.ClassificationID {
 	return document.ClassificationId
 }
 func (document *Document) GetImmutables() qualified.Immutables {
-	return document.ImmutablesList
+	return document.ImmutableList
 }
 func (document *Document) GetMutables() qualified.Mutables {
-	return document.MutablesList
+	return document.MutableList
 }
 
 // TODO write test case
 func (document *Document) Mutate(propertyList ...properties.Property) documents.Document {
-	document.MutablesList = document.MutablesList.Mutate(propertyList...).(*base.Mutables)
+	document.MutableList = document.MutableList.Mutate(propertyList...).(*base.Mutables)
 	return document
 }
 
 func NewDocument(classificationID ids.ID, immutables qualified.Immutables, mutables qualified.Mutables) documents.Document {
 	return &Document{
 		ClassificationId: classificationID.(*baseIDs.AnyID),
-		ImmutablesList:   immutables.(*base.Immutables),
-		MutablesList:     mutables.(*base.Mutables),
+		ImmutableList:    immutables.(*base.Immutables),
+		MutableList:      mutables.(*base.Mutables),
 	}
 }
