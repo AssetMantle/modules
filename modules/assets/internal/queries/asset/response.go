6e95d273da0740b387b7fdf0530c9930ec2902c1
mukundrawat <mukund@assetmantle.one>
2022-12-22 17:31:00 +0530

    listables, properties,

--- modules/assets/internal/queries/asset/response.go
+++ modules/assets/internal/queries/asset/response.go
@@ -8,24 +8,24 @@ import (
 	"github.com/AssetMantle/modules/schema/helpers"
 )
 
-type queryResponse struct {
-	Success bool               `json:"success"`
-	Error   error              `json:"error" swaggertype:"string"`
-	List    []helpers.Mappable `json:"list"`
-}
+//type queryResponse struct {
+//	Success bool               `json:"success"`
+//	Error   error              `json:"error" swaggertype:"string"`
+//	List    []helpers.Mappable `json:"list"`
+//}
 
-var _ helpers.QueryResponse = (*queryResponse)(nil)
+var _ helpers.QueryResponse = (*QueryResponse)(nil)
 
-func (queryResponse queryResponse) IsSuccessful() bool {
+func (queryResponse *QueryResponse) IsSuccessful() bool {
 	return queryResponse.Success
 }
-func (queryResponse queryResponse) GetError() error {
+func (queryResponse *QueryResponse) GetError() error {
 	return queryResponse.Error
 }
-func (queryResponse queryResponse) Encode() ([]byte, error) {
+func (queryResponse *QueryResponse) Encode() ([]byte, error) {
 	return common.LegacyAmino.MarshalJSON(queryResponse)
 }
-func (queryResponse queryResponse) Decode(bytes []byte) (helpers.QueryResponse, error) {
+func (queryResponse *QueryResponse) Decode(bytes []byte) (helpers.QueryResponse, error) {
 	if err := common.LegacyAmino.UnmarshalJSON(bytes, &queryResponse); err != nil {
 		return nil, err
 	}
@@ -33,7 +33,7 @@ func (queryResponse queryResponse) Decode(bytes []byte) (helpers.QueryResponse, 
 	return queryResponse, nil
 }
 func responsePrototype() helpers.QueryResponse {
-	return queryResponse{}
+	return &QueryResponse{}
 }
 func newQueryResponse(collection helpers.Collection, error error) helpers.QueryResponse {
 	success := true
@@ -41,7 +41,7 @@ func newQueryResponse(collection helpers.Collection, error error) helpers.QueryR
 		success = false
 	}
 
-	return queryResponse{
+	return &QueryResponse{
 		Success: success,
 		Error:   error,
 		List:    collection.GetList(),
