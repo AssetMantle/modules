94c8755658265c073c38141b1446582852e488b2
mukundrawat <mukund@assetmantle.one>
2022-12-26 16:51:25 +0530

    property impls for getters

--- schema/ids/base/anyID.go
+++ schema/ids/base/anyID.go
@@ -5,7 +5,55 @@ import (
 	"github.com/AssetMantle/modules/schema/traits"
 )
 
-var _ ids.ID = (*AnyID)(nil)
+type getter interface {
+	getID() ids.ID
+}
+
+var _ getter = (*AnyID_ClassificationID)(nil)
+var _ getter = (*AnyID_AssetID)(nil)
+var _ getter = (*AnyID_DataID)(nil)
+var _ getter = (*AnyID_HashID)(nil)
+var _ getter = (*AnyID_IdentityID)(nil)
+var _ getter = (*AnyID_MaintainerID)(nil)
+var _ getter = (*AnyID_OrderID)(nil)
+var _ getter = (*AnyID_OwnableID)(nil)
+var _ getter = (*AnyID_PropertyID)(nil)
+var _ getter = (*AnyID_SplitID)(nil)
+var _ getter = (*AnyID_StringID)(nil)
+
+func (m *AnyID_ClassificationID) getID() ids.ID {
+	return m.ClassificationID
+}
+func (m *AnyID_AssetID) getID() ids.ID {
+	return m.AssetID
+}
+func (m *AnyID_DataID) getID() ids.ID {
+	return m.DataID
+}
+func (m *AnyID_HashID) getID() ids.ID {
+	return m.HashID
+}
+func (m *AnyID_IdentityID) getID() ids.ID {
+	return m.IdentityID
+}
+func (m *AnyID_MaintainerID) getID() ids.ID {
+	return m.MaintainerID
+}
+func (m *AnyID_OrderID) getID() ids.ID {
+	return m.OrderID
+}
+func (m *AnyID_OwnableID) getID() ids.ID {
+	return m.OwnableID
+}
+func (m *AnyID_PropertyID) getID() ids.ID {
+	return m.PropertyID
+}
+func (m *AnyID_SplitID) getID() ids.ID {
+	return m.SplitID
+}
+func (m *AnyID_StringID) getID() ids.ID {
+	return m.StringID
+}
 
 func (m *AnyID) Compare(listable traits.Listable) int {
 	return m.Impl.(ids.ID).Compare(listable)
