b8d924d9be68766ece94d2de3367b0f86e97d7a9
Ankit Kumar <ankit.kr554@gmail.com>
2022-12-26 17:24:33 +0530

    test: code refactoring

--- modules/orders/internal/transactions/cancel/message_test.go
+++ modules/orders/internal/transactions/cancel/message_test.go
@@ -7,51 +7,27 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdkTypes "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/require"
-
-	"github.com/AssetMantle/modules/modules/orders/internal/module"
-	baseData "github.com/AssetMantle/modules/schema/data/base"
 	"github.com/AssetMantle/modules/schema/helpers"
 	"github.com/AssetMantle/modules/schema/ids"
 	baseIDs "github.com/AssetMantle/modules/schema/ids/base"
-	"github.com/AssetMantle/modules/schema/lists/base"
-	baseProperties "github.com/AssetMantle/modules/schema/properties/base"
-	baseQualified "github.com/AssetMantle/modules/schema/qualified/base"
-	"github.com/AssetMantle/modules/utilities/transaction"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 )
 
-func CreateTestInputForMessages(t *testing.T) (ids.OrderID, ids.IdentityID, sdkTypes.AccAddress, sdkTypes.Msg) {
-	// testOrderID := baseIDs.NewStringID("orderID")
-	immutables := baseQualified.NewImmutables(base.NewPropertyList(baseProperties.NewMesaProperty(baseIDs.NewStringID("ID2"), baseData.NewStringData("Data2"))))
-	mutables := baseQualified.NewMutables(base.NewPropertyList(baseProperties.NewMesaProperty(baseIDs.NewStringID("ID1"), baseData.NewStringData("Data1"))))
-	// mutables2 := baseQualified.NewMutables(base.NewPropertyList(baseProperties.NewMesaProperty(baseIDs.NewStringID("ID"), baseData.NewStringData(""))))
-
-	testClassificationID := baseIDs.NewClassificationID(immutables, mutables)
-	testFromID := baseIDs.NewIdentityID(testClassificationID, immutables)
-	testOrderID := baseIDs.NewOrderID(testClassificationID, immutables)
-
-	fromAddress := "cosmos1pkkayn066msg6kn33wnl5srhdt3tnu2vzasz9c"
-	fromAccAddress, err := sdkTypes.AccAddressFromBech32(fromAddress)
-	require.Nil(t, err)
-
-	testMessage := newMessage(fromAccAddress, testFromID, testOrderID)
-
-	return testOrderID, testFromID, fromAccAddress, testMessage
-}
+var (
+	testMessage = newMessage(fromAccAddress, testFromID, testOrderID)
+)
 
 func Test_messageFromInterface(t *testing.T) {
-	testOrderID, testFromID, fromAccAddress, testMessage := CreateTestInputForMessages(t)
 	type args struct {
 		msg sdkTypes.Msg
 	}
 	tests := []struct {
 		name string
 		args args
-		want message
+		want Message
 	}{
-		{"+ve", args{testMessage}, message{fromAccAddress, testFromID, testOrderID}},
+		{"+ve", args{testMessage}, Message{fromAddress, testFromID, testOrderID}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -67,7 +43,7 @@ func Test_messagePrototype(t *testing.T) {
 		name string
 		want helpers.Message
 	}{
-		{"+ve", message{}},
+		{"+ve", &Message{}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -78,53 +54,22 @@ func Test_messagePrototype(t *testing.T) {
 	}
 }
 
-func Test_message_GetSignBytes(t *testing.T) {
-	testOrderID, testFromID, fromAccAddress, testMessage := CreateTestInputForMessages(t)
-
-	type fields struct {
-		From    sdkTypes.AccAddress
-		FromID  ids.IdentityID
-		OrderID ids.OrderID
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []byte
-	}{
-		{"+ve", fields{fromAccAddress, testFromID, testOrderID}, sdkTypes.MustSortJSON(transaction.RegisterLegacyAminoCodec(messagePrototype).MustMarshalJSON(testMessage))},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			message := message{
-				From:    tt.fields.From,
-				FromID:  tt.fields.FromID,
-				OrderID: tt.fields.OrderID,
-			}
-			if got := message.GetSignBytes(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetSignBytes() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func Test_message_GetSigners(t *testing.T) {
-	testOrderID, testFromID, fromAccAddress, _ := CreateTestInputForMessages(t)
-
 	type fields struct {
-		From    sdkTypes.AccAddress
-		FromID  ids.IdentityID
-		OrderID ids.OrderID
+		From    string
+		FromID  *baseIDs.IdentityID
+		OrderID *baseIDs.OrderID
 	}
 	tests := []struct {
 		name   string
 		fields fields
 		want   []sdkTypes.AccAddress
 	}{
-		{"+ve", fields{fromAccAddress, testFromID, testOrderID}, []sdkTypes.AccAddress{fromAccAddress}},
+		{"+ve", fields{fromAddress, testFromID, testOrderID}, []sdkTypes.AccAddress{fromAccAddress}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			message := message{
+			message := Message{
 				From:    tt.fields.From,
 				FromID:  tt.fields.FromID,
 				OrderID: tt.fields.OrderID,
@@ -137,12 +82,10 @@ func Test_message_GetSigners(t *testing.T) {
 }
 
 func Test_message_RegisterCodec(t *testing.T) {
-	testOrderID, testFromID, fromAccAddress, _ := CreateTestInputForMessages(t)
-
 	type fields struct {
-		From    sdkTypes.AccAddress
-		FromID  ids.IdentityID
-		OrderID ids.OrderID
+		From    string
+		FromID  *baseIDs.IdentityID
+		OrderID *baseIDs.OrderID
 	}
 	type args struct {
 		legacyAmino *codec.LegacyAmino
@@ -152,11 +95,11 @@ func Test_message_RegisterCodec(t *testing.T) {
 		fields fields
 		args   args
 	}{
-		{"+ve", fields{fromAccAddress, testFromID, testOrderID}, args{codec.NewLegacyAmino()}},
+		{"+ve", fields{fromAddress, testFromID, testOrderID}, args{codec.NewLegacyAmino()}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			me := message{
+			me := Message{
 				From:    tt.fields.From,
 				FromID:  tt.fields.FromID,
 				OrderID: tt.fields.OrderID,
@@ -166,53 +109,22 @@ func Test_message_RegisterCodec(t *testing.T) {
 	}
 }
 
-func Test_message_Route(t *testing.T) {
-	testOrderID, testFromID, fromAccAddress, _ := CreateTestInputForMessages(t)
-
-	type fields struct {
-		From    sdkTypes.AccAddress
-		FromID  ids.IdentityID
-		OrderID ids.OrderID
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   string
-	}{
-		{"+ve", fields{fromAccAddress, testFromID, testOrderID}, module.Name},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			message := message{
-				From:    tt.fields.From,
-				FromID:  tt.fields.FromID,
-				OrderID: tt.fields.OrderID,
-			}
-			if got := message.Route(); got != tt.want {
-				t.Errorf("Route() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func Test_message_Type(t *testing.T) {
-	testOrderID, testFromID, fromAccAddress, _ := CreateTestInputForMessages(t)
-
 	type fields struct {
-		From    sdkTypes.AccAddress
-		FromID  ids.IdentityID
-		OrderID ids.OrderID
+		From    string
+		FromID  *baseIDs.IdentityID
+		OrderID *baseIDs.OrderID
 	}
 	tests := []struct {
 		name   string
 		fields fields
 		want   string
 	}{
-		{"+ve", fields{fromAccAddress, testFromID, testOrderID}, Transaction.GetName()},
+		{"+ve", fields{fromAddress, testFromID, testOrderID}, Transaction.GetName()},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			message := message{
+			message := Message{
 				From:    tt.fields.From,
 				FromID:  tt.fields.FromID,
 				OrderID: tt.fields.OrderID,
@@ -225,24 +137,22 @@ func Test_message_Type(t *testing.T) {
 }
 
 func Test_message_ValidateBasic(t *testing.T) {
-	testOrderID, testFromID, fromAccAddress, _ := CreateTestInputForMessages(t)
-
 	type fields struct {
-		From    sdkTypes.AccAddress
-		FromID  ids.IdentityID
-		OrderID ids.OrderID
+		From    string
+		FromID  *baseIDs.IdentityID
+		OrderID *baseIDs.OrderID
 	}
 	tests := []struct {
 		name    string
 		fields  fields
 		wantErr bool
 	}{
-		{"+ve", fields{fromAccAddress, testFromID, testOrderID}, false},
+		{"+ve", fields{fromAddress, testFromID, testOrderID}, false},
 		{"-ve", fields{}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			message := message{
+			message := Message{
 				From:    tt.fields.From,
 				FromID:  tt.fields.FromID,
 				OrderID: tt.fields.OrderID,
@@ -255,8 +165,6 @@ func Test_message_ValidateBasic(t *testing.T) {
 }
 
 func Test_newMessage(t *testing.T) {
-	testOrderID, testFromID, fromAccAddress, _ := CreateTestInputForMessages(t)
-
 	type args struct {
 		from    sdkTypes.AccAddress
 		fromID  ids.IdentityID
@@ -267,7 +175,7 @@ func Test_newMessage(t *testing.T) {
 		args args
 		want sdkTypes.Msg
 	}{
-		{"+ve", args{fromAccAddress, testFromID, testOrderID}, message{fromAccAddress, testFromID, testOrderID}},
+		{"+ve", args{fromAccAddress, testFromID, testOrderID}, &Message{fromAddress, testFromID, testOrderID}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
