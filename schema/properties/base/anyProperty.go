7e5b331330b8393807cfc96f4a9c4f4cb0a74ee3
mukundrawat <mukund@assetmantle.one>
2022-12-26 16:33:49 +0530

    property impls for getters

--- schema/properties/base/anyProperty.go
+++ schema/properties/base/anyProperty.go
@@ -7,32 +7,46 @@ import (
 	"github.com/AssetMantle/modules/schema/traits"
 )
 
+type getter interface {
+	isAnyProperty_Impl
+	GetProperty() properties.Property
+}
+
 var _ properties.Property = (*AnyProperty)(nil)
+var _ getter = (*AnyProperty_MetaProperty)(nil)
+var _ getter = (*AnyProperty_MesaProperty)(nil)
+
+func (m *AnyProperty_MetaProperty) GetProperty() properties.Property {
+	return m.MetaProperty
+}
+func (m *AnyProperty_MesaProperty) GetProperty() properties.Property {
+	return m.MesaProperty
+}
 
 func (m *AnyProperty) GetID() ids.PropertyID {
-	return m.Impl.(properties.Property).GetID()
+	return m.Impl.(getter).GetProperty().GetID()
 }
 func (m *AnyProperty) ScrubData() properties.Property {
-	return m.Impl.(properties.Property).ScrubData()
+	return m.Impl.(getter).GetProperty().ScrubData()
 }
 func (m *AnyProperty) GetDataID() ids.DataID {
-	return m.Impl.(properties.Property).GetDataID()
+	return m.Impl.(getter).GetProperty().GetDataID()
 }
 func (m *AnyProperty) GetKey() ids.StringID {
-	return m.Impl.(properties.Property).GetKey()
+	return m.Impl.(getter).GetProperty().GetKey()
 }
 func (m *AnyProperty) GetData() data.AnyData {
-	return m.Impl.(properties.Property).GetData()
+	return m.Impl.(getter).GetProperty().GetData()
 }
 func (m *AnyProperty) GetType() ids.StringID {
-	return m.Impl.(properties.Property).GetType()
+	return m.Impl.(getter).GetProperty().GetType()
 }
 func (m *AnyProperty) IsMeta() bool {
-	return m.Impl.(properties.Property).IsMeta()
+	return m.Impl.(getter).GetProperty().IsMeta()
 }
 func (m *AnyProperty) ToAnyProperty() properties.AnyProperty {
-	return m.Impl.(properties.Property).ToAnyProperty()
+	return m.Impl.(getter).GetProperty().ToAnyProperty()
 }
 func (m *AnyProperty) Compare(listable traits.Listable) int {
-	return m.Impl.(properties.Property).Compare(listable)
+	return m.Impl.(getter).GetProperty().Compare(listable)
 }
