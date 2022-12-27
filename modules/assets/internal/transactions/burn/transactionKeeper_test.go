c1b068e69179824c8e4aa0b09dc0f79d2e2d000a
mukundrawat <mukund@assetmantle.one>
2022-12-27 17:50:11 +0530

    property impls for getters

--- modules/assets/internal/transactions/burn/transactionKeeper_test.go
+++ modules/assets/internal/transactions/burn/transactionKeeper_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/std"
 	"github.com/cosmos/cosmos-sdk/store"
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	paramsKeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
