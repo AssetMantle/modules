937b2d774284cf8b761e6813211e96155dc74663
Ankit Kumar <ankit.kr554@gmail.com>
2022-12-28 14:30:21 +0530

    test: code refactoring for decData

--- schema/data/base/decData_test.go
+++ schema/data/base/decData_test.go
@@ -4,7 +4,6 @@
 package base
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 
@@ -28,43 +27,14 @@ func TestNewDecData(t *testing.T) {
 		args args
 		want data.Data
 	}{
-		{"+ve with nil", args{}, decData{}},
-		{"+ve with zero dec", args{types.ZeroDec()}, decData{types.ZeroDec()}},
-		{"+ve", args{types.NewDec(100)}, decData{types.NewDec(100)}},
-		{"+ve with -ve Dec", args{types.NewDec(-100)}, decData{types.NewDec(-100)}},
+		{"+ve with nil", args{}, &DecData{}},
+		{"+ve with zero dec", args{types.ZeroDec()}, &DecData{types.ZeroDec()}},
+		{"+ve", args{types.NewDec(100)}, &DecData{types.NewDec(100)}},
+		{"+ve with -ve Dec", args{types.NewDec(-100)}, &DecData{types.NewDec(-100)}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equalf(t, tt.want, NewDecData(tt.args.value), "NewDecData(%v)", tt.args.value)
-		})
-	}
-}
-
-func Test_decDataFromInterface(t *testing.T) {
-	type args struct {
-		listable traits.Listable
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    decData
-		wantErr assert.ErrorAssertionFunc
-	}{
-		{"+ve with nil", args{decData{}}, decData{}, assert.NoError},
-		{"+ve with nil", args{decData{types.Dec{}}}, decData{}, assert.NoError},
-		{"+ve with zero dec", args{decData{types.ZeroDec()}}, decData{types.ZeroDec()}, assert.NoError},
-		{"+ve", args{decData{types.NewDec(100)}}, decData{types.NewDec(100)}, assert.NoError},
-		{"+ve with -ve Dec", args{decData{types.NewDec(-100)}}, decData{types.NewDec(-100)}, assert.NoError},
-		{"-ve with nil", args{nil}, decData{}, assert.Error},
-		{"-ve stringData", args{stringData{"testData"}}, decData{}, assert.Error},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := decDataFromInterface(tt.args.listable)
-			if !tt.wantErr(t, err, fmt.Sprintf("decDataFromInterface(%v)", tt.args.listable)) {
-				return
-			}
-			assert.Equalf(t, tt.want, got, "decDataFromInterface(%v)", tt.args.listable)
 		})
 	}
 }
@@ -79,13 +49,13 @@ func Test_decData_Bytes(t *testing.T) {
 		want   []byte
 	}{
 		{"+ve with nil", fields{}, []byte{0x1}}, // TODO: Update test after fixing the bug
-		{"+ve with zero dec", fields{types.ZeroDec()}, decData{types.ZeroDec()}.Bytes()},
-		{"+ve", fields{types.NewDec(100)}, decData{types.NewDec(100)}.Bytes()},
-		{"+ve with -ve Dec", fields{types.NewDec(-100)}, decData{types.NewDec(-100)}.Bytes()},
+		{"+ve with zero dec", fields{types.ZeroDec()}, DecData{types.ZeroDec()}.Bytes()},
+		{"+ve", fields{types.NewDec(100)}, DecData{types.NewDec(100)}.Bytes()},
+		{"+ve with -ve Dec", fields{types.NewDec(-100)}, DecData{types.NewDec(-100)}.Bytes()},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			decData := decData{
+			decData := DecData{
 				Value: tt.fields.Value,
 			}
 			assert.Equalf(t, tt.want, decData.Bytes(), "Bytes()")
@@ -95,7 +65,7 @@ func Test_decData_Bytes(t *testing.T) {
 
 func Test_decData_Compare(t *testing.T) {
 	require.Panics(t, func() {
-		decData{}.Compare(nil)
+		DecData{}.Compare(nil)
 	})
 	type fields struct {
 		Value types.Dec
@@ -109,16 +79,16 @@ func Test_decData_Compare(t *testing.T) {
 		args   args
 		want   int
 	}{
-		{"+ve with nil", fields{}, args{decData{}}, 0},
-		{"+ve with nil", fields{types.Dec{}}, args{decData{types.Dec{}}}, 0},
-		{"+ve with zero dec", fields{types.ZeroDec()}, args{decData{types.ZeroDec()}}, 0},
-		{"+ve", fields{types.NewDec(100)}, args{decData{types.NewDec(100)}}, 0},
-		{"-ve", fields{types.NewDec(-100)}, args{decData{types.NewDec(100)}}, -1},
-		{"+ve with -ve Dec", fields{types.NewDec(-100)}, args{decData{types.NewDec(-100)}}, 0},
+		{"+ve with nil", fields{}, args{&DecData{}}, 0},
+		{"+ve with nil", fields{types.Dec{}}, args{&DecData{types.Dec{}}}, 0},
+		{"+ve with zero dec", fields{types.ZeroDec()}, args{&DecData{types.ZeroDec()}}, 0},
+		{"+ve", fields{types.NewDec(100)}, args{&DecData{types.NewDec(100)}}, 0},
+		{"-ve", fields{types.NewDec(-100)}, args{&DecData{types.NewDec(100)}}, -1},
+		{"+ve with -ve Dec", fields{types.NewDec(-100)}, args{&DecData{types.NewDec(-100)}}, 0},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			decData := decData{
+			decData := DecData{
 				Value: tt.fields.Value,
 			}
 			assert.Equalf(t, tt.want, decData.Compare(tt.args.listable), "Compare(%v)", tt.args.listable)
@@ -138,12 +108,12 @@ func Test_decData_GenerateHashID(t *testing.T) {
 	}{
 		{"panic case with nil", fields{types.Dec{}}, baseIDs.GenerateHashID(), false},
 		{"+ve with zero dec", fields{types.ZeroDec()}, baseIDs.GenerateHashID(), false},
-		{"+ve", fields{types.NewDec(100)}, baseIDs.GenerateHashID(decData{types.NewDec(100)}.Bytes()), false},
-		{"+ve with -ve Dec", fields{types.NewDec(-100)}, baseIDs.GenerateHashID(decData{types.NewDec(-100)}.Bytes()), false},
+		{"+ve", fields{types.NewDec(100)}, baseIDs.GenerateHashID(DecData{types.NewDec(100)}.Bytes()), false},
+		{"+ve with -ve Dec", fields{types.NewDec(-100)}, baseIDs.GenerateHashID(DecData{types.NewDec(-100)}.Bytes()), false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			decData := decData{
+			decData := DecData{
 				Value: tt.fields.Value,
 			}
 			if tt.wantPanic {
@@ -167,14 +137,14 @@ func Test_decData_Get(t *testing.T) {
 		fields fields
 		want   types.Dec
 	}{
-		{"+ve with nil", fields{}, decData{}.Value},
-		{"+ve with zero dec", fields{types.ZeroDec()}, decData{types.ZeroDec()}.Value},
-		{"+ve", fields{types.NewDec(100)}, decData{types.NewDec(100)}.Value},
-		{"+ve with -ve Dec", fields{types.NewDec(-100)}, decData{types.NewDec(-100)}.Value},
+		{"+ve with nil", fields{}, DecData{}.Value},
+		{"+ve with zero dec", fields{types.ZeroDec()}, DecData{types.ZeroDec()}.Value},
+		{"+ve", fields{types.NewDec(100)}, DecData{types.NewDec(100)}.Value},
+		{"+ve with -ve Dec", fields{types.NewDec(-100)}, DecData{types.NewDec(-100)}.Value},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			decData := decData{
+			decData := DecData{
 				Value: tt.fields.Value,
 			}
 			assert.Equalf(t, tt.want, decData.Get(), "Get()")
@@ -193,13 +163,13 @@ func Test_decData_GetID(t *testing.T) {
 		wantPanic bool
 	}{
 		{"panic case with nil", fields{types.Dec{}}, nil, true}, // TODO: Check whether planned panic in GenerateDataID is expected behaviour
-		{"+ve with zero dec", fields{types.ZeroDec()}, baseIDs.GenerateDataID(decData{types.ZeroDec()}), false},
-		{"+ve", fields{types.NewDec(100)}, baseIDs.GenerateDataID(decData{types.NewDec(100)}), false},
-		{"+ve with -ve Dec", fields{types.NewDec(-100)}, baseIDs.GenerateDataID(decData{types.NewDec(-100)}), false},
+		{"+ve with zero dec", fields{types.ZeroDec()}, baseIDs.GenerateDataID(&DecData{types.ZeroDec()}), false},
+		{"+ve", fields{types.NewDec(100)}, baseIDs.GenerateDataID(&DecData{types.NewDec(100)}), false},
+		{"+ve with -ve Dec", fields{types.NewDec(-100)}, baseIDs.GenerateDataID(&DecData{types.NewDec(-100)}), false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			decData := decData{
+			decData := DecData{
 				Value: tt.fields.Value,
 			}
 			if tt.wantPanic {
@@ -229,7 +199,7 @@ func Test_decData_GetType(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			decData := decData{
+			decData := DecData{
 				Value: tt.fields.Value,
 			}
 			assert.Equalf(t, tt.want, decData.GetType(), "GetType()")
@@ -246,14 +216,14 @@ func Test_decData_String(t *testing.T) {
 		fields fields
 		want   string
 	}{
-		{"+ve with nil", fields{}, decData{}.Value.String()},
-		{"+ve with zero dec", fields{types.ZeroDec()}, decData{types.ZeroDec()}.Value.String()},
-		{"+ve", fields{types.NewDec(100)}, decData{types.NewDec(100)}.Value.String()},
-		{"+ve with -ve Dec", fields{types.NewDec(-100)}, decData{types.NewDec(-100)}.Value.String()},
+		{"+ve with nil", fields{}, DecData{}.Value.String()},
+		{"+ve with zero dec", fields{types.ZeroDec()}, DecData{types.ZeroDec()}.Value.String()},
+		{"+ve", fields{types.NewDec(100)}, DecData{types.NewDec(100)}.Value.String()},
+		{"+ve with -ve Dec", fields{types.NewDec(-100)}, DecData{types.NewDec(-100)}.Value.String()},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			decData := decData{
+			decData := DecData{
 				Value: tt.fields.Value,
 			}
 			assert.Equalf(t, tt.want, decData.String(), "String()")
@@ -270,14 +240,14 @@ func Test_decData_ZeroValue(t *testing.T) {
 		fields fields
 		want   data.Data
 	}{
-		{"+ve with nil", fields{}, decData{types.ZeroDec()}},
-		{"+ve with zero dec", fields{types.ZeroDec()}, decData{types.ZeroDec()}},
-		{"+ve", fields{types.NewDec(100)}, decData{types.ZeroDec()}},
-		{"+ve with -ve Dec", fields{types.NewDec(-100)}, decData{types.ZeroDec()}},
+		{"+ve with nil", fields{}, &DecData{types.ZeroDec()}},
+		{"+ve with zero dec", fields{types.ZeroDec()}, &DecData{types.ZeroDec()}},
+		{"+ve", fields{types.NewDec(100)}, &DecData{types.ZeroDec()}},
+		{"+ve with -ve Dec", fields{types.NewDec(-100)}, &DecData{types.ZeroDec()}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			decData := decData{
+			decData := DecData{
 				Value: tt.fields.Value,
 			}
 			assert.Equalf(t, tt.want, decData.ZeroValue(), "ZeroValue()")
