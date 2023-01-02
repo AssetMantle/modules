7362d8b69c08f6d0dc02586c49b125d302f1ac11
deepanshutr <deepanshutr@gmail.com>
2023-01-02 21:28:49 +0530

    feat(GenesisState): defining GenesisState interface and implementing for all modules

---
+++ modules/metas/internal/genesis/genesisState.go
@@ -0,0 +1,50 @@
+package genesis
+
+import (
+	"github.com/AssetMantle/modules/modules/metas/internal/mappable"
+	"github.com/AssetMantle/modules/modules/metas/internal/parameters"
+	"github.com/AssetMantle/modules/schema/helpers"
+	parametersSchema "github.com/AssetMantle/modules/schema/parameters"
+	baseParameters "github.com/AssetMantle/modules/schema/parameters/base"
+)
+
+var _ helpers.GenesisState = (*GenesisState)(nil)
+
+func (genesisState *GenesisState) Default() helpers.GenesisState {
+	return PrototypeGenesisState()
+}
+func (genesisState *GenesisState) GetMappables() []helpers.Mappable {
+	return mappable.MappablesToInterface(genesisState.Mappables)
+}
+func (genesisState *GenesisState) GetParameters() []parametersSchema.Parameter {
+	return baseParameters.ParametersToInterfaces(genesisState.Parameters)
+}
+func (genesisState *GenesisState) Initialize(mappableList []helpers.Mappable, parameterList []parametersSchema.Parameter) helpers.GenesisState {
+	if len(mappableList) == 0 {
+		genesisState.Mappables = mappable.MappablesFromInterface(genesisState.Default().GetMappables())
+	} else {
+		genesisState.Mappables = mappable.MappablesFromInterface(mappableList)
+	}
+
+	if len(parameterList) == 0 {
+		genesisState.Parameters = baseParameters.ParametersFromInterfaces(genesisState.Default().GetParameters())
+	} else {
+		for _, defaultParameter := range genesisState.Default().GetParameters() {
+			for i, parameter := range parameterList {
+				if defaultParameter.GetID().Compare(parameter.GetID()) == 0 {
+					parameterList[i] = defaultParameter.Mutate(parameter.GetData())
+				}
+			}
+		}
+		genesisState.Parameters = baseParameters.ParametersFromInterfaces(parameterList)
+	}
+
+	return genesisState
+}
+
+func PrototypeGenesisState() helpers.GenesisState {
+	return &GenesisState{
+		Mappables:  []*mappable.Mappable{},
+		Parameters: baseParameters.ParametersFromInterfaces(parameters.Prototype().GetList()),
+	}
+}
