bcb59a8ebec6f98a91ac228d33c146811cef93f6
deepanshutr <deepanshutr@gmail.com>
2023-01-09 21:42:32 +0530

    fix(transaction): adding keyring and broadcast mode configs

--- schema/helpers/base/transaction.go
+++ schema/helpers/base/transaction.go
@@ -136,7 +136,8 @@ func (transaction transaction) RESTRequestHandler(context client.Context) http.H
 			WithChainID(baseReq.ChainID).
 			WithSimulateAndExecute(baseReq.Simulate).
 			WithTxConfig(context.TxConfig).
-			WithTimeoutHeight(baseReq.TimeoutHeight)
+			WithTimeoutHeight(baseReq.TimeoutHeight).
+			WithKeybase(context.Keyring)
 
 		msgList := []sdkTypes.Msg{msg}
 
@@ -167,7 +168,8 @@ func (transaction transaction) RESTRequestHandler(context client.Context) http.H
 
 		context = context.WithFromAddress(fromAddress)
 		context = context.WithFromName(fromName)
-		context = context.WithBroadcastMode(viper.GetString(flags.FlagBroadcastMode))
+		// TODO ***** from from client.toml, remove hardcode
+		context = context.WithBroadcastMode(flags.BroadcastBlock)
 
 		if queuing.KafkaState.IsEnabled {
 			responseWriter.WriteHeader(http.StatusAccepted)
