cae345f7791e1ba5cfb37b03ea96d98ebb3a3f9f
deepanshutr <deepanshutr@gmail.com>
2023-01-31 15:20:26 +0530

    refactor(genesis): combining genesis and genesisState to one interface

--- schema/helpers/genesis.go
+++ schema/helpers/genesis.go
@@ -1,25 +1,26 @@
-// Copyright [2021] - [2022], AssetMantle Pte. Ltd. and the code contributors
-// SPDX-License-Identifier: Apache-2.0
-
 package helpers
 
 import (
 	"context"
 
 	sdkCodec "github.com/cosmos/cosmos-sdk/codec"
+	"github.com/gogo/protobuf/proto"
 )
 
 type Genesis interface {
+	proto.Message
+
+	GetParameterList() []Parameter
+	GetMappableList() []Mappable
 	Default() Genesis
+
 	Validate() error
+
 	Import(context.Context, Mapper)
 	Export(context.Context, Mapper) Genesis
 
 	Encode(sdkCodec.JSONCodec) []byte
 	Decode(sdkCodec.JSONCodec, []byte) Genesis
 
-	Initialize([]Mappable, []Parameter) Genesis
-
-	GetParameterList() []Parameter
-	GetMappableList() []Mappable
+	Initialize(mappableList []Mappable, parameterList []Parameter) Genesis
 }
