1a62e002217bf8ceb067d08a7c02c78623aecdc4
muku314115 <mukangotan@gmail.com>
2023-02-07 14:01:13 +0530

    test(assets): add tests for renumerate assets request

---
+++ modules/assets/internal/transactions/renumerate/request_test.go
@@ -0,0 +1,256 @@
+// Copyright [2021] - [2022], AssetMantle Pte. Ltd. and the code contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package renumerate
+
+import (
+	"encoding/json"
+	"github.com/AssetMantle/modules/schema/helpers"
+	"github.com/AssetMantle/modules/utilities/transaction"
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/rest"
+	"reflect"
+	"testing"
+)
+
+var (
+	testBaseRequest = rest.BaseReq{From: fromAddress, ChainID: "test", Fees: types.NewCoins()}
+)
+
+func Test_newTransactionRequest(t *testing.T) {
+	type args struct {
+		baseReq rest.BaseReq
+		fromID  string
+		assetID string
+	}
+	tests := []struct {
+		name string
+		args args
+		want helpers.TransactionRequest
+	}{
+		// TODO: Add test cases.
+		{"+ve", args{testBaseRequest, fromID.String(), testAssetID.String()}, transactionRequest{testBaseRequest, fromID.String(), testAssetID.String()}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newTransactionRequest(tt.args.baseReq, tt.args.fromID, tt.args.assetID); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newTransactionRequest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_requestPrototype(t *testing.T) {
+	tests := []struct {
+		name string
+		want helpers.TransactionRequest
+	}{
+		// TODO: Add test cases.
+		{"+ve", transactionRequest{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := requestPrototype(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("requestPrototype() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_transactionRequest_FromCLI(t *testing.T) {
+	type fields struct {
+		BaseReq rest.BaseReq
+		FromID  string
+		AssetID string
+	}
+	type args struct {
+		cliCommand helpers.CLICommand
+		cliContext context.CLIContext
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    helpers.TransactionRequest
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		//{"+ve",fields{testBaseRequest,fromID.String(),testAssetID.String()},args{},newTransactionRequest(testBaseRequest,fromID.String(),testAssetID.String()),false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			transactionRequest := transactionRequest{
+				BaseReq: tt.fields.BaseReq,
+				FromID:  tt.fields.FromID,
+				AssetID: tt.fields.AssetID,
+			}
+			got, err := transactionRequest.FromCLI(tt.args.cliCommand, tt.args.cliContext)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FromCLI() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FromCLI() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_transactionRequest_FromJSON(t *testing.T) {
+	type fields struct {
+		BaseReq rest.BaseReq
+		FromID  string
+		AssetID string
+	}
+	type args struct {
+		rawMessage json.RawMessage
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    helpers.TransactionRequest
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"+ve", fields{testBaseRequest, fromID.String(), testAssetID.String()}, args{types.MustSortJSON(transaction.RegisterCodec(messagePrototype).MustMarshalJSON(message{fromAccAddress, fromID, testAssetID}))}, newTransactionRequest(testBaseRequest, fromID.String(), testAssetID.String()), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			transactionRequest := transactionRequest{
+				BaseReq: tt.fields.BaseReq,
+				FromID:  tt.fields.FromID,
+				AssetID: tt.fields.AssetID,
+			}
+			got, err := transactionRequest.FromJSON(tt.args.rawMessage)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FromJSON() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FromJSON() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_transactionRequest_GetBaseReq(t *testing.T) {
+	type fields struct {
+		BaseReq rest.BaseReq
+		FromID  string
+		AssetID string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   rest.BaseReq
+	}{
+		// TODO: Add test cases.
+		{"+ve", fields{testBaseRequest, fromID.String(), testAssetID.String()}, testBaseRequest},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			transactionRequest := transactionRequest{
+				BaseReq: tt.fields.BaseReq,
+				FromID:  tt.fields.FromID,
+				AssetID: tt.fields.AssetID,
+			}
+			if got := transactionRequest.GetBaseReq(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetBaseReq() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_transactionRequest_MakeMsg(t *testing.T) {
+	type fields struct {
+		BaseReq rest.BaseReq
+		FromID  string
+		AssetID string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    types.Msg
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"+ve", fields{testBaseRequest, fromID.String(), testAssetID.String()}, newMessage(fromAccAddress, fromID, testAssetID), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			transactionRequest := transactionRequest{
+				BaseReq: tt.fields.BaseReq,
+				FromID:  tt.fields.FromID,
+				AssetID: tt.fields.AssetID,
+			}
+			got, err := transactionRequest.MakeMsg()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MakeMsg() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MakeMsg() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_transactionRequest_RegisterCodec(t *testing.T) {
+	type fields struct {
+		BaseReq rest.BaseReq
+		FromID  string
+		AssetID string
+	}
+	type args struct {
+		codec *codec.Codec
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+		{"+ve", fields{testBaseRequest, fromID.String(), testAssetID.String()}, args{codec.New()}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tr := transactionRequest{
+				BaseReq: tt.fields.BaseReq,
+				FromID:  tt.fields.FromID,
+				AssetID: tt.fields.AssetID,
+			}
+			tr.RegisterCodec(tt.args.codec)
+		})
+	}
+}
+
+func Test_transactionRequest_Validate(t *testing.T) {
+	type fields struct {
+		BaseReq rest.BaseReq
+		FromID  string
+		AssetID string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"+ve", fields{testBaseRequest, fromID.String(), testAssetID.String()}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			transactionRequest := transactionRequest{
+				BaseReq: tt.fields.BaseReq,
+				FromID:  tt.fields.FromID,
+				AssetID: tt.fields.AssetID,
+			}
+			if err := transactionRequest.Validate(); (err != nil) != tt.wantErr {
+				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
