da6a33271740feefbc1b8f15a7688c3502a3b222
deepanshutr <deepanshutr@gmail.com>
2023-02-13 23:28:45 +0530

    feat(classifications): implementing maxPropertyCount as parameter

---
+++ modules/classifications/internal/parameters/maxPropertyCount/parameter.go
@@ -0,0 +1,26 @@
+// Copyright [2021] - [2022], AssetMantle Pte. Ltd. and the code contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package maxPropertyCount
+
+import (
+	baseData "github.com/AssetMantle/modules/schema/data/base"
+	"github.com/AssetMantle/modules/schema/errors/constants"
+	baseHelpers "github.com/AssetMantle/modules/schema/helpers/base"
+	baseTypes "github.com/AssetMantle/modules/schema/parameters/base"
+	"github.com/AssetMantle/modules/schema/properties/base"
+	constantProperties "github.com/AssetMantle/modules/schema/properties/constants"
+)
+
+var ID = constantProperties.MaxPropertyCountProperty.GetKey()
+var Parameter = baseTypes.NewParameter(base.NewMetaProperty(ID, baseData.NewNumberData(22)))
+
+func validator(i interface{}) error {
+	if value, ok := i.(baseData.NumberData); ok && value.Get() > 0 {
+		return nil
+	}
+
+	return constants.IncorrectFormat
+}
+
+var ValidatableParameter = baseHelpers.NewValidatableParameter(Parameter, validator)
