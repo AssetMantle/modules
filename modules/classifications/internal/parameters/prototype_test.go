9a51a2a0ab5f2f8a3c505eaf2edcbd90229f1e36
muku314115 <mukangotan@gmail.com>
2023-02-27 14:14:59 +0530

    prototype

---
+++ modules/classifications/internal/parameters/prototype_test.go
@@ -0,0 +1,33 @@
+// Copyright [2021] - [2022], AssetMantle Pte. Ltd. and the code contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package parameters
+
+import (
+	"github.com/AssetMantle/modules/modules/classifications/internal/module"
+	"github.com/AssetMantle/modules/modules/classifications/internal/parameters/bondRate"
+	"github.com/AssetMantle/modules/modules/classifications/internal/parameters/maxPropertyCount"
+	"reflect"
+	"testing"
+
+	"github.com/AssetMantle/modules/schema/helpers"
+
+	baseHelpers "github.com/AssetMantle/modules/schema/helpers/base"
+)
+
+func TestPrototype(t *testing.T) {
+	tests := []struct {
+		name      string
+		want      helpers.ParameterManager
+		wantError error
+	}{
+		{"+ve", baseHelpers.NewParameterManager(module.Name, bondRate.ValidatableParameter, maxPropertyCount.ValidatableParameter), nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Prototype(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Prototype() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
