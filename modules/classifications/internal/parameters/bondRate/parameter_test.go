158f239bcf91f934d5452949356c3759e95a5a61
muku314115 <mukangotan@gmail.com>
2023-02-27 14:13:15 +0530

    bondRate

--- modules/classifications/internal/parameters/bondRate/parameter_test.go
+++ modules/classifications/internal/parameters/bondRate/parameter_test.go
@@ -8,16 +8,38 @@ import (
 
 	baseProperties "github.com/AssetMantle/modules/schema/properties/base"
 
-	"github.com/stretchr/testify/require"
-
 	baseData "github.com/AssetMantle/modules/schema/data/base"
 	errorConstants "github.com/AssetMantle/modules/schema/errors/constants"
 	baseIDs "github.com/AssetMantle/modules/schema/ids/base"
 	baseTypes "github.com/AssetMantle/modules/schema/parameters/base"
 )
 
-func Test_Validator(t *testing.T) {
-	require.Equal(t, errorConstants.IncorrectFormat, validator(baseIDs.NewStringID("")))
-	require.Equal(t, nil, validator(Parameter))
-	require.Equal(t, errorConstants.InvalidParameter, validator(baseTypes.NewParameter(baseProperties.NewMetaProperty(baseIDs.NewStringID(""), baseData.NewStringData("")))))
+func Test_validator(t *testing.T) {
+	type args struct {
+		i interface{}
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantError error
+	}{
+		{"-ve incorrectFormat", args{baseIDs.NewStringID("")}, errorConstants.IncorrectFormat},
+		{"+ve", args{Parameter}, nil},
+		{"-ve InvalidParameter", args{baseTypes.NewParameter(baseProperties.NewMetaProperty(baseIDs.NewStringID(""), baseData.NewStringData("")))}, errorConstants.IncorrectFormat},
+		{"+ve with booleanData", args{baseData.NewBooleanData(false)}, errorConstants.IncorrectFormat},
+		{"-ve with different type of Data", args{baseData.NewStringData("stringData")}, errorConstants.IncorrectFormat},
+		{"-ve InvalidParameter", args{baseTypes.NewParameter(baseProperties.NewMetaProperty(baseIDs.NewStringID(""), baseData.NewStringData("")))}, errorConstants.IncorrectFormat},
+		{"+ve with positive NumberData", args{baseTypes.NewParameter(baseProperties.NewMetaProperty(baseIDs.NewStringID("bondRate"), baseData.NewNumberData(1)))}, nil},
+		{"+ve with negative NumberData", args{baseTypes.NewParameter(baseProperties.NewMetaProperty(baseIDs.NewStringID("bondRate"), baseData.NewNumberData(-1)))}, nil},
+		{"+ve with zero NumberData", args{baseTypes.NewParameter(baseProperties.NewMetaProperty(baseIDs.NewStringID("bondRate"), baseData.NewNumberData(0)))}, nil},
+		{"+ve with incorrect ID", args{baseTypes.NewParameter(baseProperties.NewMetaProperty(baseIDs.NewStringID("ID"), baseData.NewBooleanData(false)))}, errorConstants.IncorrectFormat},
+		{"-ve nil", args{}, errorConstants.IncorrectFormat},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := validator(tt.args.i); err != tt.wantError {
+				t.Errorf("validator() error = %v, wantErr %v", err, tt.wantError)
+			}
+		})
+	}
 }
