a39fa0328790fb22990ae148de2f1f3cc5ed2d3c
muku314115 <mukangotan@gmail.com>
2023-03-01 20:46:31 +0530

    persn[acpinqpv

--- modules/orders/internal/transactions/make/message.go
+++ modules/orders/internal/transactions/make/message.go
@@ -4,7 +4,6 @@
 package make
 
 import (
-	"github.com/AssetMantle/modules/schema/data/base"
 	"github.com/asaskevich/govalidator"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
@@ -29,7 +28,7 @@ func (message *Message) ValidateBasic() error {
 	if err != nil {
 		return errorConstants.IncorrectMessage.Wrapf(err.Error())
 	}
-	if !sdkTypes.ValidSortableDec(message.MakerOwnableSplit.Get()) || !sdkTypes.ValidSortableDec(message.TakerOwnableSplit.Get()) {
+	if !sdkTypes.ValidSortableDec(message.MakerOwnableSplit) || !sdkTypes.ValidSortableDec(message.TakerOwnableSplit) {
 		return errorConstants.IncorrectMessage.Wrapf("invalid split")
 	}
 	return nil
@@ -68,8 +67,8 @@ func newMessage(from sdkTypes.AccAddress, fromID ids.IdentityID, classificationI
 		MakerOwnableID:          makerOwnableID.(*baseIds.AnyOwnableID),
 		TakerOwnableID:          takerOwnableID.(*baseIds.AnyOwnableID),
 		ExpiresIn:               expiresIn.(*baseTypes.Height),
-		MakerOwnableSplit:       base.NewDecData(makerOwnableSplit).(*base.DecData),
-		TakerOwnableSplit:       base.NewDecData(takerOwnableSplit).(*base.DecData),
+		MakerOwnableSplit:       makerOwnableSplit,
+		TakerOwnableSplit:       takerOwnableSplit,
 		ImmutableMetaProperties: immutableMetaProperties.(*baseLists.PropertyList),
 		ImmutableProperties:     immutableProperties.(*baseLists.PropertyList),
 		MutableMetaProperties:   mutableMetaProperties.(*baseLists.PropertyList),
