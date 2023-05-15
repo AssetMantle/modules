6d23fcc0e3686e7a87692f033534e96335bf316a
muku314115 <mukangotan@gmail.com>
2023-05-15 16:37:42 +0530

    adding splits send

--- x/splits/simulator/genesis.go
+++ x/splits/simulator/genesis.go
@@ -4,24 +4,23 @@
 package simulator
 
 import (
+	"github.com/AssetMantle/modules/simulation/simulatedDatabase/assets"
+	"github.com/AssetMantle/modules/simulation/simulatedDatabase/identities"
+	"github.com/AssetMantle/modules/x/splits/mappable"
+	"github.com/AssetMantle/schema/go/ids/base"
+	base2 "github.com/AssetMantle/schema/go/types/base"
 	"math/rand"
 
 	"github.com/AssetMantle/modules/helpers"
 	baseHelpers "github.com/AssetMantle/modules/helpers/base"
-	baseSimulation "github.com/AssetMantle/modules/simulation/schema/types/base"
 	"github.com/AssetMantle/modules/x/splits/genesis"
-	"github.com/AssetMantle/modules/x/splits/mappable"
 	splitsModule "github.com/AssetMantle/modules/x/splits/module"
 	"github.com/AssetMantle/modules/x/splits/parameters/wrapAllowedCoins"
 	"github.com/AssetMantle/schema/go/data"
 	baseData "github.com/AssetMantle/schema/go/data/base"
-	baseIDs "github.com/AssetMantle/schema/go/ids/base"
 	baseParameters "github.com/AssetMantle/schema/go/parameters/base"
-	baseQualified "github.com/AssetMantle/schema/go/qualified/base"
-	"github.com/AssetMantle/schema/go/types/base"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	simulationTypes "github.com/cosmos/cosmos-sdk/types/simulation"
 )
 
 func (simulator) RandomizedGenesisState(simulationState *module.SimulationState) {
@@ -35,12 +34,24 @@ func (simulator) RandomizedGenesisState(simulationState *module.SimulationState)
 		func(rand *rand.Rand) { Data = baseData.NewDecData(sdkTypes.NewDecWithPrec(int64(rand.Intn(99)), 2)) },
 	)
 
-	mappableList := make([]helpers.Mappable, simulationState.Rand.Intn(99))
+	mappableList := make([]helpers.Mappable, len(assets.ClassificationIDMappableBytesMap))
 
-	for i := range mappableList {
-		immutables := baseQualified.NewImmutables(baseSimulation.GenerateRandomPropertyList(simulationState.Rand))
-		mutables := baseQualified.NewMutables(baseSimulation.GenerateRandomPropertyList(simulationState.Rand))
-		mappableList[i] = mappable.NewMappable(base.NewSplit(baseIDs.NewIdentityID(baseIDs.NewClassificationID(immutables, mutables), immutables), baseIDs.NewCoinID(baseIDs.NewStringID(simulationTypes.RandStringOfLength(simulationState.Rand, simulationState.Rand.Intn(99)))), simulationTypes.RandomAmount(simulationState.Rand, sdkTypes.NewInt(9999999999))))
+	var assetIDString, identityIDString string
+	for i := 0; i < len(assets.ClassificationIDMappableBytesMap); i++ {
+		assetMap := assets.GetAssetData(simulationState.Accounts[i].Address.String())
+		for _, id := range assetMap {
+			assetIDString = id
+		}
+		assetID, _ := base.ReadAssetID(assetIDString)
+
+		identityMap := identities.GetIDData(simulationState.Accounts[i].Address.String())
+
+		for _, id := range identityMap {
+			identityIDString = id
+		}
+		identityID, _ := base.ReadIdentityID(identityIDString)
+
+		mappableList[i] = mappable.NewMappable(base2.NewSplit(identityID, assetID.ToAnyOwnableID().Get(), sdkTypes.NewInt(1)))
 	}
 
 	genesisState := genesis.Prototype().Initialize(mappableList, baseParameters.NewParameterList(wrapAllowedCoins.Parameter.Mutate(Data)))
--- x/splits/simulator/operations.go
+++ x/splits/simulator/operations.go
@@ -4,6 +4,11 @@
 package simulator
 
 import (
+	simulationModules "github.com/AssetMantle/modules/simulation"
+	"github.com/AssetMantle/modules/simulation/simulatedDatabase/assets"
+	"github.com/AssetMantle/modules/simulation/simulatedDatabase/identities"
+	"github.com/AssetMantle/modules/x/splits/transactions/send"
+	baseIDs "github.com/AssetMantle/schema/go/ids/base"
 	"math/rand"
 
 	"github.com/AssetMantle/modules/helpers"
@@ -24,17 +29,51 @@ func (simulator) WeightedOperations(simulationState module.SimulationState, modu
 		},
 	)
 
-	//return simulation.WeightedOperations{
-	//	simulation.NewWeightedOperation(
-	//		weightMsg,
-	//		simulateMsg(),
-	//	),
-	//}
+	return simulation.WeightedOperations{
+		simulation.NewWeightedOperation(
+			weightMsg+1000,
+			simulateSendMsg(module),
+		),
+	}
 	return nil
 }
 
-func simulateMsg() simulationTypes.Operation {
+func simulateSendMsg(module helpers.Module) simulationTypes.Operation {
 	return func(rand *rand.Rand, baseApp *baseapp.BaseApp, context sdkTypes.Context, simulationAccountList []simulationTypes.Account, chainID string) (simulationTypes.OperationMsg, []simulationTypes.FutureOperation, error) {
-		return simulationTypes.NewOperationMsg(nil, true, "", base.CodecPrototype().GetProtoCodec()), nil, nil
+		var err error
+		var result *sdkTypes.Result
+		var assetIDString, identityIDString string
+
+		from, _ := simulationTypes.RandomAcc(rand, simulationAccountList)
+		to, _ := simulationTypes.RandomAcc(rand, simulationAccountList)
+		fromIDMap := identities.GetIDData(from.Address.String())
+
+		for _, id := range fromIDMap {
+			identityIDString = id
+			break
+		}
+		fromID, _ := baseIDs.ReadIdentityID(identityIDString)
+
+		toIDMap := identities.GetIDData(to.Address.String())
+		for _, id := range toIDMap {
+			identityIDString = id
+			break
+		}
+		toID, _ := baseIDs.ReadIdentityID(identityIDString)
+
+		assetMap := assets.GetAssetData(from.Address.String())
+		for _, id := range assetMap {
+			assetIDString = id
+			break
+		}
+
+		assetID, _ := baseIDs.ReadAssetID(assetIDString)
+		message := send.NewMessage(from.Address, fromID, toID, assetID, sdkTypes.NewInt(1))
+
+		result, err = simulationModules.ExecuteMessage(context, module, message.(helpers.Message))
+		if err != nil {
+			return simulationTypes.NewOperationMsg(message, false, err.Error(), base.CodecPrototype().GetProtoCodec()), nil, nil
+		}
+		return simulationTypes.NewOperationMsg(message, true, string(result.Data), base.CodecPrototype().GetProtoCodec()), nil, nil
 	}
 }
