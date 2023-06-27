472c54198d8c536c37e28511e4fcc871dc48f715
deepanshutr <deepanshutr@gmail.com>
2023-06-27 19:28:48 +0530

    revert: {refactor(queries): field page_request to pagination to follow convention}

--- x/splits/queries/splits/query_keeper.go
+++ x/splits/queries/splits/query_keeper.go
@@ -23,7 +23,7 @@ func (queryKeeper queryKeeper) Enquire(context context.Context, queryRequest hel
 	return queryResponse, err
 }
 func (queryKeeper queryKeeper) Handle(context context.Context, queryRequest *QueryRequest) (*QueryResponse, error) {
-	return newQueryResponse(queryKeeper.mapper.NewCollection(context).FetchPaginated(key.NewKey(base.PrototypeSplitID()), queryRequest.Pagination)), nil
+	return newQueryResponse(queryKeeper.mapper.NewCollection(context).FetchPaginated(key.NewKey(base.PrototypeSplitID()), queryRequest.PageRequest)), nil
 }
 
 func (queryKeeper queryKeeper) Initialize(mapper helpers.Mapper, _ helpers.ParameterManager, _ []interface{}) helpers.Keeper {
