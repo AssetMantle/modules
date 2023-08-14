5d672d8ab029c506f148d6e2f903bca873fea47c
deepanshutr <deepanshutr@gmail.com>
2023-08-14 15:15:12 +0530

    chore(splits): simplify code

--- x/splits/auxiliaries/mint/auxiliary_keeper.go
+++ x/splits/auxiliaries/mint/auxiliary_keeper.go
@@ -6,13 +6,8 @@ package mint
 import (
 	"context"
 
-	baseIDs "github.com/AssetMantle/schema/go/ids/base"
-	"github.com/AssetMantle/schema/go/types/base"
-
 	"github.com/AssetMantle/modules/helpers"
-	"github.com/AssetMantle/modules/x/splits/key"
-	"github.com/AssetMantle/modules/x/splits/mappable"
-	"github.com/AssetMantle/modules/x/splits/record"
+	"github.com/AssetMantle/modules/x/splits/utilities"
 )
 
 type auxiliaryKeeper struct {
@@ -24,14 +19,8 @@ var _ helpers.AuxiliaryKeeper = (*auxiliaryKeeper)(nil)
 func (auxiliaryKeeper auxiliaryKeeper) Help(context context.Context, request helpers.AuxiliaryRequest) (helpers.AuxiliaryResponse, error) {
 	auxiliaryRequest := auxiliaryRequestFromInterface(request)
 
-	splitID := baseIDs.NewSplitID(auxiliaryRequest.AssetID, auxiliaryRequest.OwnerID)
-	splits := auxiliaryKeeper.mapper.NewCollection(context).Fetch(key.NewKey(splitID))
-
-	Mappable := splits.GetMappable(key.NewKey(splitID))
-	if Mappable == nil {
-		splits.Add(record.NewRecord(baseIDs.NewSplitID(auxiliaryRequest.AssetID, auxiliaryRequest.OwnerID), base.NewSplit(auxiliaryRequest.Value)))
-	} else {
-		splits.Mutate(record.NewRecord(splitID, mappable.GetSplit(Mappable).Add(auxiliaryRequest.Value)))
+	if _, err := utilities.AddSplits(auxiliaryKeeper.mapper.NewCollection(context), auxiliaryRequest.OwnerID, auxiliaryRequest.AssetID, auxiliaryRequest.Value); err != nil {
+		return newAuxiliaryResponse(), err
 	}
 
 	return newAuxiliaryResponse(), nil
