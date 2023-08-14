2c27452c0e84c27109419be685637b4fc9dc93bb
deepanshutr <deepanshutr@gmail.com>
2023-08-14 13:25:19 +0530

    chore(splits): code optimization

--- x/splits/auxiliaries/transfer/auxiliary_keeper.go
+++ x/splits/auxiliaries/transfer/auxiliary_keeper.go
@@ -6,15 +6,8 @@ package transfer
 import (
 	"context"
 
-	errorConstants "github.com/AssetMantle/schema/go/errors/constants"
-	baseIDs "github.com/AssetMantle/schema/go/ids/base"
-	"github.com/AssetMantle/schema/go/types/base"
-	sdkTypes "github.com/cosmos/cosmos-sdk/types"
-
 	"github.com/AssetMantle/modules/helpers"
-	"github.com/AssetMantle/modules/x/splits/key"
-	"github.com/AssetMantle/modules/x/splits/mappable"
-	"github.com/AssetMantle/modules/x/splits/record"
+	"github.com/AssetMantle/modules/x/splits/utilities"
 )
 
 type auxiliaryKeeper struct {
@@ -25,34 +18,15 @@ var _ helpers.AuxiliaryKeeper = (*auxiliaryKeeper)(nil)
 
 func (auxiliaryKeeper auxiliaryKeeper) Help(context context.Context, request helpers.AuxiliaryRequest) (helpers.AuxiliaryResponse, error) {
 	auxiliaryRequest := auxiliaryRequestFromInterface(request)
-	if auxiliaryRequest.Value.LTE(sdkTypes.ZeroInt()) {
-		return nil, errorConstants.InvalidRequest.Wrapf("transfer value must be greater than zero")
-	}
 
 	splits := auxiliaryKeeper.mapper.NewCollection(context)
 
-	fromSplitID := baseIDs.NewSplitID(auxiliaryRequest.AssetID, auxiliaryRequest.FromID)
-	Mappable := splits.Fetch(key.NewKey(fromSplitID)).GetMappable(key.NewKey(fromSplitID))
-	if Mappable == nil {
-		return nil, errorConstants.EntityNotFound.Wrapf("split with ID %s not found", fromSplitID.AsString())
-	}
-	fromSplit := mappable.GetSplit(Mappable)
-
-	switch fromSplit = fromSplit.Subtract(auxiliaryRequest.Value); {
-	case fromSplit.GetValue().LT(sdkTypes.ZeroInt()):
-		return nil, errorConstants.InsufficientBalance.Wrapf("insufficient balance")
-	case fromSplit.GetValue().Equal(sdkTypes.ZeroInt()):
-		splits.Remove(record.NewRecord(fromSplitID, fromSplit))
-	default:
-		splits.Mutate(record.NewRecord(fromSplitID, fromSplit))
+	if _, err := utilities.SubtractSplits(splits, auxiliaryRequest.FromID, auxiliaryRequest.AssetID, auxiliaryRequest.Value); err != nil {
+		return newAuxiliaryResponse(), err
 	}
 
-	toSplitID := baseIDs.NewSplitID(auxiliaryRequest.AssetID, auxiliaryRequest.ToID)
-
-	if Mappable := splits.Fetch(key.NewKey(toSplitID)).GetMappable(key.NewKey(toSplitID)); Mappable == nil {
-		splits.Add(record.NewRecord(baseIDs.NewSplitID(auxiliaryRequest.AssetID, auxiliaryRequest.ToID), base.NewSplit(auxiliaryRequest.Value)))
-	} else {
-		splits.Mutate(record.NewRecord(toSplitID, mappable.GetSplit(Mappable).Add(auxiliaryRequest.Value)))
+	if _, err := utilities.AddSplits(splits, auxiliaryRequest.ToID, auxiliaryRequest.AssetID, auxiliaryRequest.Value); err != nil {
+		return newAuxiliaryResponse(), err
 	}
 
 	return newAuxiliaryResponse(), nil
