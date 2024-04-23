cb4de6e5a27db5b18720a79b8ff708302f53c60b
deepanshutr <deepanshutr@gmail.com>
2024-04-23 12:33:12 +0530

    fix(assets): modifying parameter unwrap allowed coins for easier testing

--- x/assets/parameters/unwrap_allowed_coins/parameter.go
+++ x/assets/parameters/unwrap_allowed_coins/parameter.go
@@ -16,7 +16,7 @@ import (
 )
 
 var ID = constantProperties.UnwrapAllowedCoinsProperty.GetKey()
-var Parameter = baseParameters.NewParameter(base.NewMetaProperty(ID, baseData.NewListData()))
+var Parameter = baseParameters.NewParameter(base.NewMetaProperty(ID, baseData.NewListData(baseData.NewStringData(sdkTypes.DefaultBondDenom))))
 
 func validator(i interface{}) error {
 	switch value := i.(type) {
