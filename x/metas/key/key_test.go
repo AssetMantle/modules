96e901bcc246e9d0d63d1523ac0d33bf8292f7a5
Deepanshu Tripathi <deepanshutr@gmail.com>
2024-06-20 15:24:13 +0530

    refactor(key_test): pull back all packages one level
    Signed-off-by: Deepanshu Tripathi <deepanshutr@gmail.com>

--- x/metas/key/key_test.go
+++ x/metas/key/key_test.go
@@ -8,9 +8,9 @@ import (
 	"testing"
 
 	"github.com/AssetMantle/modules/helpers"
-	baseData "github.com/AssetMantle/schema/go/data/base"
-	"github.com/AssetMantle/schema/go/ids"
-	baseIDs "github.com/AssetMantle/schema/go/ids/base"
+	baseData "github.com/AssetMantle/schema/data/base"
+	"github.com/AssetMantle/schema/ids"
+	baseIDs "github.com/AssetMantle/schema/ids/base"
 )
 
 var (
