f788d432f6a63982a00b0196b0eba5d5cd797659
Deepanshu Tripathi <deepanshutr@gmail.com>
2024-08-29 14:15:35 +0530

    refactor(common transaction request): replacing complex types with string for simplicity
    Signed-off-by: Deepanshu Tripathi <deepanshutr@gmail.com>

--- helpers/base/common_transaction_request.go
+++ helpers/base/common_transaction_request.go
@@ -11,17 +11,17 @@ import (
 )
 
 type commonTransactionRequest struct {
-	From          string            `json:"from"`
-	Memo          string            `json:"memo"`
-	TimeoutHeight uint64            `json:"timeout_height"`
-	AccountNumber uint64            `json:"account_number"`
-	Sequence      uint64            `json:"sequence"`
-	ChainID       string            `json:"chain_id"`
-	Gas           string            `json:"gas"`
-	Fees          sdkTypes.Coins    `json:"fees"`
-	GasPrices     sdkTypes.DecCoins `json:"gas_prices"`
-	Simulate      bool              `json:"simulate"`
-	GasAdjustment string            `json:"gas_adjustment"`
+	From          string `json:"from"`
+	Memo          string `json:"memo"`
+	TimeoutHeight uint64 `json:"timeout_height"`
+	AccountNumber uint64 `json:"account_number"`
+	Sequence      uint64 `json:"sequence"`
+	ChainID       string `json:"chain_id"`
+	Gas           string `json:"gas"`
+	Fees          string `json:"fees"`
+	GasPrices     string `json:"gas_prices"`
+	Simulate      bool   `json:"simulate"`
+	GasAdjustment string `json:"gas_adjustment"`
 }
 
 var _ helpers.CommonTransactionRequest = commonTransactionRequest{}
@@ -53,7 +53,12 @@ func (commonTransactionRequest commonTransactionRequest) GetTimeoutHeight() uint
 	return commonTransactionRequest.TimeoutHeight
 }
 func (commonTransactionRequest commonTransactionRequest) GetGasPrices() sdkTypes.DecCoins {
-	return commonTransactionRequest.GasPrices
+	decCoins, err := sdkTypes.ParseDecCoins(commonTransactionRequest.GasPrices)
+	if err != nil {
+		panic(err)
+	}
+
+	return decCoins
 }
 func (commonTransactionRequest commonTransactionRequest) IsSimulated() bool {
 	return commonTransactionRequest.Simulate
@@ -69,21 +74,34 @@ func (commonTransactionRequest commonTransactionRequest) SetChainID(chainIDStrin
 	return commonTransactionRequest
 }
 func (commonTransactionRequest commonTransactionRequest) GetFees() sdkTypes.Coins {
-	return commonTransactionRequest.Fees
+	coins, err := sdkTypes.ParseCoinsNormalized(commonTransactionRequest.Fees)
+	if err != nil {
+		panic(err)
+	}
+
+	return coins
 }
 func (commonTransactionRequest commonTransactionRequest) Validate() error {
 	if _, err := sdkTypes.AccAddressFromBech32(commonTransactionRequest.From); err != nil || len(commonTransactionRequest.From) == 0 {
 		return fmt.Errorf("invalid from address: %s", commonTransactionRequest.From)
 	}
 
+	if _, err := sdkTypes.ParseDecCoins(commonTransactionRequest.GasPrices); err != nil {
+		return fmt.Errorf("invalid gas prices %s", commonTransactionRequest.GasPrices)
+	}
+
+	if _, err := sdkTypes.ParseCoinsNormalized(commonTransactionRequest.Fees); err != nil {
+		return fmt.Errorf("invalid fees %s", commonTransactionRequest.Fees)
+	}
+
 	if !commonTransactionRequest.Simulate {
 		if len(commonTransactionRequest.ChainID) == 0 {
 			return fmt.Errorf("chain-id required but not specified")
 		}
-		if !commonTransactionRequest.Fees.IsZero() && !commonTransactionRequest.GasPrices.IsZero() {
+		if !commonTransactionRequest.GetFees().IsZero() && !commonTransactionRequest.GetGasPrices().IsZero() {
 			return fmt.Errorf("cannot provide both fees and gas prices")
 		}
-		if !commonTransactionRequest.Fees.IsValid() && !commonTransactionRequest.GasPrices.IsValid() {
+		if !commonTransactionRequest.GetFees().IsValid() && !commonTransactionRequest.GetGasPrices().IsValid() {
 			return fmt.Errorf("invalid fees or gas prices provided")
 		}
 	}
@@ -102,13 +120,13 @@ func (commonTransactionRequest commonTransactionRequest) ValidateBasic(responseW
 	return true
 }
 
-func NewCommonTransactionRequest(from, memo, chainID, gas, gasAdjustment string, accountNumber, sequence, timeOutHeight uint64, fees sdkTypes.Coins, gasPrices sdkTypes.DecCoins, simulate bool) helpers.CommonTransactionRequest {
+func NewCommonTransactionRequest(from, memo, chainID, gas, gasAdjustment string, accountNumber, sequence, timeOutHeight uint64, fees, gasPrices string, simulate bool) helpers.CommonTransactionRequest {
 	return commonTransactionRequest{
 		From:          strings.TrimSpace(from),
 		Memo:          strings.TrimSpace(memo),
 		ChainID:       strings.TrimSpace(chainID),
-		Fees:          fees,
-		GasPrices:     gasPrices,
+		Fees:          strings.TrimSpace(fees),
+		GasPrices:     strings.TrimSpace(gasPrices),
 		Gas:           strings.TrimSpace(gas),
 		GasAdjustment: strings.TrimSpace(gasAdjustment),
 		AccountNumber: accountNumber,
@@ -122,5 +140,9 @@ func PrototypeCommonTransactionRequest() helpers.CommonTransactionRequest {
 }
 
 func NewCommonTransactionRequestFromContext(context client.Context) helpers.CommonTransactionRequest {
-	return NewCommonTransactionRequest(context.From, "", context.ChainID, "", "", uint64(context.Height), 0, 0, sdkTypes.Coins{}, sdkTypes.DecCoins{}, context.Simulate)
+	return commonTransactionRequest{
+		From:     context.FromAddress.String(),
+		ChainID:  context.ChainID,
+		Simulate: context.Simulate,
+	}
 }
