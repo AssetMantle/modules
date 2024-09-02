4afe2943982dd26acfd6c3d51043ab574c5dbc36
Deepanshu Tripathi <deepanshutr@gmail.com>
2024-09-02 13:06:39 +0530

    code cleanup
    Signed-off-by: Deepanshu Tripathi <deepanshutr@gmail.com>

--- helpers/amino_codec.go
+++ helpers/amino_codec.go
@@ -8,9 +8,7 @@ func RegisterLegacyAminoCodec(legacyAmino *codec.LegacyAmino) {
 	legacyAmino.RegisterInterface((*Mappable)(nil), nil)
 	legacyAmino.RegisterInterface((*QueryResponse)(nil), nil)
 	legacyAmino.RegisterInterface((*QueryRequest)(nil), nil)
-	legacyAmino.RegisterInterface((*CommonTransactionRequest)(nil), nil)
 	legacyAmino.RegisterInterface((*TransactionRequest)(nil), nil)
-	legacyAmino.RegisterInterface((*Request)(nil), nil)
 	legacyAmino.RegisterInterface((*Error)(nil), nil)
 	legacyAmino.RegisterInterface((*error)(nil), nil)
 }
