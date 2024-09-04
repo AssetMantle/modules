261472f41cb80344ee34e0fa45bcfbb30da3ae56
Deepanshu Tripathi <deepanshutr@gmail.com>
2024-09-04 19:00:15 +0530

    feat(mappable,record): defining and implementing ReadMappableFromIterator generic method
    Signed-off-by: Deepanshu Tripathi <deepanshutr@gmail.com>

--- helpers/mappable.go
+++ helpers/mappable.go
@@ -5,9 +5,18 @@ package helpers
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 )
 
 type Mappable interface {
 	codec.ProtoMarshaler
 	ValidateBasic() error
+}
+
+func ReadMappableFromIterator[T Mappable](iterator sdkTypes.Iterator, mappable T) Mappable {
+	if err := mappable.Unmarshal(iterator.Value()); err != nil {
+		panic(err)
+	}
+
+	return mappable
 }
