96df4829846ecc1129104a661f3a0e8737827ee5
deepanshutr <deepanshutr@gmail.com>
2024-12-02 18:41:45 +0530

    feat(parameter list): new implementations incorporates deterministic parameter list and kv store manipulation logic

--- helpers/base/parameter_manager.go
+++ helpers/base/parameter_manager.go
@@ -5,103 +5,93 @@ package base
 
 import (
 	"fmt"
-	"github.com/AssetMantle/schema/ids"
+	"github.com/AssetMantle/schema/lists"
+	"github.com/AssetMantle/schema/lists/base"
 	"github.com/AssetMantle/schema/parameters"
 	storeTypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
-	paramsTypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"golang.org/x/net/context"
 
 	"github.com/AssetMantle/modules/helpers"
 )
 
+// TODO: move to a constants file
+var parametersStorePrefix = []byte{0x01}
+
 type parameterManager struct {
-	kvStoreKey            *storeTypes.KVStoreKey
-	validatableParameters []helpers.ValidatableParameter
+	kvStoreKey                   *storeTypes.KVStoreKey
+	parameterList                lists.ParameterList
+	defaultValidatableParameters []helpers.ValidatableParameter
 }
 
 var _ helpers.ParameterManager = (*parameterManager)(nil)
 
-func (parameterManager parameterManager) Get() []parameters.Parameter {
-	Parameters := make([]parameters.Parameter, len(parameterManager.validatableParameters))
-	for i, validatableParameter := range parameterManager.validatableParameters {
-		Parameters[i] = validatableParameter.GetParameter()
-	}
-	return Parameters
+func (parameterManager parameterManager) Get() lists.ParameterList {
+	return parameterManager.parameterList
 }
-func (parameterManager parameterManager) GetParameter(propertyID ids.PropertyID) parameters.Parameter {
-	if validatableParameter := parameterManager.getValidatableParameter(propertyID); validatableParameter != nil {
-		return validatableParameter.GetParameter()
-	}
-	return nil
-}
-func (parameterManager parameterManager) getValidatableParameter(propertyID ids.PropertyID) helpers.ValidatableParameter {
-	for _, validatableParameter := range parameterManager.validatableParameters {
-		if validatableParameter.GetParameter().GetMetaProperty().GetID().Compare(propertyID) == 0 {
-			return validatableParameter
-		}
-	}
-	return nil
-}
-func (parameterManager parameterManager) ValidateGenesisParameters(genesis helpers.Genesis) error {
-	if len(genesis.GetParameters()) != len(parameterManager.validatableParameters) {
-		return fmt.Errorf("genesis parameters length mismatch")
+func (parameterManager parameterManager) GetDefaultParameterList() lists.ParameterList {
+	defaultParameters := make([]parameters.Parameter, len(parameterManager.defaultValidatableParameters))
+
+	for i, validatableParameter := range parameterManager.defaultValidatableParameters {
+		defaultParameters[i] = validatableParameter.GetParameter()
 	}
 
-	for _, parameter := range genesis.GetParameters() {
-		if err := parameter.ValidateBasic(); err != nil {
-			return fmt.Errorf("invalid parameter in genesis %s : %s", parameter.GetMetaProperty().GetID().AsString(), err.Error())
+	return base.NewParameterList(defaultParameters...)
+}
+func (parameterManager parameterManager) Set(parameters ...parameters.Parameter) helpers.ParameterManager {
+	if parameterManager.parameterList == nil {
+		parameterManager.parameterList = parameterManager.GetDefaultParameterList()
+	}
+
+	parameterManager.parameterList = parameterManager.parameterList.Mutate(parameters...)
+	return parameterManager
+}
+func (parameterManager parameterManager) Validate() error {
+	if len(parameterManager.defaultValidatableParameters) != len(parameterManager.parameterList.Get()) {
+		return fmt.Errorf("parameter count mismatch, wanted %d, got %d", len(parameterManager.defaultValidatableParameters), len(parameterManager.parameterList.Get()))
+	}
+
+	if err := parameterManager.parameterList.ValidateBasic(); err != nil {
+		return err
+	}
+	
+	for _, validatableParameter := range parameterManager.defaultValidatableParameters {
+		parameter := parameterManager.parameterList.GetParameter(validatableParameter.GetParameter().GetMetaProperty().GetID())
+		if parameter == nil {
+			return fmt.Errorf("parameter with id %s not found", validatableParameter.GetParameter().GetMetaProperty().GetID())
 		}
 
-		validator := parameterManager.getValidatableParameter(parameter.GetMetaProperty().GetID())
-		if validator == nil {
-			return fmt.Errorf("invalid parameter in genesis %s : not found", parameter.GetMetaProperty().GetID().AsString())
-		}
-
-		if err := validator.GetValidator()(parameter.GetMetaProperty().GetData().Get().AsString()); err != nil {
-			return fmt.Errorf("invalid parameter in genesis %s : %s", parameter.GetMetaProperty().GetID().AsString(), err.Error())
+		if err := validatableParameter.Mutate(parameter.GetMetaProperty().GetData()).Validate(); err != nil {
+			return err
 		}
 	}
 
 	return nil
 }
 func (parameterManager parameterManager) Fetch(context context.Context) helpers.ParameterManager {
-	for _, validatableParameter := range parameterManager.validatableParameters {
-		if data, err := validatableParameter.GetParameter().GetMetaProperty().GetData().Get().FromString(string(sdkTypes.UnwrapSDKContext(context).KVStore(parameterManager.kvStoreKey).Get(validatableParameter.GetParameter().GetMetaProperty().GetID().Bytes()))); err != nil {
-			panic(err)
-		} else {
-			validatableParameter = validatableParameter.Mutate(data)
-		}
-	}
+	parameterList := base.PrototypeParameterList()
+	CodecPrototype().MustUnmarshal(sdkTypes.UnwrapSDKContext(context).KVStore(parameterManager.kvStoreKey).Get(parametersStorePrefix), parameterList)
+	parameterManager.parameterList = parameterList
 
 	return parameterManager
 }
-func (parameterManager parameterManager) Set(context context.Context, parameters []parameters.Parameter) helpers.ParameterManager {
-	for _, parameter := range parameters {
-		sdkTypes.UnwrapSDKContext(context).KVStore(parameterManager.kvStoreKey).Set(parameter.GetMetaProperty().GetID().Bytes(), []byte(parameter.GetMetaProperty().GetData().Get().AsString()))
+func (parameterManager parameterManager) Update(context context.Context) (helpers.ParameterManager, error) {
+	if err := parameterManager.Validate(); err != nil {
+		return nil, err
 	}
 
-	return parameterManager
-}
-func (parameterManager parameterManager) ParamSetPairs() paramsTypes.ParamSetPairs {
-	paramSetPairList := make([]paramsTypes.ParamSetPair, len(parameterManager.validatableParameters))
+	sdkTypes.UnwrapSDKContext(context).KVStore(parameterManager.kvStoreKey).Set(parametersStorePrefix, CodecPrototype().MustMarshal(parameterManager.parameterList))
 
-	for i, validatableParameter := range parameterManager.validatableParameters {
-		paramSetPairList[i] = paramsTypes.NewParamSetPair(validatableParameter.GetParameter().GetMetaProperty().GetID().Bytes(), validatableParameter.GetParameter().GetMetaProperty().GetData().Get().AsString(), validatableParameter.GetValidator())
-	}
+	return parameterManager, nil
+}
 
-	return paramSetPairList
-}
-func (parameterManager parameterManager) GetKeyTable() paramsTypes.KeyTable {
-	return paramsTypes.NewKeyTable().RegisterParamSet(parameterManager)
-}
 func (parameterManager parameterManager) Initialize(kvStoreKey *storeTypes.KVStoreKey) helpers.ParameterManager {
 	parameterManager.kvStoreKey = kvStoreKey
 	return parameterManager
 }
 
-func NewParameterManager(validatableParameters ...helpers.ValidatableParameter) helpers.ParameterManager {
+func NewParameterManager(defaultValidatableParameters ...helpers.ValidatableParameter) helpers.ParameterManager {
 	return parameterManager{
-		validatableParameters: validatableParameters,
+		defaultValidatableParameters: defaultValidatableParameters,
 	}
 }
